fbdc640525d05991c8709e06e899c31998294281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:19 +0000

    Add package lyrix-api/0.1.1/lyrix-api-0.1.1.tar.gz

---
+++ packages/lyrix-api/lyrix-api-0.1.1.tar.gz/lyrix-api-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lyrix_api']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.25.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'lyrix-api',
+    'version': '0.1.1',
+    'description': 'Python bindings for Lyrix backend',
+    'long_description': None,
+    'author': 'Srevin Saju',
+    'author_email': 'srevinsaju@sugarlabs.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)

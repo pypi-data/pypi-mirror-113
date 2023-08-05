96fd29afdffb6c2a7f7541e9af39662d004ea053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:41 +0000

    Add package sensai/0.1.6/sensai-0.1.6.tar.gz

---
+++ packages/sensai/sensai-0.1.6.tar.gz/sensai-0.1.6/src/sensai/util/datastruct.py
@@ -0,0 +1,229 @@
+from bisect import bisect_right, bisect_left
+from enum import Enum
+from typing import Sequence, Optional, TypeVar, Generic, Tuple, Dict, Any
+
+from . import sequences as array_util
+
+TKey = TypeVar("TKey")
+TValue = TypeVar("TValue")
+
+
+class Trivalent(Enum):
+    TRUE = "true"
+    FALSE = "false"
+    UNKNOWN = "unknown"
+
+    @classmethod
+    def fromBool(cls, b: bool):
+        return cls.TRUE if b else cls.FALSE
+
+    def isTrue(self):
+        return self == Trivalent.TRUE
+
+    def isFalse(self):
+        return self == Trivalent.FALSE
+
+
+class DeferredParams:
+    """
+    Represents a dictionary of parameters that is specifically designed to hold parameters that can only defined late within
+    a process (i.e. not initially at construction time), e.g. because the parameters are data-dependent and therefore can only
+    be determined once the data has been seen.
+    """
+    UNDEFINED = "__undefined__DeferredParams"
+
+    def __init__(self):
+        self.params = {}
+
+    def setParam(self, name: str, value: Any):
+        self.params[name] = value
+
+    def getParam(self, name, default=UNDEFINED):
+        """
+        :param name: the parameter name
+        :param default: in case no value is set, return this value, and if UNDEFINED (default), raise KeyError
+        :return: the parameter value
+        """
+        if default == self.UNDEFINED:
+            return self.params[name]
+        else:
+            return self.params.get(name, default)
+
+    def getDict(self) -> Dict[str, Any]:
+        return self.params
+
+
+class SortedValues(Generic[TValue]):
+    """
+    Provides convenient binary search (bisection) operations for sorted sequences
+    """
+    def __init__(self, sortedValues: Sequence[TValue]):
+        self.values = sortedValues
+
+    def __len__(self):
+        return len(self.values)
+
+    def floorIndex(self, value) -> Optional[int]:
+        """
+        Finds the rightmost index where the value is less than or equal to the given value
+
+        :param value: the value to search for
+        :return: the index or None if there is no such index
+        """
+        return array_util.floorIndex(self.values, value)
+
+    def ceilIndex(self, value) -> Optional[int]:
+        """
+        Finds the leftmost index where the value is greater than or equal to the given value
+
+        :param value: the value to search for
+        :return: the index or None if there is no such index
+        """
+        return array_util.ceilIndex(self.values, value)
+
+    def _value(self, idx: Optional[int]) -> Optional[TValue]:
+        if idx is None:
+            return None
+        else:
+            return self.values[idx]
+
+    def floorValue(self, value) -> Optional[TValue]:
+        """
+        Finds the largest value that is less than or equal to the given value
+
+        :param value: the value to search for
+        :return: the value or None if there is no such value
+        """
+        return self._value(self.floorIndex(value))
+
+    def ceilValue(self, value) -> Optional[TValue]:
+        """
+        Finds the smallest value that is greater than or equal to the given value
+
+        :param value: the value to search for
+        :return: the value or None if there is no such value
+        """
+        return self._value(self.ceilIndex(value))
+
+    def _valueSlice(self, firstIndex, lastIndex):
+        if firstIndex is None or lastIndex is None:
+            return None
+        return self.values[firstIndex:lastIndex+1]
+
+    def valueSlice(self, lowestKey, highestKey) -> Optional[Sequence[TValue]]:
+        return self._valueSlice(self.ceilIndex(lowestKey), self.floorIndex(highestKey))
+
+
+class SortedKeysAndValues(Generic[TKey, TValue]):
+    def __init__(self, keys: Sequence[TKey], values: Sequence[TValue]):
+        """
+        :param keys: a sorted sequence of keys
+        :param values: a sequence of corresponding values
+        """
+        if len(keys) != len(values):
+            raise ValueError("Lengths of keys and values do not match")
+        self.keys = keys
+        self.values = values
+
+    def floorIndex(self, value) -> Optional[int]:
+        """
+        Finds the rightmost index where the value is less than or equal to the given value
+
+        :param value: the value to search for
+        :return: the index or None if there is no such index
+        """
+        return array_util.floorIndex(self.values, value)
+
+    def ceilIndex(self, value) -> Optional[int]:
+        """
+        Finds the leftmost index where the value is greater than or equal to the given value
+
+        :param value: the value to search for
+        :return: the index or None if there is no such index
+        """
+        return array_util.ceilIndex(self.values, value)
+
+    def floorValue(self, key) -> Optional[TValue]:
+        """
+        Returns the value for the largest index where the corresponding key is less than or equal to the given value
+
+        :param value: the value to search for
+        :return: the value or None if there is no such value
+        """
+        return array_util.floorValue(self.keys, key, values=self.values)
+
+    def ceilValue(self, key) -> Optional[TValue]:
+        """
+        Returns the value for the smallest index where the corresponding key is greater than or equal to the given value
+
+        :param value: the value to search for
+        :return: the value or None if there is no such value
+        """
+        return array_util.ceilValue(self.keys, key, values=self.values)
+
+    def valueSliceInner(self, lowerBoundKey, upperBoundKey):
+        return array_util.valueSliceOuter(self.keys, lowerBoundKey, upperBoundKey, values=self.values)
+
+    def valueSliceOuter(self, lowerBoundKey, upperBoundKey):
+        return array_util.valueSliceOuter(self.keys, lowerBoundKey, upperBoundKey, values=self.values)
+
+
+class SortedKeyValuePairs(Generic[TKey, TValue]):
+    @classmethod
+    def fromUnsortedKeyValuePairs(cls, unsortedKeyValuePairs: Sequence[Tuple[TKey, TValue]]):
+        return cls(sorted(unsortedKeyValuePairs, key=lambda x: x[0]))
+
+    def __init__(self, sortedKeyValuePairs: Sequence[Tuple[TKey, TValue]]):
+        self.entries = sortedKeyValuePairs
+        self._sortedKeys = SortedValues([t[0] for t in sortedKeyValuePairs])
+
+    def _value(self, idx: Optional[int]) -> Optional[TValue]:
+        if idx is None:
+            return None
+        return self.entries[idx][1]
+
+    def floorIndex(self, key) -> Optional[int]:
+        """Finds the rightmost index where the key is less than or equal to the given key"""
+        return self._sortedKeys.floorIndex(key)
+
+    def floorValue(self, key) -> Optional[TValue]:
+        return self._value(self.floorIndex(key))
+
+    def ceilIndex(self, key) -> Optional[int]:
+        """Find leftmost index where the key is greater than or equal to the given key"""
+        return self._sortedKeys.ceilIndex(key)
+
+    def ceilValue(self, key) -> Optional[TValue]:
+        return self._value(self.ceilIndex(key))
+
+    def _valueSlice(self, firstIndex, lastIndex):
+        if firstIndex is None or lastIndex is None:
+            return None
+        return [e[1] for e in self.entries[firstIndex:lastIndex+1]]
+
+    def valueSlice(self, lowestKey, highestKey) -> Optional[Sequence[TValue]]:
+        return self._valueSlice(self.ceilIndex(lowestKey), self.floorIndex(highestKey))
+
+    def slice(self, lowerBoundKey=None, upperBoundKey=None, inner=True) -> "SortedKeyValuePairs":
+        """
+        :param lowerBoundKey: the key defining the start of the slice (depending on inner);
+            if None, the first included entry will be the very first entry
+        :param upperBoundKey: the key defining the end of the slice (depending on inner);
+            if None, the last included entry will be the very last entry
+        :param inner: if True, the returned slice will be within the bounds; if False, the the returned
+            slice is extended by one entry in both directions such that it contains the bounds (where possible)
+        :return:
+        """
+        if lowerBoundKey is not None:
+            fromIndex = self.ceilIndex(lowerBoundKey) if inner else self.floorIndex(lowerBoundKey)
+            if fromIndex is None:
+                fromIndex = 0
+        else:
+            fromIndex = 0
+        if upperBoundKey is not None:
+            toIndex = self.floorIndex(upperBoundKey) if inner else self.ceilIndex(upperBoundKey)
+            if toIndex is None:
+                toIndex = len(self.entries)
+        else:
+            toIndex = len(self.entries)
+        return SortedKeyValuePairs(self.entries[fromIndex:toIndex])
---
+++ packages/sensai/sensai-0.1.6.tar.gz/sensai-0.1.6/src/sensai/util/string.py
@@ -0,0 +1,122 @@
+from typing import Union, List, Dict, Any, Sequence, Iterable, Optional
+import re
+
+
+def dictString(d):
+    return ', '.join([f'{k}={v}' for k, v in d.items()])
+
+
+def objectRepr(obj, memberNamesOrDict: Union[List[str], Dict[str, Any]]):
+    def toString(x):
+        if type(x) == dict:
+            return "{" + dictString(x) + "}"
+        else:
+            return str(x)
+
+    if type(memberNamesOrDict) == dict:
+        membersDict = memberNamesOrDict
+    else:
+        membersDict = {m: toString(getattr(obj, m)) for m in memberNamesOrDict}
+    return f"{obj.__class__.__name__}[{dictString(membersDict)}]"
+
+
+def orRegexGroup(allowedNames: Sequence[str]):
+    """
+
+    :param allowedNames: strings to include as literals in the regex
+    :return: raw string of the type (<name1>| ...|<nameN>), where special characters in the names have been escaped
+    """
+    allowedNames = [re.escape(name) for name in allowedNames]
+    return r"(%s)" % "|".join(allowedNames)
+
+
+class ToStringMixin:
+    """
+    Provides default implementations for __str__ and __repr__ which contain all attribute names and their values. The
+    latter also contains the object id.
+    """
+
+    def _toStringClassName(self):
+        return type(self).__qualname__
+
+    def _toStringProperties(self, exclude: Optional[Union[str, Iterable[str]]] = None, include: Optional[Union[str, Iterable[str]]] = None,
+            **additionalEntries) -> str:
+        """
+        Creates a string of the class attributes, with optional exclusions/inclusions/additions
+
+        :param exclude: attributes to be excluded; can only be non-empty if include is empty
+        :param include: attributes to be included; can only be non-empty if exclude is empty
+        :param additionalEntries: additional key-value-pairs which are added to the string just like the other attributes
+        :return: a string containing all attribute names and values
+        """
+        def mklist(x):
+            if x is None:
+                return []
+            if type(x) == str:
+                return [x]
+            return x
+
+        exclude = mklist(exclude)
+        include = mklist(include)
+        if len(exclude) > 0 and len(include) > 0:
+            raise ValueError("Cannot provide inclusions and exclusions at the same time")
+
+        if len(include) == 0:
+            attributeDict = self.__dict__
+            if self._toStringExcludePrivate():
+                attributeDict = {k: v for k, v in attributeDict.items() if not k.startswith("_")}
+        else:
+            attributeDict = {k: getattr(self, k) for k in include}
+        d = {k: v for k, v in attributeDict.items() if k not in exclude}
+        d.update(additionalEntries)
+        return dictString(d)
+
+    def _toStringObjectInfo(self) -> str:
+        """
+        Creates a string containing information on the objects state, which is the name and value of all attributes
+        without the attributes that are in the list provided by _toStringExclusions. It is used by the methods __str__
+        and __repr__. This method can be overwritten by sub-classes to provide a custom string.
+
+        :return: a string containing all attribute names and values
+        """
+        return self._toStringProperties(exclude=self._toStringExcludes(), include=self._toStringIncludes(), **self._toStringAdditionalEntries())
+
+    def _toStringExcludes(self) -> List[str]:
+        """
+        Returns a list of attribute names to be excluded from __str__ and __repr__. This method can be overwritten by
+        sub-classes which can call super to extend this list. This method will only have an effect if _toStringObjectInfo
+        is not overwritten by the sub class.
+
+        :return: a list of attribute names
+        """
+        return []
+
+    def _toStringIncludes(self) -> List[str]:
+        """
+        Returns a list of attribute names to be included in __str__ and __repr__. This method can be overwritten by
+        sub-classes which can call super to extend this list. This method will only have an effect if _toStringObjectInfo
+        is not overwritten by the sub class.
+
+        :return: a list of attribute names
+        """
+        return []
+
+    def _toStringAdditionalEntries(self) -> Dict[str, Any]:
+        return {}
+
+    def _toStringExcludePrivate(self) -> bool:
+        """
+        :return: whether to exclude properties that are private, i.e. start with an underscore; explicitly included attributes
+            will still be considered
+        """
+        return False
+
+    def __str__(self):
+        return f"{self._toStringClassName()}[{self._toStringObjectInfo()}]"
+
+    def __repr__(self):
+        info = f"id={id(self)}"
+        propertyInfo = self._toStringObjectInfo()
+        if len(propertyInfo) > 0:
+            info += ", " + propertyInfo
+        return f"{self._toStringClassName()}[{info}]"

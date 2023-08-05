db548ef44f68b4b39aee484325267be779ac5697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:41 +0000

    Add package designer/0.0.0/designer-0.0.0.tar.gz

---
+++ packages/designer/designer-0.0.0.tar.gz/designer-0.0.0/README.md
@@ -0,0 +1,2 @@
+# Playing
+### An evidence-based and student friendly visual graphics library to learn computer science with Python.
---
+++ packages/designer/designer-0.0.0.tar.gz/designer-0.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='designer',
+    version='0.0.0',
+    python_requires='>=3.6',
+    author='krishols',
+    author_email='kris@udel.edu',
+    description='Student-friendly and evidence-based visual graphics library.',
+    license='MIT',
+    long_description=open('README.md').read())

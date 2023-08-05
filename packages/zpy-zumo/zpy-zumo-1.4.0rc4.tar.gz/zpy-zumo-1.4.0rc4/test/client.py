4ad6485d93532872cb2fe05458917050bb83fbee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:57 +0000

    Add package zpy-zumo/1.4.0rc4/zpy-zumo-1.4.0rc4.tar.gz

---
+++ packages/zpy-zumo/zpy-zumo-1.4.0rc4.tar.gz/zpy-zumo-1.4.0rc4/test/client.py
@@ -0,0 +1,58 @@
+import json
+
+import zpy.client as zpy
+
+
+def test_1(**init_kwargs):
+    """In local env, simruns exist for config { "run.padding_style": "square" }"""
+    zpy.init(**init_kwargs)
+    dataset_config = zpy.DatasetConfig(sim_name="can_v7")
+    dataset_config.set("run\\.padding_style", "square")
+    print(dataset_config.config)
+    previews = zpy.preview(dataset_config)
+    urls = [preview["url"] for preview in previews]
+    print(json.dumps(urls, indent=4, sort_keys=True))
+
+
+def test_2(**init_kwargs):
+    """In local env, simruns do NOT exist for config { "run.padding_style": "messy" }"""
+    zpy.init(**init_kwargs)
+    dataset_config = zpy.DatasetConfig("can_v7")
+    # dataset_config.set("run\\.padding_style", "messy")
+    print(dataset_config.config)
+    previews = zpy.preview(dataset_config)
+    urls = [preview["url"] for preview in previews]
+    print(json.dumps(previews, indent=4, sort_keys=True))
+    print(json.dumps(urls, indent=4, sort_keys=True))
+
+
+def test_3(**init_kwargs):
+    """"""
+    zpy.init(**init_kwargs)
+    dataset_config = zpy.DatasetConfig("dumpster_v2")
+    # dataset_config.set("run\\.padding_style", "square")
+    zpy.generate("dumpster_v2.16", dataset_config, num_datapoints=8, materialize=True)
+
+
+if __name__ == "__main__":
+    init_kwargs = {
+        "base_url": "http://localhost:8000",
+        "project_uuid": "aad8e2b2-5431-4104-a205-dc3b638b0dab",
+        "auth_token": "214540cbd525f1ecf2bc52e2ddb7ef76801048e3f55aa4b33a9e501b115a736e",
+    }
+    # init_kwargs = {
+    #     "base_url": "https://ragnarok.stage.zumok8s.org",
+    #     "project_uuid": "91419af0-4815-41e7-9b77-5ef8154148c8",  # Zumo Internal
+    #     "auth_token": "a51cacaa01082ba5237b49f74cd6ffa5cf88339345383d97bcadd1f99e5f9a01",
+    # }
+    # init_kwargs = {
+    #     "base_url": "https://ragnarok.zumok8s.org",
+    #     "project_uuid": "91419af0-4815-41e7-9b77-5ef8154148c8",  # Compology
+    #     "auth_token": "7c1baae380c14a89b558a2fbf5f1c0ad923e61298c3ec87a0bdae6debbe549cb",
+    # }
+    # print("Running test_1:")
+    # test_1(**init_kwargs)
+    # print("Running test_2:")
+    # test_2(**init_kwargs)
+    print("Running test_3:")
+    test_3(**init_kwargs)

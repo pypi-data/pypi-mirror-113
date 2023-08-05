4d4b1d31815298a2491253173749044cfa915d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:56 +0000

    Add package tastyworks-cli/0.2.0/tastyworks_cli-0.2.0-py3-none-any.whl

---
+++ packages/tastyworks-cli/tastyworks_cli-0.2.0-py3-none-any.whl/src/app.py
@@ -0,0 +1,22 @@
+import click
+
+from .future.commands import future
+from .option.commands import option
+from .pairs.commands import pairs
+from .plot.commands import plot
+from .utils import VERSION
+
+
+@click.group()
+@click.version_option(VERSION)
+def app():
+    pass
+
+
+def main():
+    app.add_command(future)
+    app.add_command(option)
+    app.add_command(plot)
+    app.add_command(pairs)
+
+    app()
---
+++ packages/tastyworks-cli/tastyworks_cli-0.2.0-py3-none-any.whl/src/utils.py
@@ -0,0 +1,5 @@
+VERSION = '0.2.0'
+
+
+class TastyworksCLIError(Exception):
+    pass

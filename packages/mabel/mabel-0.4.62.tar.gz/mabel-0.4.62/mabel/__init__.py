c971ba0d1352791024cbc48d5c5c55dd93d9ca45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:54 +0000

    Add package mabel/0.4.62/mabel-0.4.62.tar.gz

---
+++ packages/mabel/mabel-0.4.62.tar.gz/mabel-0.4.62/mabel/__init__.py
@@ -0,0 +1,28 @@
+# python-dotenv allows us to create an environment file to store secrets. If
+# there is no .env it will fail gracefully and fall back to the actual os
+# environment.
+
+try:
+    import dotenv  # type:ignore
+except ImportError:  # pragma: no cover
+    dotenv = None  # type:ignore
+
+from pathlib import Path
+
+env_path = Path(".") / ".env"
+
+if env_path.exists() and dotenv is None:  # pragma: no cover
+    print("`.env` file exists but `dotEnv` not installed.")
+else:  # pragma: no cover
+    dotenv.load_dotenv(dotenv_path=env_path)
+
+import os
+from .version import __version__
+
+if os.environ.get("RESOURCE_MONITORING", False):  # pragma: no cover
+    from .utils.resource_monitoring import ResourceMonitor
+
+from .flows.flow import Flow
+from .flows.internals.base_operator import BaseOperator
+from .flows.internals.decorators import operator
+from .data.readers.reader import Reader

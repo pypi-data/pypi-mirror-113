4d00514b960fc82b978428ff2585355755994bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:03 +0000

    Add package jh-utils/0.0.18/jh_utils-0.0.18.tar.gz

---
+++ packages/jh-utils/jh_utils-0.0.18.tar.gz/jh_utils-0.0.18/jh_utils/sql/connection.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+import psycopg2
+
+def create_connection(database: str, user: str, password: str, host: str, port: str, sgbd = 'postgresql'):
+    """
+    Declare a db connection
+
+    Args:
+        database (str): database name
+        user (str): user
+        password (str): password
+        host (str): host
+        port (str): port
+        sgbd (str, optional): Defaults to 'postgresql'. Change the sgbd string in the db connection-string
+
+    Returns:
+        sql alchemy db engine: 
+    """    
+
+    ## Creating db string connection
+    if sgbd == 'postgresql':
+        con_string = 'postgresql://'+user+':'+password+'@'+host+':'+port+'/'+database
+    if sgbd == 'mysql':
+        con_string = 'mysql+pymysql://'+user+':'+password+'@'+host+':'+port+'/'+database
+    if sgbd == 'redshift':
+        con_string = f'redshift+psycopg2://{user}:{password}@{host}:{port}/{db}'
+    return create_engine(con_string)

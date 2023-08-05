e8ac89478801ebcc4573438da092e828abb0a32f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:47 +0000

    Add package spider-man-uas/1.0/spider_man_uas-1.0.tar.gz

---
+++ packages/spider-man-uas/spider_man_uas-1.0.tar.gz/spider_man_uas-1.0/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import  setup
+
+
+packages = ['spider_man_uas']
+
+setup(name='spider_man_uas',version='1.0',author='zhaojiafu', packages=packages, package_dir={'spider_man_uas': 'spider_man_uas'},)

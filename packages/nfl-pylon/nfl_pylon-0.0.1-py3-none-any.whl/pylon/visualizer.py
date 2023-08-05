1393466ab97bfa37c4125d5a7d5721d5856f4375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:13 +0000

    Add package nfl-pylon/0.0.1/nfl_pylon-0.0.1-py3-none-any.whl

---
+++ packages/nfl-pylon/nfl_pylon-0.0.1-py3-none-any.whl/pylon/visualizer.py
@@ -0,0 +1,19 @@
+# python 3.6
+# basic data viz
+import pandas as pd
+
+
+def build_histogram(series: list, **kwargs) -> None:
+	raise NotImplementedError("This function is not supported by this version of pylon. ")
+
+
+def build_scatterplot(x: pd.Series, y: pd.Series, **kwargs) -> None:
+	raise NotImplementedError("This function is not supported by this version of pylon. ")
+
+
+def build_hbar(x: pd.Series, y: pd.Series) -> None:
+	raise NotImplementedError("This function is not supported by this version of pylon. ")
+
+
+def build_vbar(x: pd.Series, y: pd.Series) -> None:
+	raise NotImplementedError("This function is not supported by this version of pylon. ")

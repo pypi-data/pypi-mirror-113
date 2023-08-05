493a6aaa7dc0075e201bfe693dd00cffad87d344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:28 +0000

    Add package crawl-web-page-script/2.0.0/crawl-web-page-script-2.0.0.tar.gz

---
+++ packages/crawl-web-page-script/crawl-web-page-script-2.0.0.tar.gz/crawl-web-page-script-2.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+     name="crawl-web-page-script",  
+     version="2.0.0",                          
+     py_modules=["webcrawl_pack"],
+     python_requires='>=3.6, <4',               
+)
---
+++ packages/crawl-web-page-script/crawl-web-page-script-2.0.0.tar.gz/crawl-web-page-script-2.0.0/webcrawl_pack.py
@@ -0,0 +1,66 @@
+import requests
+import sys
+import json
+import os
+import logging
+
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+
+class Webcrawling():
+
+    def __init__(self, username, password, endpoint='http://localhost:8000/'):
+        self.username = username
+        self.password = password
+        self.endpoint = endpoint
+        self.generate_access_token()
+
+    def generate_access_token(self):
+        params = {
+            "username": self.username,
+            "password": self.password
+        }
+        try:
+            endpoint = os.path.join(self.endpoint,"api/token/")
+            response = requests.post(endpoint, data=params)
+            response = response.json()
+            self.access_token = response['access']
+            return self.access_token
+        except Exception as E:
+            logging.error(f'An error as occured {E}')
+        
+
+    def create_webcrawl_url(self, url, interval):
+        params = {
+            "url": url,
+            "schedule_interval": interval,
+        }
+        try:
+            endpoint = os.path.join(self.endpoint,"submit_job/")
+            response = requests.post(endpoint, data=json.dumps(params),
+                                headers={'Content-Type':'application/json',
+                                        'Authorization': 'Bearer {}'.format(self.access_token)})
+            return response.json()
+        except Exception as E:
+            logging.error(f'An error as occured {E}')
+        
+    def fetch_job(self):
+        params = {
+            "id": self.job_id,
+        }
+        try:
+            endpoint = os.path.join(self.endpoint,"crawl_urls/")
+            response = requests.get(endpoint, data=params, headers={'Content-Type':'application/json',
+                                        'Authorization': 'Bearer {}'.format(self.access_token)})
+            return response.json()
+        except Exception as E:
+            logging.error(f'An error as occured {E}') 
+        
+if __name__=='__main__':
+    res = Webcrawling(username = "nami", password = "nami@123", endpoint='http://localhost:8000/')
+    url = sys.argv[1]
+    interval = sys.argv[2]
+    crawl_url_submit = res.create_webcrawl_url(url, interval)
+    print(crawl_url_submit)
+
+
+

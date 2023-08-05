e83e64507314335ae8527a6c1f982a447c7eacec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:01 +0000

    Add package saphyre/1.0.2/saphyre-1.0.2.tar.gz

---
+++ packages/saphyre/saphyre-1.0.2.tar.gz/saphyre-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    f1 = f.read()
+
+setup(
+    name='saphyre',
+    version='1.0.2',
+    description='A package for computational photography in Python.',
+    py_modules=['saphyre'],
+    install_requires=['Pillow>=7.2.0', 'opencv-python>=4.5.3.56'],
+    license='MIT',
+    package_dir={'': 'saphyre'},
+    long_description=f1,
+    long_description_content_type='text/markdown'
+)

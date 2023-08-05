b5f3b0eca98744e2c2d020ddd3be58a76d04e8a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:57 +0000

    Add package ez-distributions/1.0/ez_distributions-1.0.tar.gz

---
+++ packages/ez-distributions/ez_distributions-1.0.tar.gz/ez_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ez_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['ez_distributions'],
+      author = 'Giorgos Moysiadis',
+      author_mail = 'giorgosmoysiadis@gmail.com',
+      zip_safe=False)

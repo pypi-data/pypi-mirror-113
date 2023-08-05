7926925e4c88ae330747390bbe790bb552fd9a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:12 +0000

    Add package check-techstack-vulnerabilities/1.0.1/check-techstack-vulnerabilities-1.0.1.tar.gz

---
+++ packages/check-techstack-vulnerabilities/check-techstack-vulnerabilities-1.0.1.tar.gz/check-techstack-vulnerabilities-1.0.1/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+import setuptools
+
+#The directory containing this file
+BASE = pathlib.Path(__file__).parent
+
+# The text of the readme file
+README = (BASE / "README.md").read_text()
+
+#This call to setup does all the work
+setuptools.setup(
+    name="check-techstack-vulnerabilities",
+    version="1.0.1",
+    deecription="Bug in url fixed.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/devarajug/check-techstack-vulnerabilities",
+    author="Devaraju Garigapati",
+    author_email="devarajugarigapati@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=["pandas", "openpyxl", "lxml", "requests", "beautifulsoup4"],
+    python_requires='>=3.6'
+)

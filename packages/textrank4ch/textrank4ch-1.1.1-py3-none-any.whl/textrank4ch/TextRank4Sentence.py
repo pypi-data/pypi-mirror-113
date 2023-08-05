45f90b84345c66c30b0ad6830847741324052067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:32 +0000

    Add package textrank4ch/1.1.1/textrank4ch-1.1.1-py3-none-any.whl

---
+++ packages/textrank4ch/textrank4ch-1.1.1-py3-none-any.whl/textrank4ch/TextRank4Sentence.py
@@ -0,0 +1,170 @@
+from . import utils
+from segment import WordSegment, SentenceSegment
+
+class TextRank4Sentence(object):
+    """
+    提取关键句
+
+    Attributes:
+    ----------
+    sentences: list, 切分后的结果句子列表
+
+    allow_pos: list, 切词时保留的词性
+
+    path_stop_words: str, 停用词表路径
+
+    punct: list, 剔除的特殊字符列表
+
+    delimiters: list, 切句使用的特殊字符列表
+
+    ws: class object, 用于切词的类
+
+    ss: class object, 用于切句的类
+
+    words_no_filters: list, 分词结果, 不剔除停用词、不剔除词性、
+
+    words_stop_words_filters: list, 分词结果, 剔除停用词、不剔除词性
+
+    words_flags_filters: list, 分词结果, 不剔除停用词, 剔除词性
+
+    words_all_filters: list, 分词结果, 剔除停用词、剔除词性
+
+    key_sentences: list, 提取的全量句子以及其权重(PR值)
+    """
+    def __init__(self):
+        self.sentences = None
+        self.allow_pos = None
+        self.path_stop_words = None
+        self.punct = None
+        self.delimiters  = None
+        self.ws = None
+        self.ss = None
+
+        self.words_no_filters = None
+        self.words_stop_words_filters = None
+        self.words_flags_filters = None
+        self.words_all_filters = None
+
+        self.key_sentences = None
+
+    def analyze(self, text,
+                punct=utils.word_delimiters,
+                delimiters=utils.sentence_delimiters,
+                source='all_filters',
+                sim_func=utils.get_similarity,
+                path_stop_words=None,
+                allow_pos=utils.allow_pos,
+                is_lower=True):
+        """
+        基于TextRank提取全量的句子以及其权重(PR值)
+
+
+        Parameters:
+        ----------
+        text: str, 输入的文本
+
+        punct: list, 剔除的特殊字符列表
+
+        delimiters: list, 切句使用的特殊字符列表
+
+        source: str, 最后用于提取摘要句计算PR时用的分词结果
+
+        sim_func: function, 用于计算两个词列表的相似度函数, 支持用户自定义该函数, 格式为 func(list, list) -> float
+
+        path_stop_words: str, 停用词表路径
+
+        allow_pos: list, 切词时保留的词性
+
+        is_lower: bool, 是否要转小写字母
+
+        Returns:
+        -------
+
+
+        """
+        self.punct = punct
+        self.delimiters = delimiters
+        self.allow_pos = allow_pos
+        self.path_stop_words = path_stop_words
+
+        self.ws = WordSegment(path_stop_words=self.path_stop_words,
+                              allow_pos=self.allow_pos,
+                              punct=self.punct)
+
+        self.ss = SentenceSegment(delimiters=delimiters)
+
+        # 切分句子, 这里会输出一个列表, 一个列表包含一个句子
+        # PS:这里也处理了大小写转化
+        self.sentences = self.ss.segment(content=text, use_is_lower=is_lower)
+
+        # 对所有的句子开始切词, 其中要区分四种情况
+        ## 1.no_filters, 不剔除停用词、不剔除词性、
+        self.words_no_filters = self.ws.seg_sentences(sentences=self.sentences,
+                                                      use_stop_words=False,
+                                                      use_allow_pos=False)
+        ## 2.stop_words_filters, 剔除停用词、不剔除词性
+        self.words_stop_words_filters = self.ws.seg_sentences(sentences=self.sentences,
+                                                              use_stop_words=True,
+                                                              use_allow_pos=False)
+
+        ## 3.flags_filters, 不剔除停用词, 剔除词性
+        self.words_flags_filters = self.ws.seg_sentences(sentences=self.sentences,
+                                                         use_stop_words=True,
+                                                         use_allow_pos=False)
+
+        ## 4.all_filters, 剔除停用词、剔除词性
+        self.words_all_filters = self.ws.seg_sentences(sentences=self.sentences,
+                                                       use_stop_words=True,
+                                                       use_allow_pos=True)
+
+        # 基于source决定输入的分词列表
+        if source == 'no_filters':
+            _words = self.words_no_filters
+        elif source == 'stop_words_filters':
+            _words = self.words_stop_words_filters
+        elif source == 'flags_filters':
+            _words = self.words_flags_filters
+        else:
+            ## all_filters作为兜底
+            _words = self.words_all_filters
+
+        self.key_sentences = utils.sort_sentences(sentences=self.sentences,
+                                                  words=_words,
+                                                  sim_func=sim_func)
+
+    def get_key_sentences(self, top_k=5, sentences_min_len=5, with_weight=True):
+        """
+        筛选需要的topK摘要句
+
+        Parameters:
+        ----------
+        top_k: int, 获取topN权重的句子
+
+        sentences_min_len: int, 获取大于等于长度阈值的句子
+
+        with_weight: bool, 是否要带着权重(PR值)进行输出
+
+        Returns:
+        -------
+        摘要句列表
+
+        """
+        list_res = []
+
+        sentences = [item['sentence'] for item in self.key_sentences if len(item['sentence']) >= sentences_min_len]
+        weights = [item['weight'] for item in self.key_sentences if len(item['sentence']) >= sentences_min_len]
+
+        list_res.append(sentences)
+
+        if with_weight:
+            list_res.append(weights)
+
+        return list(zip(*list_res))[:top_k]
+
+
+
+
+
+
+
+
---
+++ packages/textrank4ch/textrank4ch-1.1.1-py3-none-any.whl/textrank4ch/utils.py
@@ -0,0 +1,135 @@
+import networkx as nx
+import numpy as np
+
+word_delimiters = ['!', '"', '#', '%', '&', '(', ')', '*', '+',
+                   ',', '-', '.', '...', '......', '/', ':', ';',
+                   '<', '=', '>', '?', '@', '[', ']', '^', '_',
+                   '`', '{', '|', '}', '~', '，', '。', '。。。',
+                   '。。。。。。', '！', '；', '？', '：', '、', '（', '）',
+                   '\t', '\n', '”', '’' , '“', '‘', "'"]
+
+sentence_delimiters = ['？', '?', '；', ';', '！', '!', '。', '……', '…', '\n']
+
+allow_pos = ['an', 'nz', 'vn', 'v', 'vd', 'x', 'n', 'nt', 'nz', 'd']
+
+def get_stop_words(path_stop_words):
+    """
+
+    Parameters:
+    ----------
+    path_stop_words: 停用词文件路径, 其中一行存储一个停用词
+
+    Returns:
+    -------
+    stop_words: list, 停用词列表
+
+    """
+    stop_words = set()
+    if path_stop_words:
+        with open(path_stop_words, 'r') as fr:
+            stop_words = set([word.strip().replace('\n', '') for word in fr.readlines()])
+
+    return stop_words
+
+def get_similarity(words1, words2):
+    """
+    计算句子之间的相似度
+
+    公式: similarity = |A∩B| / (log(|A|) + log(|B|))
+
+    Parameters:
+    ----------
+    words1: list, 词列表
+
+    words2: list, 词列表
+
+    Returns:
+    -------
+    sim_value: float, 句子之间的相似度
+    """
+
+    if len(words1) == 0 or len(words2) == 0:
+        sim_value = 0
+    else:
+        sim_value = float(len(set(words1).intersection(set(words2))) / (np.log(len(words1)) + np.log(len(words2))))
+
+    return sim_value
+
+
+def sort_sentences(sentences, words,
+                   sim_func=get_similarity,
+                   pagerank_config=None,
+                   pr_error_handle='iterator'):
+    f"""
+    基于TextRank方法对句子以及分词结果进行提取摘要
+
+    Parameters:
+    ----------
+    sentences: list, 切分后的结果句子列表
+
+    words: list, 切分后的结果词列表
+
+    pagerank_config: dict, networkx.pagerank的参数字典
+
+    pr_error_handle: str, pagerank不收敛的时候的处理策略, iterator增加迭代轮次（兜底）, tolerance增加迭代轮次前后的差值阈值
+
+    Returns:
+    -------
+    list_res: list, 结果列表
+    """
+    list_res = []
+
+    # 默认的PR收敛时的参数
+    pr_alpha = 1
+    pr_max_iter = 200
+    pr_tol = 1e-6
+
+    if pagerank_config is None:
+        pagerank_config = {'alpha': pr_alpha,
+                           'max_iter': pr_max_iter,
+                           'tol': pr_tol}
+
+    len_sentences = len(sentences)
+
+    # 初始化句子之间的无向权图, 整体为N*N的矩阵
+    graph = np.zeros((len_sentences, len_sentences))
+
+    # 计算权重, 权重由切词的相似度进行计算, 由于是无向的, a(ij) = a(ji)
+    for i in range(len_sentences):
+        for j in range(len_sentences):
+            sim_value = sim_func(words[i], words[j])
+            graph[i, j] = sim_value
+            graph[j, i] = sim_value
+
+    # 构造无向权图
+    nx_graph = nx.from_numpy_matrix(graph)
+
+    # 计算PR值, 注意, 初始参数在计算PR值时可能不收敛, 这个时候可以
+    flag = True
+
+    while flag:
+        # noinspection PyBroadException
+        try:
+            ## 开始计算PR值, 可能存在不收敛的情况
+            pr_values = nx.pagerank(nx_graph, **pagerank_config)
+            ## 成功收敛则停止循环
+            flag = False
+
+        except Exception:
+            ## 如果PR不收敛, 以提升迭代前后轮次之间的差值为策略
+            if pr_error_handle == 'tolerance':
+                pr_tol = min(pr_tol * 10, 0)
+            ## 否则以提升迭代轮次作为策略
+            else:
+                pr_max_iter += 50
+
+            pagerank_config = {'alpha': pr_alpha,
+                               'max_iter': pr_max_iter,
+                               'tol': pr_tol}
+
+    # pr_values: 一个dict, {index:pr, index:pr}
+    for idx, val in sorted(pr_values.items(), key=lambda x: x[1], reverse=True):
+        list_res.append({'sentence': sentences[idx], 'weight': val})
+
+    return list_res
+

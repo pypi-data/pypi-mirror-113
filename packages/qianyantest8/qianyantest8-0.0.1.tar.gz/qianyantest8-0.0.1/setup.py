f8c053466aec566a9f95fb0a761efe63d00d013c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:38 +0000

    Add package qianyantest8/0.0.1/qianyantest8-0.0.1.tar.gz

---
+++ packages/qianyantest8/qianyantest8-0.0.1.tar.gz/qianyantest8-0.0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="qianyantest8",
+    version="0.0.1",
+    license="MIT Licence",
+
+    url="",
+    author="",
+    author_email="",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)

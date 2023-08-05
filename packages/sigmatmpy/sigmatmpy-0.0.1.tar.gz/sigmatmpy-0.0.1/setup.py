5333daa08685ec0acfc8790caeeb07ed9ae32d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:53 +0000

    Add package sigmatmpy/0.0.1/sigmatmpy-0.0.1.tar.gz

---
+++ packages/sigmatmpy/sigmatmpy-0.0.1.tar.gz/sigmatmpy-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
+
+with open("README.md", "r", encoding="utf8") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="sigmatmpy",
+	version="0.0.1",
+	author="SigmaTM",
+	author_email="wuyou1020@hotmail.com",
+	description="BFJ manager api for python user",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/pypa/sampleproject",
+	packages=setuptools.find_packages(),
+	classifiers=[
+	"Programming Language :: Python :: 3",
+	"License :: OSI Approved :: MIT License",
+	"Operating System :: OS Independent",
+	],
+)

8670e6f2151f696d67434c05f0ff4a0cd7971b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:41 +0000

    Add package BetterSockets/0.0.1a2/BetterSockets-0.0.1a2.tar.gz

---
+++ packages/BetterSockets/BetterSockets-0.0.1a2.tar.gz/BetterSockets-0.0.1a2/BetterSockets/__init__.py
@@ -0,0 +1,8 @@
+# Version, Author
+__version__ = "0.0.1a2"
+__author__ = "Luca Michael Schmidt"
+__license__ = "MIT"
+
+# importing of the classes
+from .Client import Client
+from .Server import HubServer, ProcessorServer

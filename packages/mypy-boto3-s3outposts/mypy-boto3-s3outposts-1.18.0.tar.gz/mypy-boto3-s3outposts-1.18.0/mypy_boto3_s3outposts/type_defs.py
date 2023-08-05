0f45c701c628b58cb9c92f5212c35a3ad610b973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:06 +0000

    Add package mypy-boto3-s3outposts/1.18.0/mypy-boto3-s3outposts-1.18.0.tar.gz

---
+++ packages/mypy-boto3-s3outposts/mypy-boto3-s3outposts-1.18.0.tar.gz/mypy-boto3-s3outposts-1.18.0/mypy_boto3_s3outposts/type_defs.py
@@ -0,0 +1,121 @@
+"""
+Type annotations for s3outposts service type definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_s3outposts/type_defs.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_s3outposts.type_defs import CreateEndpointRequestRequestTypeDef
+
+    data: CreateEndpointRequestRequestTypeDef = {...}
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import Any, Dict, List
+
+from .literals import EndpointStatusType
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+__all__ = (
+    "CreateEndpointRequestRequestTypeDef",
+    "CreateEndpointResultTypeDef",
+    "DeleteEndpointRequestRequestTypeDef",
+    "EndpointTypeDef",
+    "ListEndpointsRequestRequestTypeDef",
+    "ListEndpointsResultTypeDef",
+    "NetworkInterfaceTypeDef",
+    "PaginatorConfigTypeDef",
+    "ResponseMetadataTypeDef",
+)
+
+CreateEndpointRequestRequestTypeDef = TypedDict(
+    "CreateEndpointRequestRequestTypeDef",
+    {
+        "OutpostId": str,
+        "SubnetId": str,
+        "SecurityGroupId": str,
+    },
+)
+
+CreateEndpointResultTypeDef = TypedDict(
+    "CreateEndpointResultTypeDef",
+    {
+        "EndpointArn": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+DeleteEndpointRequestRequestTypeDef = TypedDict(
+    "DeleteEndpointRequestRequestTypeDef",
+    {
+        "EndpointId": str,
+        "OutpostId": str,
+    },
+)
+
+EndpointTypeDef = TypedDict(
+    "EndpointTypeDef",
+    {
+        "EndpointArn": str,
+        "OutpostsId": str,
+        "CidrBlock": str,
+        "Status": EndpointStatusType,
+        "CreationTime": datetime,
+        "NetworkInterfaces": List["NetworkInterfaceTypeDef"],
+    },
+    total=False,
+)
+
+ListEndpointsRequestRequestTypeDef = TypedDict(
+    "ListEndpointsRequestRequestTypeDef",
+    {
+        "NextToken": str,
+        "MaxResults": int,
+    },
+    total=False,
+)
+
+ListEndpointsResultTypeDef = TypedDict(
+    "ListEndpointsResultTypeDef",
+    {
+        "Endpoints": List["EndpointTypeDef"],
+        "NextToken": str,
+        "ResponseMetadata": "ResponseMetadataTypeDef",
+    },
+)
+
+NetworkInterfaceTypeDef = TypedDict(
+    "NetworkInterfaceTypeDef",
+    {
+        "NetworkInterfaceId": str,
+    },
+    total=False,
+)
+
+PaginatorConfigTypeDef = TypedDict(
+    "PaginatorConfigTypeDef",
+    {
+        "MaxItems": int,
+        "PageSize": int,
+        "StartingToken": str,
+    },
+    total=False,
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, Any],
+        "RetryAttempts": int,
+    },
+)

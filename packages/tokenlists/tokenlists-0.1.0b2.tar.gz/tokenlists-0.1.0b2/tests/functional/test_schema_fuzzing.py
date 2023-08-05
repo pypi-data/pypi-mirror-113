9f8a1faa1af7f03fd0fcfd95a764d8460b6064db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:31 +0000

    Add package tokenlists/0.1.0b2/tokenlists-0.1.0b2.tar.gz

---
+++ packages/tokenlists/tokenlists-0.1.0b2.tar.gz/tokenlists-0.1.0b2/tests/functional/test_schema_fuzzing.py
@@ -0,0 +1,15 @@
+import pytest
+import requests  # type: ignore
+from hypothesis import HealthCheck, given, settings
+from hypothesis_jsonschema import from_schema
+
+from tokenlists import TokenList
+
+TOKENLISTS_SCHEMA = "https://uniswap.org/tokenlist.schema.json"
+
+
+@pytest.mark.fuzzing
+@given(token_list=from_schema(requests.get(TOKENLISTS_SCHEMA).json()))
+@settings(suppress_health_check=(HealthCheck.too_slow,))
+def test_schema(token_list):
+    assert TokenList.from_dict(token_list).to_dict() == token_list

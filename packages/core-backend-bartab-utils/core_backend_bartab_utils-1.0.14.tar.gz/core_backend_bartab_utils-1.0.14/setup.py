51e10bf8506f57c7ae3213326c94c951c59eb7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:18 +0000

    Add package core-backend-bartab-utils/1.0.14/core_backend_bartab_utils-1.0.14.tar.gz

---
+++ packages/core-backend-bartab-utils/core_backend_bartab_utils-1.0.14.tar.gz/core_backend_bartab_utils-1.0.14/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='core_backend_bartab_utils',
+    version='1.0.14',
+    description='Common utils files used for any python3 BarTab repository',
+    url='https://github.com/BarTabPayments/core_backend_bartab_utils',
+    author='Cassio Hudson',
+    author_email='cassio@bartabapp.co',
+    license='BSD 2-clause',
+    packages=['bartab_core'],
+    install_requires=['mpi4py>=2.0',
+                      'numpy',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)

97f980dea7565a05f5bf590e2ef691fc28899af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:51 +0000

    Add package asheembasiccalculator/0.0.1/asheembasiccalculator-0.0.1.tar.gz

---
+++ packages/asheembasiccalculator/asheembasiccalculator-0.0.1.tar.gz/asheembasiccalculator-0.0.1/README.md
@@ -0,0 +1,5 @@
+# Calculator
+
+### A very simple calculator, and using it as base to learn how to publish to pypi
+
+Author: Asheem Chhetri
---
+++ packages/asheembasiccalculator/asheembasiccalculator-0.0.1.tar.gz/asheembasiccalculator-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='asheembasiccalculator',
+    version='0.0.1',
+    description='A very basic calculator',
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Asheem Chhetri',
+    author_email='achhetri@alumni.purdue.edu',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='calculator',
+    packages=find_packages(),
+    install_requires=['']
+)

4e8f9c3b90cbb31b402d6ad2dce5419759da9710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:22 +0000

    Add package awspy/0.4.0/awspy-0.4.0.tar.gz

---
+++ packages/awspy/awspy-0.4.0.tar.gz/awspy-0.4.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="awspy",
+    version="0.4.0",
+    author="ScholarPack",
+    author_email="dev@scholarpack.com",
+    description="Utility tools for running Python services in AWS.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ScholarPack/awspy",
+    packages=["awspy", "awspy.ecs"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=["requests >= 2.26.0", "types-requests >= 2.25.0"],
+)

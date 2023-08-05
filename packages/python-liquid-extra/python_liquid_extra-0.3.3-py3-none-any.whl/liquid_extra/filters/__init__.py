ec54beccfbb80e49f077a7f380db1c0041d71dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:30 +0000

    Add package python-liquid-extra/0.3.3/python_liquid_extra-0.3.3-py3-none-any.whl

---
+++ packages/python-liquid-extra/python_liquid_extra-0.3.3-py3-none-any.whl/liquid_extra/filters/__init__.py
@@ -0,0 +1,9 @@
+from .additional import JSON
+from .additional import Translate
+from .array import Index
+from .html import StylesheetTag
+from .html import ScriptTag
+
+from .array import index
+from .html import stylesheet_tag
+from .html import script_tag

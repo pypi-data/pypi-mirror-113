358e3153c9118d2b6e55a8d8b0c9e465e866f914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:33 +0000

    Add package logt/0.1.0/logt-0.1.0.tar.gz

---
+++ packages/logt/logt-0.1.0.tar.gz/logt-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='logt',
+    packages=find_packages(include=['logt']),
+    version='0.1.0',
+    description='logt launching soon',
+    author='Soham Chakraborty',
+    license='AGPLv3',
+    install_requires=[],
+)

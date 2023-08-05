9e73d98d631e6cc7d6faf049e9f8a1b3aecdab44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:26 +0000

    Add package pluscodepy/0.0.5/pluscodepy-0.0.5-py3-none-any.whl

---
+++ packages/pluscodepy/pluscodepy-0.0.5-py3-none-any.whl/pluscodepy/pluscodepy.py
@@ -0,0 +1,28 @@
+import json
+import re
+import openlocationcode.openlocationcode as olc
+
+class Converter:
+    def __init__(self, cities_file):
+        self.cities = json.load(cities_file)
+
+    def decode(self, pluscode):
+        match = re.match('^(\S*\S{2})\s*(.*?),\s*(\S*)$', pluscode)
+        if match is None:
+            return False
+        if len(match.groups()) > 1:
+            fullcode = self.convertShortcodeToFull(match.group(1), match.group(2))
+        else:
+            fullcode = pluscode
+        if not fullcode:
+            return False
+        decoded = olc.decode(fullcode)
+        return (decoded.latitudeCenter, decoded.longitudeCenter)
+
+    def convertShortcodeToFull(self, pluscode, city):
+        city = [item for item in self.cities if item['name'] == city]
+        if len(city) == 0:
+            return False
+        city = city.pop()
+        city_prefix = olc.encode(city['latitude'], city['longitude'])[0:4]
+        return f'{city_prefix}{pluscode}'

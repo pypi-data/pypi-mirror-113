60d610330a063e6326d6eb11590bc3dec5f42e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:17 +0000

    Add package tradernet-client/0.5.0/tradernet_client-0.5.0.tar.gz

---
+++ packages/tradernet-client/tradernet_client-0.5.0.tar.gz/tradernet_client-0.5.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tradernet_client']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.8.2,<2.0.0', 'requests>=2.25.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'tradernet-client',
+    'version': '0.5.0',
+    'description': '',
+    'long_description': None,
+    'author': 'roci',
+    'author_email': 'rocinantt@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

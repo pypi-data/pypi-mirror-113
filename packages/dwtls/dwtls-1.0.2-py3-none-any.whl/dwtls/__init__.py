4c8ef54ad2dec15284514b10c8919b6ea9dd5c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:27 +0000

    Add package dwtls/1.0.2/dwtls-1.0.2-py3-none-any.whl

---
+++ packages/dwtls/dwtls-1.0.2-py3-none-any.whl/dwtls/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) Tomohiko Nakamura
+# All rights reserved.
+"""Library of downsampling/pooling layers based on discrete wavelet transforms (DWTs)
+
+https://github.com/TomohikoNakamura/dwtls
+"""

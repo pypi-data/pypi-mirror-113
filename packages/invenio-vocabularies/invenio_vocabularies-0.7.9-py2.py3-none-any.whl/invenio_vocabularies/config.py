c41bfe14037c2b12ad9bef839d24a0a8eed391fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:43 +0000

    Add package invenio-vocabularies/0.7.9/invenio_vocabularies-0.7.9-py2.py3-none-any.whl

---
+++ packages/invenio-vocabularies/invenio_vocabularies-0.7.9-py2.py3-none-any.whl/invenio_vocabularies/config.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+# Copyright (C) 2021 Northwestern University.
+#
+# Invenio-Vocabularies is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Vocabularies configuration."""
+
+from .resources.resource import VocabulariesResourceConfig
+from .services.service import VocabulariesServiceConfig
+
+VOCABULARIES_RESOURCE_CONFIG = VocabulariesResourceConfig
+"""Configure the resource."""
+
+VOCABULARIES_SERVICE_CONFIG = VocabulariesServiceConfig
+"""Configure the service."""
+
+VOCABULARIES_AFFILIATION_SCHEMES = [
+    ("grid", lambda x: True),
+    "gnd",
+    "isni",
+    "ror"
+]
+"""Affiliations allowed identifier schemes."""

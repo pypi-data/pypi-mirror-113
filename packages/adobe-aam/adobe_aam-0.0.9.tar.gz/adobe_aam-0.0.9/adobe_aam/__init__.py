107d8b00d4729a2fefcc2d14849b0d2aa66da189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:46 +0000

    Add package adobe-aam/0.0.9/adobe_aam-0.0.9.tar.gz

---
+++ packages/adobe-aam/adobe_aam-0.0.9.tar.gz/adobe_aam-0.0.9/adobe_aam/__init__.py
@@ -0,0 +1,32 @@
+import os
+
+# Let users know if they're missing any of our hard dependencies
+hard_dependencies = ("datetime",
+                     "json",
+                     "jwt",
+                     "cryptography",
+                     "os",
+                     "pandas",
+                     "requests"
+                     )
+
+missing_dependencies = []
+
+for dependency in hard_dependencies:
+    try:
+        __import__(dependency)
+    except ImportError as e:
+        missing_dependencies.append(f"{dependency}: {e}")
+
+if missing_dependencies:
+    raise ImportError(
+        "Unable to import required dependencies:\n" + "\n".join(missing_dependencies)
+    )
+del hard_dependencies, dependency, missing_dependencies
+
+from .login.login import *
+from .traits.traits import *
+from .segments.segments import *
+from .reports.reports import *
+from .traitFolders.traitFolders import *
+from .segmentFolders.segmentFolders import *

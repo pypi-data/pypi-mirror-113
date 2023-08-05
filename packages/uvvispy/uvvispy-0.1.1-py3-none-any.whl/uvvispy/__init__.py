69c6ab4871dc3858ed06c26f72ff42907b28b8e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:13 +0000

    Add package uvvispy/0.1.1/uvvispy-0.1.1-py3-none-any.whl

---
+++ packages/uvvispy/uvvispy-0.1.1-py3-none-any.whl/uvvispy/__init__.py
@@ -0,0 +1,25 @@
+"""UVVisPy package.
+
+Tools for reproducible and traceable analysis of UV-visible (UVVis) data.
+
+Available modules
+-----------------
+:mod:`uvvispy.dataset`
+    Unit containing data and metadata
+
+:mod:`uvvispy.metadata`
+    Metadata contained in the datasets
+
+:mod:`uvvispy.io`
+    Import and export data into and from datasets
+
+:mod:`uvvispy.processing`
+    Processing steps operating on datasets
+
+:mod:`uvvispy.analysis`
+    Analysis steps operating on datasets
+
+:mod:`uvvispy.plotting`
+    Graphical representation of data in datasets
+
+"""

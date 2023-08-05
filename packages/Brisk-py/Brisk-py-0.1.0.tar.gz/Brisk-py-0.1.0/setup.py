59339924412a8565826c5280c9fabfff8d2ee032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:12 +0000

    Add package Brisk-py/0.1.0/Brisk-py-0.1.0.tar.gz

---
+++ packages/Brisk-py/Brisk-py-0.1.0.tar.gz/Brisk-py-0.1.0/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+gh = 'https://github.com/briskpy/brisk'
+
+setuptools.setup(
+    name='Brisk-py',
+    version='0.1.0',
+    author='Reet Singh',
+    author_email='reet22singh+brisk@gmail.com',
+    description='A way to create a lightning fast HTTP server using ASGI',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=gh,
+    project_urls={
+        'Bug Tracker': f'{gh}/issues',
+        'Documentation': f'{gh}'
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    package_dir={'': 'src'},
+    python_requires='>=3.7'
+)

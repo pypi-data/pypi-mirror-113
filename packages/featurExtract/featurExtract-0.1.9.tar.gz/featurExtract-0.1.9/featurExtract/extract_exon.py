968c29ba11dbcd347fa93132f9861498abe7fff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:35 +0000

    Add package featurExtract/0.1.9/featurExtract-0.1.9.tar.gz

---
+++ packages/featurExtract/featurExtract-0.1.9.tar.gz/featurExtract-0.1.9/featurExtract/extract_exon.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import sys
+import gffutils
+import pandas as pd 
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from collections import defaultdict
+from featurExtract.util import utr3_type, utr5_type, mRNA_type
+
+
+def get_exon(args):
+    '''
+    parameters:
+     
+    '''
+    db = gffutils.FeatureDB(args.database, keep_order=True) # load database
+    exon_seq = pd.DataFrame(columns=['TranscriptID','Chrom','Start','End','Strand','Exon']) # header
+    mRNA_str = mRNA_type(args.style)
+    if args.transcript:
+        # return a specific transcript
+        out = [] 
+        for t in db.features_of_type(mRNA_str, order_by='start'):
+            if args.transcript in t.id:
+                # exon
+                exon_index = 1
+                for e in db.children(t, featuretype='exon', order_by='start'):
+                    exon = e.sequence(args.genome, use_strand=False) # 不反向互补，对于负链要得到全部的cds后再一次性反向互补
+                    exon = Seq(exon)
+                    if t.strand == '-':
+                        exon = exon.reverse_complement()
+                    exonRecord = SeqRecord(exon,id=args.transcript, 
+                                 description='strand %s exon %d start %d end %d length=%d'%(t.strand, 
+                                             exon_index, e.start, e.end, len(exon)))
+                    out.append(exonRecord)
+                    exon_index += 1
+                break 
+        if not args.print:
+            SeqIO.write(out, args.output, "fasta")
+        else:
+            SeqIO.write(out, sys.stdout, "fasta")
+    else:
+        print('transcript id needed!')
+        sys.exit(1)
+        
---
+++ packages/featurExtract/featurExtract-0.1.9.tar.gz/featurExtract-0.1.9/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import os
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+from setuptools import setup 
+from featurExtract.version import __version__
+
+PACKAGES = [
+    "featurExtract"
+]
+setup(
+    name='featurExtract',
+    version=__version__,
+    keywords='genome feature, extract',
+    description='Extract genome ferature sequence for biologists',
+    long_description_content_type='text/markdown',
+    long_description=readme(),
+    entry_points = {'console_scripts': [
+                       'featurExtract=featurExtract.command_gff:main',
+                       'genBankExtract=featurExtract.command_gb:main'
+                   ]},
+    author='zhusitao',
+    author_email='zhusitao1990@163.com',
+    url='https://github.com/SitaoZ/featurExtract.git',
+    include_package_data=True, # done via MANIFEST.in under setuptools
+    packages=PACKAGES,
+    license='MIT',
+    install_requires = ['argparse>=1.1', 
+                        'pandas>=1.2.4', 
+                        'gffutils>=0.10.1',
+                        'setuptools>=49.2.0',
+                        'biopython>=1.78'],
+    python_requires=">=3.7.6")

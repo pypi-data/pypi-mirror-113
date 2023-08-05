31c3a8367045c845a738a72d3f76c8fc2c8817fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:56 +0000

    Add package tastyworks-cli/0.2.0/tastyworks-cli-0.2.0.tar.gz

---
+++ packages/tastyworks-cli/tastyworks-cli-0.2.0.tar.gz/tastyworks-cli-0.2.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+from src.utils import VERSION
+
+
+f = open('README.md', 'r')
+LONG_DESCRIPTION = f.read()
+f.close()
+
+setup(
+    name='tastyworks-cli',
+    version=VERSION,
+    description='An easy-to-use command line interface for Tastyworks!',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author='Graeme Holliday',
+    author_email='graeme.holliday@pm.me',
+    url='https://github.com/Graeme22/tastyworks-cli/',
+    license='MIT',
+    install_requires=[
+        'click>=8.0.1',
+        'tastyworks==4',
+        'matplotlib>=3.4.2',
+        'pandas>=1.2.4',
+        'QuantLib>=1.21',
+        'python-dateutil>=2.8.1'
+    ],
+    packages=find_packages(exclude=['ez_setup', 'tests*']),
+    include_package_data=True,
+    entry_points="""
+        [console_scripts]
+        tw = src.app:main
+    """,
+)

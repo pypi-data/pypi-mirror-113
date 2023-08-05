29c738a002aafcf09ce9987184a2edbb514caaf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:23 +0000

    Add package mikes-toolbox/0.0.3/mikes_toolbox-0.0.3.tar.gz

---
+++ packages/mikes-toolbox/mikes_toolbox-0.0.3.tar.gz/mikes_toolbox-0.0.3/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='mikes_toolbox',
+    packages=['mikes_toolbox'],
+    version='0.0.3',
+    license='MIT',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Mike Huls',
+    author_email='mike_huls@hotmail.com',
+    url='https://github.com/Muls/toolbox_public',
+    project_urls = {
+        "Bug Tracker": "https://github.com/Muls/toolbox/issues"
+    },
+    install_requires=['requests'],
+
+    download_url="https://github.com/mike-huls/toolbox_public/archive/refs/tags/0.0.3.tar.gz",
+    keywords=["pypi", "mikes_toolbox", "tutorial"],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Documentation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
+

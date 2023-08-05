0403fb696e41e9238af26f01833f2ad960d7df7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:36 +0000

    Add package JaviMartinez/1.1/JaviMartinez-1.1.tar.gz

---
+++ packages/JaviMartinez/JaviMartinez-1.1.tar.gz/JaviMartinez-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+setup(
+    name='JaviMartinez',
+    version='1.1',
+    packages=find_packages(),
+    py_modules=['ExploratoryDataAnalysis','FeatureEngineering','FeatureSelection']
+)

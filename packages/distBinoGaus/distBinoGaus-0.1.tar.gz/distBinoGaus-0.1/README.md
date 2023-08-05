d3bd94aecb0b3424d9df09e130918e2a2b6f2d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:52 +0000

    Add package distBinoGaus/0.1/distBinoGaus-0.1.tar.gz

---
+++ packages/distBinoGaus/distBinoGaus-0.1.tar.gz/distBinoGaus-0.1/README.md
@@ -0,0 +1,3 @@
+Distributions statistic package
+-Gaussian
+-Binomial
---
+++ packages/distBinoGaus/distBinoGaus-0.1.tar.gz/distBinoGaus-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distBinoGaus',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['distBinoGaus'],
+      zip_safe=False)

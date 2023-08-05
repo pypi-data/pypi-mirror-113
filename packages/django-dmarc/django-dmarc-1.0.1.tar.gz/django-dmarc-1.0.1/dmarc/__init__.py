c7eb38c0b1d9022c770b14452b394c2d34d1165a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:37 +0000

    Add package django-dmarc/1.0.1/django-dmarc-1.0.1.tar.gz

---
+++ packages/django-dmarc/django-dmarc-1.0.1.tar.gz/django-dmarc-1.0.1/dmarc/__init__.py
@@ -0,0 +1,15 @@
+#----------------------------------------------------------------------
+# Copyright (c) 2015-2021, Persistent Objects Ltd https://p-o.co.uk/
+#
+# License: BSD
+#----------------------------------------------------------------------
+
+"""
+DMARC models for managing Aggregate Reports
+http://dmarc.org/resources/specification/
+"""
+
+__version__ = '1.0.1'
+__author__ = 'Alan Hicks'
+
+default_app_config = 'dmarc.apps.DmarcConfig'

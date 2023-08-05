655e97e05099be29375a0eca73e5eaac32ea3620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:14 +0000

    Add package binary-fractions/1.0.0/binary_fractions-1.0.0.tar.gz

---
+++ packages/binary-fractions/binary_fractions-1.0.0.tar.gz/binary_fractions-1.0.0/binary_fractions/binary_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Testing the Binary class and module."""
+
+from binary import TestTwosComplement
+from binary import TestBinary
+
+TestTwosComplement().selftest()  # run self-test test cases
+TestBinary().selftest()  # run self-test test cases
+
+# End of file
---
+++ packages/binary-fractions/binary_fractions-1.0.0.tar.gz/binary_fractions-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+classifiers = []
+
+setup(
+    name="binary_fractions",
+    version="1.0.0",
+    description="A Python package for floating-point binary fractions. Do math in base 2!",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jonny-exe/binary-fractions",
+    author="Jonny-exe",
+    author_email="",
+    license="GPL3+",
+    classifiers=classifiers,
+    keywords="binary fractions python python3 floating-point high-precision base-2 floating-point-arithmetic binary-fractions twos-complement",
+    install_requires=[""],
+    packages=["binary_fractions"]
+)

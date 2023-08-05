eccb1a3641b71b303b97a2a5c4c741ee6c589cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:37 +0000

    Add package ert/2.25.1/ert-2.25.1-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/ert/ert-2.25.1-cp37-cp37m-macosx_10_9_x86_64.whl/ert3/config/_ensemble_config.py
@@ -0,0 +1,47 @@
+import sys
+from typing import Tuple, Optional, Dict, Any
+from pydantic import BaseModel, ValidationError
+
+import ert3
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+class _EnsembleConfig(BaseModel):
+    class Config:
+        validate_all = True
+        validate_assignment = True
+        extra = "forbid"
+        allow_mutation = False
+        arbitrary_types_allowed = True
+
+
+class ForwardModel(_EnsembleConfig):
+    stage: str
+    driver: Literal["local", "pbs"] = "local"
+
+
+class Input(_EnsembleConfig):
+    source: str
+    record: str
+
+
+class Output(_EnsembleConfig):
+    record: str
+
+
+class EnsembleConfig(_EnsembleConfig):
+    forward_model: ForwardModel
+    input: Tuple[Input, ...]
+    output: Tuple[Output, ...]
+    size: Optional[int] = None
+
+
+def load_ensemble_config(config_dict: Dict[str, Any]) -> EnsembleConfig:
+    try:
+        return EnsembleConfig(**config_dict)
+    except ValidationError as err:
+        raise ert3.exceptions.ConfigValidationError(str(err), source="ensemble")

8da1563e11af8dd191de1a85e1c6d2b5de0d4732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:15 +0000

    Add package nepse-api/1.1.8/nepse_api-1.1.8-py3-none-any.whl

---
+++ packages/nepse-api/nepse_api-1.1.8-py3-none-any.whl/nepse/__init__.py
@@ -0,0 +1,7 @@
+from .core import Client
+
+__package_name__ = "nepse-api"
+__package_description__ = "This is a API wrapper for NEPSE API."
+
+__author__ = "Samrid Pandit"
+__author_email__ = "samrid.pandit@gmail.com"

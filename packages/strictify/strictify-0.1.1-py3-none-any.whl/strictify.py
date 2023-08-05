a97ab926e07435adb9f113e1470f1dd35c498f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:30 +0000

    Add package strictify/0.1.1/strictify-0.1.1-py3-none-any.whl

---
+++ packages/strictify/strictify-0.1.1-py3-none-any.whl/strictify.py
@@ -0,0 +1,24 @@
+def strict(func):
+    def wrapper(*args, **kwargs):
+        del_array = []
+
+        for (name, type_), j in zip(func.__annotations__.items(), args):
+            if type_ != type(j):
+                raise TypeError()
+            else:
+                del_array.append(name)
+        for name in del_array:
+            func.__annotations__.pop(name)
+
+        for (name, type_), (key, value) in zip(func.__annotations__.items(), kwargs.items()):
+            if type_ != type(value):
+                raise TypeError()
+
+        res = func(*args, **kwargs)
+
+        if 'return' in func.__annotations__.keys() and type(res) != func.__annotations__['return']:
+            raise TypeError()
+
+        return res
+
+    return wrapper

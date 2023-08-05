137439c9751d308db2d53cdacea4154476b254bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:01 +0000

    Add package logi/1.0/logi-1.0.tar.gz

---
+++ packages/logi/logi-1.0.tar.gz/logi-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='logi',
+      version='1.0',
+      description='a loging library',
+      packages=['logi'],
+      author_email='mishptitsin@yandex.ru',
+      zip_safe=False)

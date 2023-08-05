66ac68410e5de044bf25f481cdd5e0a86d1d9c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:15 +0000

    Add package persontitles/0.2.7/persontitles-0.2.7.tar.gz

---
+++ packages/persontitles/persontitles-0.2.7.tar.gz/persontitles-0.2.7/src/persontitles/degrees/academic_degrees.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# academic_degrees.py
+"""Collection of academic degrees."""
+import json
+import os
+# import pkgutil
+import sys
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(
+    os.path.realpath(os.path.join(os.getcwd(),
+    os.path.expanduser(__file__))),
+)  # isort:skip # noqa # pylint: disable=wrong-import-position
+sys.path.append(
+    os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)),
+)  # isort: skip # noqa # pylint: disable=wrong-import-position
+
+from degrees.academic_german import degrees_ger  # noqa
+from degrees.academic_german import german_abbrevs  # noqa
+from degrees.academic_uk import degrees_uk  # noqa
+from degrees.academic_us import degrees_us  # noqa
+
+
+def degrees() -> dict:
+    print(os.getcwd())
+    try:
+        with open('./data/degrees.json', mode='r', encoding='utf-8') as fin:  # noqa
+            DEGREES = json.load(fin)
+    except FileNotFoundError:
+        DEGREES = collect_degrees()
+        with open('./data/degrees.json', mode='w', encoding='utf-8') as fout:  # noqa
+            json.dump(DEGREES, fout)
+
+    return DEGREES
+
+
+def collect_degrees():
+    DEGREES = dict()
+    degrees = []
+
+    for degree in degrees_ger():
+        degrees.append(degree)
+    DEGREES['D'] = degrees
+
+    degrees = []
+    degrees_d = degrees_ger()
+    for degree in german_abbrevs(degrees_d):
+        degrees.append(degree)
+    DEGREES['german_abbrevs'] = degrees
+
+    degrees = []
+    for degree in degrees_uk():
+        degrees.append(degree)
+    DEGREES['UK'] = degrees
+
+    degrees = []
+    for degree in degrees_us():
+        degrees.append(degree)
+    DEGREES['US'] = degrees
+
+    return DEGREES
+
+
+if __name__ == '__main__':
+    DEGREES = collect_degrees()
+    for k, v in DEGREES.items():
+        print(k)
+        print(v)
+        print()
+
+    print(os.getcwd())

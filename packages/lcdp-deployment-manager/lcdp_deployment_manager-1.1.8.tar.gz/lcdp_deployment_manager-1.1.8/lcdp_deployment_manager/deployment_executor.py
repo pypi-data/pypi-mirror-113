f27ad217a79706a6274103d142a0f0b7c19866b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:00 +0000

    Add package lcdp-deployment-manager/1.1.8/lcdp_deployment_manager-1.1.8.tar.gz

---
+++ packages/lcdp-deployment-manager/lcdp_deployment_manager-1.1.8.tar.gz/lcdp_deployment_manager-1.1.8/lcdp_deployment_manager/deployment_executor.py
@@ -0,0 +1,18 @@
+from .constant import *
+
+
+# Démarre tous les services d'un environement et attend qu'il soit entièrement up
+def start_environment_and_wait_for_health(environment):
+    environment.start_up_services()
+    environment.wait_for_services_health()
+
+
+# Passe d'un environnement à l'autre en modifiant les targets groups des règles du listener
+def do_balancing(deployment_manager, from_environment, to_environment):
+    print("Do balancing from environment {} to environment {}".format(from_environment.color, to_environment.color))
+    deployment_manager.modify_listener_default_target_group(to_environment.default_target_group_arn)
+    deployment_manager.update_rule_target_group(
+        expected_rule_type=TARGET_GROUP_DEFAULT_TYPE,
+        expected_rule_color=from_environment.color,
+        new_target_group_arn=to_environment.default_target_group_arn
+    )

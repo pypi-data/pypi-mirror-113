19dcdb166f9cb4054db826a0d478db94873be72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:40 +0000

    Add package mask/1.0.0/mask-1.0.0.tar.gz

---
+++ packages/mask/mask-1.0.0.tar.gz/mask-1.0.0/setup.py
@@ -0,0 +1,25 @@
+# !/usr/local/python/bin/python
+# -*- coding: utf-8 -*-
+# (C) Wu Dong, 2021
+# All rights reserved
+# @Author: 'Wu Dong <wudong@eastwu.cn>'
+# @Time: '5/26/21 10:06 AM'
+# sys
+from setuptools import setup
+
+
+setup(
+    name="mask",
+    install_requires=[
+        "click>=8.0.1",
+        "grpcio>=1.38.0",
+        "protobuf==3.16.0",
+        "grpcio-tools>=1.38.0",
+        "grpcio-reflection>=1.38.0",
+    ],
+    extras_require={
+        "health": [
+            "grpcio-health-checking>=1.38.0"
+        ]
+    }
+)

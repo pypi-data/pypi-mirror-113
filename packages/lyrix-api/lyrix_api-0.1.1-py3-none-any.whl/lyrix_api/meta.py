f17d18e9618bc1764cf65774344c5e1e78c24d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:19 +0000

    Add package lyrix-api/0.1.1/lyrix_api-0.1.1-py3-none-any.whl

---
+++ packages/lyrix-api/lyrix_api-0.1.1-py3-none-any.whl/lyrix_api/meta.py
@@ -0,0 +1,16 @@
+from typing import NamedTuple, Optional
+
+
+class Song:
+    def __init__(
+        track: str,
+        artist: str,
+        source: Optional[str],
+        url: Optional[str],
+        **kwargs,
+    ):
+        self.track = track
+        self.artist = artist
+        self.source = source
+        self.url = url
+        self.kwargs = kwargs

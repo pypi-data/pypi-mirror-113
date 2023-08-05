46ccfcb85efd3bc2b5376171dad21f793e4e2ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:59 +0000

    Add package dpys/4.0.2/dpys-4.0.2.tar.gz

---
+++ packages/dpys/dpys-4.0.2.tar.gz/dpys-4.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='dpys',
+  version='4.0.2',
+  description='A library to simplify discord.py',
+  long_description="The goal of DPYS is to make basic functionalities that every good bot needs easy to implement for beginners.",
+  url='https://sites.google.com/view/dpys',  
+  author='George Luca',
+  author_email='fixingg@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='discord', 
+  packages=find_packages(),
+  install_requires=['discord.py==1.7.3', 'aiosqlite']
+)

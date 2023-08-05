1440deb9ea1f06997634879407fcdda91f94cbfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:19 +0000

    Add package tinyml/0.0.5/tinyml-0.0.5.tar.gz

---
+++ packages/tinyml/tinyml-0.0.5.tar.gz/tinyml-0.0.5/tinyml/core/__init__.py
@@ -0,0 +1,4 @@
+from .backend import GPU, Backend
+from .parameter import Parameter
+
+__all__ = ['Parameter', 'Backend', 'GPU']
---
+++ packages/tinyml/tinyml-0.0.5.tar.gz/tinyml-0.0.5/tinyml/losses/mse.py
@@ -0,0 +1,6 @@
+def mse_loss(predicted, ground_truth):
+    '''
+    Compute the mean square error loss.
+    '''
+    diff = predicted - ground_truth.reshape(predicted.shape)
+    return (diff**2).mean(), 2 * diff / diff.shape[1]

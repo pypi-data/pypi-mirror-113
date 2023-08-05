51eb8bf7f6aa74d19bc3a8e20fd114270bd9bf61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:30 +0000

    Add package strictify/0.1.1/strictify-0.1.1.tar.gz

---
+++ packages/strictify/strictify-0.1.1.tar.gz/strictify-0.1.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['strictify']
+setup_kwargs = {
+    'name': 'strictify',
+    'version': '0.1.1',
+    'description': 'A strict decorator for python',
+    'long_description': None,
+    'author': 'Mikhail',
+    'author_email': 'joinmma@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)

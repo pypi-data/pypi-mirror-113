0f468d6764dd9bccc7776f6d7fd633d2cf5f5a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:46 +0000

    Add package pypassgenmode/1.0.1/pypassgenmode-1.0.1-py3-none-any.whl

---
+++ packages/pypassgenmode/pypassgenmode-1.0.1-py3-none-any.whl/pypassgen/__init__.py
@@ -0,0 +1,11 @@
+import random
+
+def genpass(length):
+  chars = 'abcdefghyjklmnopqrstuvwxyz'
+  chars += chars.upper()
+  nums = str(1234567890)
+  chars += nums
+  special_chars = '!@#$%^&*()_+-'
+  chars += special_chars
+  result = "".join(random.sample(chars, length))
+  return result

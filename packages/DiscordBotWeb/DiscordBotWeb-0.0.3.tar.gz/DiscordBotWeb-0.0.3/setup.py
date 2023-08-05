164c3ce40b065c92c6c3f53aeaa57db3d1dbc535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:56 +0000

    Add package DiscordBotWeb/0.0.3/DiscordBotWeb-0.0.3.tar.gz

---
+++ packages/DiscordBotWeb/DiscordBotWeb-0.0.3.tar.gz/DiscordBotWeb-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md") as f:
+  README = f.read()
+  
+setuptools.setup(
+  name="DiscordBotWeb",
+  version="0.0.3",
+  description="Make A Simple Website For Your Discord Bot Without Any HTML Knowledge!",
+  long_description=README,
+  long_description_content_type="text/markdown",
+  author="nooby xviii",
+  author_email="xviii2008@gmail.com",
+  packages=setuptools.find_packages(),
+  include_package_data=True,
+  install_requires=[],
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires=">=3.6"
+  )

c580e4aad4dfcc9b9c142a3dda47a35652697acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:01 +0000

    Add package distributions-ramon/0.11/distributions_ramon-0.11.tar.gz

---
+++ packages/distributions-ramon/distributions_ramon-0.11.tar.gz/distributions_ramon-0.11/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_ramon',
+      version='0.11',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = "RamonSantiago",
+      author_email = "raysantiago1994@gmail.com",
+      zip_safe=False)

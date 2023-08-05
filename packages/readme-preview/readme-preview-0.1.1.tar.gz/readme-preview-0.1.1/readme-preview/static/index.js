a7b0015078d20eb618c5d206fb9288b41eb46dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:45 +0000

    Add package readme-preview/0.1.1/readme-preview-0.1.1.tar.gz

---
+++ packages/readme-preview/readme-preview-0.1.1.tar.gz/readme-preview-0.1.1/readme-preview/static/index.js
@@ -0,0 +1,87 @@
+define(['exports', './showdown', 'jquery'], function (exports, showdown, $) {
+
+    converter = new showdown.Converter()
+
+    converter.setFlavor('github');
+    converter.setOption('simpleLineBreaks', true)
+    converter.setOption('tasklists', true)
+    converter.setOption('tables', true)
+    converter.setOption('openLinksInNewWindow', true)
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+
+
+    function addStyle(){
+    
+        const style = document.createElement('style');
+        // add CSS styles
+        style.innerHTML = `
+            #div_readme {
+                padding:20px; 
+                border:1px solid #ddd; 
+                border-radius:5px; 
+                background: #fcfcfc;
+            }
+            #div_readme p {
+                margin: 2rem 0 1rem 0;
+            }
+        `;
+
+        // append the style to the DOM in <head> section
+        document.head.appendChild(style);
+
+    }
+
+    function drawHtml(markdown){
+
+        var html = converter.makeHtml(markdown)
+
+        div = document.createElement('div')
+        div.setAttribute("id", "div_readme");
+
+        div.innerHTML = "<h4 style='font-weight:bold'>README.md</h4><hr/>"
+        div.innerHTML += html
+        document.querySelector('#notebooks').appendChild(div)
+    }
+
+    function showReadMe(){
+
+        var opts = {
+            method: 'GET',      
+            headers: {'Content-Type':'application/json'}
+        };
+
+        var rows = Array.prototype.concat.apply([], document.querySelectorAll('.list_item.row'));
+        var row = rows.filter(row=>row.querySelector('.item_name').innerText=='README.md')
+
+        if(row.length > 0){
+
+            var url = row[0].querySelector('.item_link').href.replace(/^.*\/\/[^\/]+/, '')
+            url = "/api/contents/"+ url.split("/").splice(2).join("/")+"?type=file&format=text"
+
+            console.log(url)
+
+            fetch(url, opts).then(function (response) {
+                return response.json();
+            })
+            .then(function (body) {
+                drawHtml(body.content)
+            });
+
+        }
+    }
+
+    
+
+    function load_ipython_extension() {
+        addStyle()
+        showReadMe()
+
+    }
+
+
+    exports.load_ipython_extension = load_ipython_extension;
+});
+

6c2a1359ed1800a515734748f682d7aff9047e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:28 +0000

    Add package twcmanager-client/0.2.0/twcmanager_client-0.2.0.tar.gz

---
+++ packages/twcmanager-client/twcmanager_client-0.2.0.tar.gz/twcmanager_client-0.2.0/src/twcmanager_client/client.py
@@ -0,0 +1,37 @@
+import aiohttp
+from typing import Any
+
+class TWCManagerClient:
+    """TWCManager client."""
+
+
+    def __init__(self, host: str):
+        """Construct a new TWCManager client."""
+        self._host = host
+    
+
+    async def async_get_config(self) -> dict[str, Any]:
+        """Get the current configuration."""
+        return await self._async_get("http://" + self._host + ':8080/api/getConfig')
+    
+
+    async def async_get_policy(self) -> dict[str, Any]:
+        """Get the policy configuration."""
+        return await self._async_get("http://" + self._host + ':8080/api/getPolicy')
+
+
+    async def async_get_slave_twcs(self) -> dict[str, Any]:
+        """Get a list of connected slave TWCs and their state."""
+        return await self._async_get("http://" + self._host + ':8080/api/getSlaveTWCs')
+
+
+    async def async_get_status(self) -> dict[str, Any]:
+        """Get the current status (charge rate, policy)."""
+        return await self._async_get("http://" + self._host + ':8080/api/getStatus')
+    
+
+    async def _async_get(self,path: str,) -> dict[str, Any]:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(path) as response:
+                response.raise_for_status()
+                return await response.json()

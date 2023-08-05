52ca416cbbd4b660a3b0d2eda9297b95cb7ff71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:54 +0000

    Add package maso/1.6/maso-1.6.tar.gz

---
+++ packages/maso/maso-1.6.tar.gz/maso-1.6/maso/__init__.py
@@ -0,0 +1,3 @@
+from .supercv import *
+from .superos import *
+from .AI_model import *
---
+++ packages/maso/maso-1.6.tar.gz/maso-1.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='maso',
+    version='1.6',
+    description='方便使用OPENCV包的套件',
+    url='https://github.com/maso0310/maso/tree/master',
+    author='Maso',
+    author_email='balimon@hotmail.com',
+    install_requires=["opencv-python", "numpy","joblib","pandas","scikit-learn==0.24.1"],
+    license='MIT License',
+    packages=['maso'],
+    zip_safe=False,
+    keywords=['maso'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)

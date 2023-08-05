dd3e87fa05ef5f71998315864297581a67bb948e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:17 +0000

    Add package financepy/0.202/financepy-0.202-py3-none-any.whl

---
+++ packages/financepy/financepy-0.202-py3-none-any.whl/financepy/__init__.py
@@ -0,0 +1,11 @@
+cr = "\n"
+
+s = "####################################################################" + cr
+s += "# FINANCEPY BETA Version " + str('0.202') + " - This build:  16 Jul 2021 at 18:46 #" + cr
+s += "# **** NEW PEP8 COMPLIANT VERSION -- PLEASE UPDATE YOUR CODE  **** #" + cr
+s += "#      This software is distributed FREE & WITHOUT ANY WARRANTY    #" + cr
+s += "#  Report bugs as issues at https://github.com/domokane/FinancePy  #" + cr
+s += "####################################################################"
+s += cr
+
+print(s)

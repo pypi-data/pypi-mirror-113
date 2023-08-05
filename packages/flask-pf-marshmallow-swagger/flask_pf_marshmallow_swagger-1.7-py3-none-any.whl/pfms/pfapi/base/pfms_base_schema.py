c2cb0a80fa7f1e72629ceb256967fac62057b3a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:51 +0000

    Add package flask-pf-marshmallow-swagger/1.7/flask_pf_marshmallow_swagger-1.7-py3-none-any.whl

---
+++ packages/flask-pf-marshmallow-swagger/flask_pf_marshmallow_swagger-1.7-py3-none-any.whl/pfms/pfapi/base/pfms_base_schema.py
@@ -0,0 +1,48 @@
+from marshmallow import Schema, fields
+from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
+from pf_sqlalchemy.db.orm import BaseModel
+
+
+class PfOnlySchema(Schema):
+    pass
+
+
+class PfBaseSchema(SQLAlchemySchema):
+    pass
+
+
+class PfDetailBaseSchema(PfBaseSchema):
+    class Meta:
+        model = BaseModel
+
+    id = auto_field()
+    created = auto_field()
+    updated = auto_field()
+    uuid = auto_field()
+
+
+class ModelViewSort(Schema):
+    id = fields.Integer(required=True, error_messages={"required": "Please enter entity id."})
+    viewOrder = fields.Integer(required=True, error_messages={"required": "Please enter view order."})
+
+
+def common_exclude():
+    exclude = ["id", "created", "updated", "uuid"]
+    return exclude
+
+
+def update_exclude():
+    exclude = ("created", "updated", "uuid")
+    return exclude
+
+
+def common_exclude_append(*args):
+    list_args = list(args)
+    list_args.extend(common_exclude())
+    return list_args
+
+
+def update_exclude_append(*args):
+    list_args = list(args)
+    list_args.extend(update_exclude())
+    return list_args
---
+++ packages/flask-pf-marshmallow-swagger/flask_pf_marshmallow_swagger-1.7-py3-none-any.whl/pfms/swagger/pfms_swagger_schemas.py
@@ -0,0 +1,70 @@
+from pfms.swagger.pfms_definition import PFMSDefinition
+from pfms.swagger.pfms_swagger_cons import ERROR_DETAILS_RESPONSE, MESSAGE_RESPONSE
+
+IN_PATH = "path"
+IN_QUERY = "query"
+OBJ = "object"
+ARRAY = "array"
+
+
+def get_parameter(place, name, data_type, required=False):
+    return {
+        "name": name,
+        "in": place,
+        "schema": {
+            "type": data_type
+        },
+        "required": required
+    }
+
+
+def get_component_schemas_link(name):
+    return "#/components/schemas/" + name
+
+
+def get_schema_def_ref(ref, s_type=None):
+    schema = {"type": s_type}
+    if s_type == ARRAY:
+        schema["items"] = {
+            "$ref": get_component_schemas_link(ref)
+        }
+        return schema
+    return {"$ref": get_component_schemas_link(ref)}
+
+
+def get_request_body(definition: PFMSDefinition, is_bulk=False):
+    s_type = None
+    if is_bulk:
+        s_type = ARRAY
+    return {
+        "required": True,
+        "content": {
+            definition.request_type: {
+                "schema": get_schema_def_ref(definition.request_component, s_type)
+            }
+        }
+    }
+
+
+def get_response(definition: PFMSDefinition, http_code=200):
+    any_of_response = {"anyOf": []}
+    if definition.response_obj:
+        any_of_response["anyOf"].append(get_schema_def_ref(definition.response_component))
+
+    any_of_response["anyOf"].append(get_schema_def_ref(MESSAGE_RESPONSE))
+    if definition.error_details:
+        any_of_response["anyOf"].append(get_schema_def_ref(ERROR_DETAILS_RESPONSE))
+
+    return {
+        http_code: {
+            "content": {
+                definition.response_type: {
+                    "schema": any_of_response
+                }
+            }
+        }
+    }
+
+
+def get_spec_from_definition():
+    pass

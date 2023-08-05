d937960621c2577913043583724b09c27aa46556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:48 +0000

    Add package poetry-date-version-plugin/2021.7.16-3/poetry-date-version-plugin-2021.7.16-3.tar.gz

---
+++ packages/poetry-date-version-plugin/poetry-date-version-plugin-2021.7.16-3.tar.gz/poetry-date-version-plugin-2021.7.16-3/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['poetry_date_version_plugin']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cleo>=1.0.0a1,<1.0.0', 'poetry>=1.2.0a0']
+
+entry_points = \
+{'poetry.application.plugin': ['plugin = '
+                               'poetry_date_version_plugin.plugin:VersionPlugin']}
+
+setup_kwargs = {
+    'name': 'poetry-date-version-plugin',
+    'version': '2021.7.16-3',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Dustyn Gibson',
+    'author_email': 'miigotu@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)

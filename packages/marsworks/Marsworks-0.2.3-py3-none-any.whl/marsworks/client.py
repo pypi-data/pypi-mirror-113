f0ffef37fae828393b75b06d266b72e60430a674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:40 +0000

    Add package marsworks/0.2.3/Marsworks-0.2.3-py3-none-any.whl

---
+++ packages/marsworks/Marsworks-0.2.3-py3-none-any.whl/marsworks/client.py
@@ -0,0 +1,177 @@
+import datetime
+import io
+import os
+import typing
+from typing import Union, Optional
+
+import httpx
+
+from marsworks.origin import Rest, Camera, Rover, ensure_type, BadArgumentError
+from marsworks.manifest import Manifest
+from marsworks.photo import Photo
+
+__all__ = ("Client",)
+
+
+class Client:
+
+    __slots__ = ("__http",)
+
+    def __init__(
+        self,
+        *,
+        api_key: Optional[str] = None,
+        session: Optional[httpx.AsyncClient] = None,
+        suppress_warnings: bool = False,
+    ) -> None:
+        """
+        Client Constructor.
+
+        Arguments:
+            api_key: NASA [API key](https://api.nasa.gov/). (optional)
+            session: An [AsyncClient](https://www.python-httpx.org/api/#asyncclient) object. (optional)
+            suppress_warnings: Whether to suppress warnings.
+        """  # noqa: E501
+        self.__http = Rest(
+            api_key=api_key, session=session, suppress_warnings=suppress_warnings
+        )
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, exc_tb):
+        await self.close()
+
+    async def get_mission_manifest(self, name: str) -> Manifest:
+        """
+        Gets the mission manifest of the rover passed in `name` arg.
+
+        Arguments:
+            name : Name of rover. (not case-sensitive)
+
+        Note:
+            `name` can be a class variable of [Rover](../API-Reference/Choices/rover.md).
+
+        Returns:
+            A [Manifest](./manifest.md) object containing mission's info.
+        """  # noqa: E501
+        name = name.upper() if isinstance(name, str) else name
+        if name in Rover():
+            metadata = await self.__http.start(name)
+            mfst = await metadata.manifest_content()
+            return mfst
+        else:
+            raise BadArgumentError(f"name should be one of <{', '.join(Rover())}>.")
+
+    async def get_photo_by_sol(
+        self, name: str, sol: typing.Union[int, str], *, camera: Optional[str] = None
+    ) -> list:
+        """
+        Gets the photos taken by the given rover on the given sol.
+        We can sort the images with `camera` param.
+
+        Arguments:
+            name : Name of rover. (not case-sensitive)
+            sol: The sol when photo was captured.
+            camera: Camera with which photo is taken. (not case-sensitive) (Optional)
+
+        Note:
+            `name` can be a class variable of [Rover](../API-Reference/Choices/rover.md).
+        Note:
+            `camera` can be a class variable of [Camera](../API-Reference/Choices/camera.md).
+
+        Returns:
+            A list of [Photo](./photo.md) objects with url and info.
+        """  # noqa: E501
+        name = name.upper() if isinstance(name, str) else name
+        if name in Rover():
+            camera = (
+                camera.upper()
+                if isinstance(camera, str) and camera.upper() in Camera()
+                else None
+            )
+            metadata = await self.__http.start(name + "/photos", sol=sol, camera=camera)
+            pht = await metadata.photo_content()
+            return pht
+        else:
+            raise BadArgumentError(f"name should be one of <{', '.join(Rover())}>.")
+
+    async def get_photo_by_earthdate(
+        self, name: str, earth_date: datetime.date, *, camera: Optional[str] = None
+    ) -> list:
+        """
+        Gets the photos taken by the given rover on the given date.
+        We can sort the images with `camera` param.
+
+        Arguments:
+            name : Name of rover. (not case-sensitive)
+            earth_date: An [datetime.date](https://docs.python.org/3/library/datetime.html?highlight=datetime%20date#datetime.date) object.
+            camera: Camera with which photo is taken. (not case-sensitive) (Optional)
+
+        Note:
+            `name` can be a class variable of [Rover](../API-Reference/Choices/rover.md).
+        Note:
+            `camera` can be a class variable of [Camera](../API-Reference/Choices/camera.md).
+
+        Returns:
+            A list of [Photo](./photo.md) objects with url and info.
+        """  # noqa: E501
+        name = name.upper() if isinstance(name, str) else name
+        if name in Rover():
+            camera = (
+                camera.upper()
+                if isinstance(camera, str) and camera.upper() in Camera()
+                else None
+            )
+            metadata = await self.__http.start(
+                name.name + "/photos", earth_date=str(earth_date), camera=camera
+            )
+            pht = await metadata.photo_content()
+            return pht
+        else:
+            raise BadArgumentError(f"name should be one of <{', '.join(Rover())}>.")
+
+    @ensure_type
+    async def read(self, photo: Photo) -> io.BytesIO:
+        """
+        Reads the bytes of image url in photo.
+
+        Arguments:
+            photo : The [Photo](./photo.md) object whose image url is to be read.
+
+        Returns:
+            A [BytesIO](https://docs.python.org/3/library/io.html?highlight=bytesio#io.BytesIO) object.
+        """  # noqa: E501
+        data = await self.__http.read(photo.img_src)
+        return data
+
+    @ensure_type
+    async def save(
+        self, photo: Photo, fp: Union[str, bytes, os.PathLike, io.BufferedIOBase]
+    ) -> int:
+        """
+        Saves the image of [Photo](./photo.md) object.
+
+        Arguments:
+            photo : The [Photo](./photo.md) object whose image is to be saved.
+            fp: The file path (with name and extension) where the image has to be saved.
+
+        Returns:
+            Number of bytes written.
+        """  # noqa: E501
+        read_data = await self.__http.read(photo.img_src, bytes_=True)
+        if isinstance(fp, io.IOBase) and fp.writable():
+            bw = fp.write(read_data)
+            return bw
+        else:
+            with open(fp, "wb") as f:
+                return f.write(read_data)
+
+    async def close(self) -> None:
+        """
+        Closes the AsyncClient.
+
+        Warning:
+            It can close user given [AsyncClient](https://www.python-httpx.org/api/#asyncclient) session too.
+        """  # noqa: E501
+        await self.__http.close()
---
+++ packages/marsworks/Marsworks-0.2.3-py3-none-any.whl/marsworks/origin/rest.py
@@ -0,0 +1,114 @@
+import inspect
+import io
+from typing import Optional, Any, Union
+import warnings
+
+import httpx
+from marsworks.origin.exceptions import BadStatusCodeError, ContentTypeError
+from marsworks.origin.metainfo import MetaInfo
+from rfc3986.builder import URIBuilder
+
+__all__ = ("Rest",)
+
+
+class Rest:
+
+    __slots__ = ("_session", "_api_key", "_base_url", "_suppress_warnings")
+
+    def __init__(
+        self,
+        *,
+        api_key: Optional[str] = None,
+        session: Optional[httpx.AsyncClient] = None,
+        suppress_warnings: bool = False,
+    ) -> None:
+        self._session = session
+        self._api_key = api_key or "DEMO_KEY"
+        self._base_url = "api.nasa.gov/mars-photos/api/v1/rovers"
+        self._suppress_warnings = suppress_warnings
+
+    async def _session_initializer(self) -> None:
+        """
+        Initailizes an AsyncClient if no (or bad) session arg is
+        passed to constructor.
+        """
+        self._session = httpx.AsyncClient()
+
+    async def start(self, path: str, **params: Any) -> MetaInfo:
+        """
+        Starts an api call.
+        """
+        if not isinstance(self._session, httpx.AsyncClient):
+            await self._session_initializer()
+        params["api_key"] = self._api_key
+
+        if self._api_key == "DEMO_KEY" and not self._suppress_warnings:
+            warnings.warn("Using DEMO_KEY for api call. Please use your api key.")
+
+        url = self._build_url(path, params)
+
+        resp = await self._session.get(url)
+
+        if self._checks(resp):
+            return MetaInfo(resp)
+
+    async def read(
+        self, url: str, bytes_: bool = False
+    ) -> Union[io.BytesIO, bytes]:
+        """
+        Reads bytes of image.
+        """
+        if not isinstance(self._session, httpx.AsyncClient):
+            await self._session_initializer()
+        resp = await self._session.get(url)
+        recon = await resp.aread()
+        if self._checks(resp):
+            if bytes_:
+                return recon
+            return io.BytesIO(recon)
+
+    # ===========Factory-like helper methods.================================
+    def _checks(self, resp: httpx.AsyncClient) -> bool:
+        """
+        Checks status code and content type.
+        """
+        if not (300 > resp.status_code >= 200):
+            raise BadStatusCodeError(resp)
+        elif resp.headers["content-type"] not in (
+            "application/json; charset=utf-8",
+            "image/jpeg",
+        ):
+            raise ContentTypeError(resp)
+        else:
+            return True
+
+    def _build_url(self, path: str, queries: dict) -> str:
+        """
+        Builds the url.
+        """
+        for q in list(queries):
+            if queries[q] is None:
+                queries.pop(q)
+        url = URIBuilder(
+            scheme="https", host=self._base_url, path="/" + path
+        ).add_query_from(queries)
+        return url.geturl()
+
+    # =========================================================================
+
+    async def close(self) -> None:
+        """
+        Closes the AsyncClient and marks self.session as None.
+        """
+        if self._session is not None:
+            await self._session.aclose()
+            self._session = None
+
+    def __repr__(self):
+        fil = filter(
+            lambda attr: not attr[0].startswith("_")
+            and not callable(getattr(self, attr[0], None)),
+            inspect.getmembers(self),
+        )
+        rpr = "".join(f"{i[0]} = {i[1]}, " for i in fil)[:-2]
+        return f"{__class__.__name__}({rpr})"

a089f0795355708da9eb36a1d049f43186b56bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:28 +0000

    Add package aiodag/0.2/aiodag-0.2.tar.gz

---
+++ packages/aiodag/aiodag-0.2.tar.gz/aiodag-0.2/setup.py
@@ -0,0 +1,32 @@
+import sys
+from setuptools import setup, find_packages
+
+
+if sys.version_info < (3, 7):
+    raise RuntimeError('aiodag requires Python 3.7+')
+
+
+classifiers = [
+    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3',
+    'Framework :: AsyncIO',
+]
+
+
+keywords = ["dag", "asyncio", "aiodag"]
+
+
+setup(
+    name='aiodag',
+    version='0.2',
+    description=('Build and execute AsyncIO powered DAGs.'),
+    classifiers=classifiers,
+    platforms=['POSIX'],
+    author="aa1371",
+    url='https://github.com/aa1371/aiodag',
+    download_url='https://github.com/aa1371/aiodag/archive/refs/tags/v0.2.tar.gz',
+    license='MIT',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    keywords=keywords
+)

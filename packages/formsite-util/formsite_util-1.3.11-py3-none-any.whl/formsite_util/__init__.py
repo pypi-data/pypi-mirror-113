c67e71cb26161156e503b26e041e221de01296b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:29 +0000

    Add package formsite-util/1.3.11/formsite_util-1.3.11-py3-none-any.whl

---
+++ packages/formsite-util/formsite_util-1.3.11-py3-none-any.whl/formsite_util/__init__.py
@@ -0,0 +1,6 @@
+from formsite_util.internal.interfaces import FormsiteParams, FormsiteInterface, FormsiteCredentials
+
+# try:
+#     from internal.interfaces import FormsiteParams, FormsiteInterface, FormsiteCredentials
+# except ImportError:
+#     from .internal.interfaces import FormsiteParams, FormsiteInterface, FormsiteCredentials

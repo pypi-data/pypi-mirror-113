7eed4d9d24b5b479d646018f62e5e936dd9c2b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:10 +0000

    Add package Andencento/0.6/Andencento-0.6.tar.gz

---
+++ packages/Andencento/Andencento-0.6.tar.gz/Andencento-0.6/userbot/assistant/__init__.py
@@ -0,0 +1,16 @@
+
+from telethon import version
+from telethon.errors.rpcerrorlist import (MediaEmptyError,
+                                          WebpageCurlFailedError,
+                                          WebpageMediaEmptyError)
+from telethon.events import CallbackQuery
+
+from userbot import *
+from userbot.config import Config
+from userbot.helpers import *
+from userbot.utils import *
+
+from .. import *
+from ..config import Config
+from ..helpers import *
+from ..utils import *

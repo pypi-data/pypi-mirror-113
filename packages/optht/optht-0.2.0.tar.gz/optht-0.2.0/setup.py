940f2a850643f1bbaec8d607228184a6c9aa3531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:14 +0000

    Add package optht/0.2.0/optht-0.2.0.tar.gz

---
+++ packages/optht/optht-0.2.0.tar.gz/optht-0.2.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name='optht',
+    version='0.2.0',
+    description='Optimal hard threshold for matrix denoising',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='N. Benjamin Erichson, Steven Dahdah',
+    author_email='erichson@berkeley.edu, Steven.Dahdah@mail.mcgill.ca',
+    url='https://github.com/erichson/optht',
+    project_urls={
+        "Bug Tracker": "https://github.com/erichson/optht/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(exclude=('tests', 'examples')),
+    install_requires=['numpy', 'scipy'],
+    python_requires=">=3.6",
+)

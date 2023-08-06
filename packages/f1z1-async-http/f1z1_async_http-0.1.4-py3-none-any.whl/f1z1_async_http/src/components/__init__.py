f0e88bb2649ab1328de31543cafaa0455c86f40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:34 +0000

    Add package f1z1-async-http/0.1.4/f1z1_async_http-0.1.4-py3-none-any.whl

---
+++ packages/f1z1-async-http/f1z1_async_http-0.1.4-py3-none-any.whl/f1z1_async_http/src/components/__init__.py
@@ -0,0 +1,11 @@
+# @Time     : 2021/5/30
+# @Project  : f1z1-g
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+# from .interceptors import InterceptorManager, Interceptors, AsyncFunction, IInterceptorManager, IInterceptors
+# from .method import RequestMethod, IRequestMethod
+# from .message import RequestMessageQueue, IRequestMessage
+# from .notifier import AsyncRequestNotifier, AsyncResponseNotifier, AbstractAsyncNotifier, notify
+# from .options import RequestOptions, IRequestOptions
+# from .url import RequestURL, IRequestURL
---
+++ packages/f1z1-async-http/f1z1_async_http-0.1.4-py3-none-any.whl/f1z1_async_http/src/core/client.py
@@ -0,0 +1,89 @@
+# @Time     : 2021/5/30
+# @Project  : f1z1-g
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+from typing import AnyStr, Optional
+
+from httpx import AsyncClient, Response
+
+from ..components.method import MethodTypes
+from ..components.options import IRequestOptions
+from ..components.message import IRequestMessage
+from .adapter import Adapters
+
+
+class IAsyncHttpClient(object):
+
+    async def request(self,
+                      method: MethodTypes,
+                      url: AnyStr,
+                      options: Optional[IRequestOptions]) -> Response:
+        raise NotImplementedError("")
+
+    async def __aenter__(self) -> ["IAsyncHttpClient"]:
+        raise NotImplementedError("")
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        raise NotImplementedError("")
+
+
+class AsyncHttpClient(IAsyncHttpClient):
+
+    def __init__(self, client: AsyncClient, rm: IRequestMessage):
+        self._client = client
+        self._rm = rm
+
+    @property
+    def client(self):
+        return self._client
+
+    @client.setter
+    def client(self, client: AsyncClient) -> None:
+        if not self._is_client(client):
+            raise ValueError(
+                f"client need AsyncClient, but got {type(client).__name__}"
+            )
+        self._client = client
+
+    async def request(self,
+                      method: MethodTypes,
+                      url: AnyStr,
+                      options: Optional[IRequestOptions]) -> Response:
+        method, url, kwargs = self._convert(method, url, options)
+        return await self._send(method, url, **kwargs)
+
+    async def open(self):
+        if self._is_closed():
+            await self._client.__aenter__()
+
+    async def close(self):
+        if all([self._is_empty(), not self._is_closed()]):
+            print("close", self._rm)
+            await self._client.aclose()
+
+    async def _send(self, method, url, **kwargs):
+        return await self.client.request(method, url, **kwargs)
+
+    def _convert(self, method: MethodTypes,
+                 url: AnyStr,
+                 options: Optional[IRequestOptions]):
+        return Adapters(method, url, options).convert()
+
+    def _is_closed(self):
+        return self.client.is_closed
+
+    def _is_empty(self):
+        return self._rm.empty()
+
+    def _is_client(self, client):
+        return isinstance(client, AsyncClient)
+
+    async def __aenter__(self):
+        await self.open()
+        print("rmq", self._rm)
+        print("rmq id", id(self._rm))
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.close()

e6e8a6111e85488b29512f2bb786bc3f5d288379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:09 +0000

    Add package HDistributions/0.2/HDistributions-0.2.tar.gz

---
+++ packages/HDistributions/HDistributions-0.2.tar.gz/HDistributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='HDistributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['HDistributions'],
+      author = "Abid Ali Awan",
+      author_email = "abidaliawan@rocketmail.com",
+      zip_safe=False)

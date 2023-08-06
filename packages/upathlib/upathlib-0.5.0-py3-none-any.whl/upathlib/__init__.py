424adbb81af4cfae96de6c2e607fd0a7979bdff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:56 +0000

    Add package upathlib/0.5.0/upathlib-0.5.0-py3-none-any.whl

---
+++ packages/upathlib/upathlib-0.5.0-py3-none-any.whl/upathlib/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '0.5.0'
+
+
+from ._upath import Upath, FileInfo, LockAcquisitionTimeoutError
+from ._local import LocalUpath
+from ._blob import BlobUpath
+
+
+__all__ = [
+    'Upath',
+    'FileInfo',
+    'LockAcquisitionTimeoutError',
+    'LocalUpath',
+    'BlobUpath',
+]

a9a47ad6734dfbc70cd9b1e93cd9fcf2cd91d98e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:10 +0000

    Add package mamanteo-distributions/0.1/mamanteo_distributions-0.1.tar.gz

---
+++ packages/mamanteo-distributions/mamanteo_distributions-0.1.tar.gz/mamanteo_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mamanteo_distributions',
+      version='0.1',
+      description='Binomial and Gaussian Distributions',
+      packages=['mamanteo_distributions'],
+      zip_safe=False)

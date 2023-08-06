94a127bf34f9ae8f6ad45e1980079ab24622154d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:19 +0000

    Add package ngrok-server/1.7/ngrok_server-1.7-py3-none-any.whl

---
+++ packages/ngrok-server/ngrok_server-1.7-py3-none-any.whl/ngrok_server-1.7.data/scripts/get_ngrok_address.py
@@ -0,0 +1,11 @@
+#!python
+
+import json
+import requests
+
+response = requests.get("http://www.localhost:4040/api/tunnels")
+json = response.json()
+ip = json['tunnels'][0]['public_url']
+
+ip = ip.replace("tcp://","")
+print(ip)

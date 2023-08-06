3dddffa2286bd1a2f7cfb0f06b002fd5dc0437cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:33 +0000

    Add package lrabbit-scrapy/0.0.3/lrabbit_scrapy-0.0.3-py3-none-any.whl

---
+++ packages/lrabbit-scrapy/lrabbit_scrapy-0.0.3-py3-none-any.whl/lrabbit_scrapy/utils.py
@@ -0,0 +1,30 @@
+import aiohttp
+
+
+async def get_text_by_url(url, headers=None, proxy=None):
+    res = await get_res_by_url(url, headers, proxy)
+    text = await res.text()
+    return text
+
+
+async def get_res_by_url(url, headers=None, proxy=None):
+    async with aiohttp.ClientSession() as session:
+        kwargs = {'url': url}
+        if headers:
+            kwargs['headers'] = headers
+        if proxy:
+            kwargs['proxy'] = proxy
+        res = await session.get(**kwargs)
+        return res
+
+
+async def download_by_url(url, file_path, headers=None, proxy=None):
+    res = await get_res_by_url(url, headers, proxy)
+    with open(file_path, 'wb') as f:
+        while True:
+            chunk = await res.content.read(1024)
+            if chunk:
+                f.write(chunk)
+            else:
+                break
+    return file_path

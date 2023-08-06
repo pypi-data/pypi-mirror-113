8a7470a89777a0271e8ad81cc48ba0214613bcf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:07 +0000

    Add package dsnd-dist/0.1/dsnd_dist-0.1.tar.gz

---
+++ packages/dsnd-dist/dsnd_dist-0.1.tar.gz/dsnd_dist-0.1/dsnd_dist/Binomialdistribution.py
@@ -0,0 +1,200 @@
+import math
+import matplotlib.pyplot as plt
+from .Generaldistribution import Distribution
+
+class Binomial(Distribution):
+    """ Binomial distribution class for calculating and
+    visualizing a Binomial distribution.
+
+    Attributes:
+        mean (float) representing the mean value of the distribution
+        stdev (float) representing the standard deviation of the distribution
+        data_list (list of floats) a list of floats to be extracted from the data file
+        p (float) representing the probability of an event occurring
+        n (int) the total number of trials
+
+    """
+
+    #       A binomial distribution is defined by two variables:
+    #           the probability of getting a positive outcome
+    #           the number of trials
+
+    #       If you know these two values, you can calculate the mean and the standard deviation
+    #
+    #       For example, if you flip a fair coin 25 times, p = 0.5 and n = 25
+    #       You can then calculate the mean and standard deviation with the following formula:
+    #           mean = p * n
+    #           standard deviation = sqrt(n * p * (1 - p))
+
+    #
+
+    def __init__(self, prob=.5, size=20):
+
+        self.p = prob
+        self.n = size
+        self.calculate_mean()
+        self.calculate_stdev()
+
+        Distribution.__init__(self, self.calculate_mean(), self.calculate_stdev())
+
+
+    def calculate_mean(self):
+
+        """Function to calculate the mean from p and n
+
+        Args:
+            None
+
+        Returns:
+            float: mean of the data set
+
+        """
+
+        self.mean = self.p * self.n
+        return self.mean
+
+
+    def calculate_stdev(self):
+
+        """Function to calculate the standard deviation from p and n.
+
+        Args:
+            None
+
+        Returns:
+            float: standard deviation of the data set
+
+        """
+
+        self.stdev = (self.n * self.p * (1 - self.p))**(1/2)
+        return self.stdev
+
+
+    def replace_stats_with_data(self):
+
+        """Function to calculate p and n from the data set
+
+        Args:
+            None
+
+        Returns:
+            float: the p value
+            float: the n value
+
+        """
+
+        self.read_data_file(file_name='numbers_binomial.txt')
+
+        if not self.data:
+            return self.p, self.n
+
+        self.n = len(self.data)
+        self.p = sum(self.data)/self.n
+        self.calculate_mean()
+        self.calculate_stdev()
+
+        return self.p, self.n
+
+
+    def plot_bar(self):
+        """Function to output a histogram of the instance variable data using
+        matplotlib pyplot library.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+
+        plt.bar(x = ['0', '1'], height = [(1 - self.p) * self.n, self.p * self.n])
+        plt.title('Bar Chart of Data')
+        plt.xlabel('outcome')
+        plt.ylabel('count')
+
+
+    def pdf(self, k):
+        """Probability density function calculator for the gaussian distribution.
+
+        Args:
+            k (float): point for calculating the probability density function
+
+
+        Returns:
+            float: probability density function output
+        """
+
+        n = self.n
+        p = self.p
+        return (
+            (math.factorial(n))/(math.factorial(n-k)
+            * math.factorial(k)))*((p)**(k))*((1-p)**(n-k)
+        )
+
+
+    def plot_bar_pdf(self):
+
+        """Function to plot the pdf of the binomial distribution
+
+        Args:
+            None
+
+        Returns:
+            list: x values for the pdf plot
+            list: y values for the pdf plot
+
+        """
+
+        x = []
+        y = []
+
+        # calculate the x values to visualize
+        for i in range(self.n + 1):
+            x.append(i)
+            y.append(self.pdf(i))
+
+        # make the plots
+        plt.bar(x, y)
+        plt.title('Distribution of Outcomes')
+        plt.ylabel('Probability')
+        plt.xlabel('Outcome')
+
+        plt.show()
+
+        return x, y
+
+
+    def __add__(self, other):
+
+        """Function to add together two Binomial distributions with equal p
+
+        Args:
+            other (Binomial): Binomial instance
+
+        Returns:
+            Binomial: Binomial distribution
+
+        """
+
+        try:
+            assert self.p == other.p, 'p values are not equal'
+        except AssertionError as error:
+            raise
+
+        return Binomial(prob=self.p, size=self.n+other.n)
+
+
+    def __repr__(self):
+
+        """Function to output the characteristics of the Binomial instance
+
+        Args:
+            None
+
+        Returns:
+            string: characteristics of the Gaussian
+
+        """
+
+        return "mean {}, standard deviation {}, p {}, n {}".\
+        format(self.mean, self.stdev, self.p, self.n)
---
+++ packages/dsnd-dist/dsnd_dist-0.1.tar.gz/dsnd_dist-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dsnd_dist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_dist'],
+      author='jfpinedap',
+      author_email='jfpinedap@gmail.com',
+      zip_safe=False,
+)

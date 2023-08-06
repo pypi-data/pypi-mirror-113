52b8037cd11362d60523dc6e9afd3b5054cbd05d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:45 +0000

    Add package django-admin-utils/2.0.2/django_admin_utils-2.0.2-py2.py3-none-any.whl

---
+++ packages/django-admin-utils/django_admin_utils-2.0.2-py2.py3-none-any.whl/admin_utils/__init__.py
@@ -0,0 +1,12 @@
+from .brief import inline
+from .brief import register
+from .mock import make_admin_class
+from .mock import register_view
+
+__version__ = '2.0.2'
+__all__ = [
+    'inline',
+    'make_admin_class',
+    'register',
+    'register_view',
+]

f9070835ad039fb0171ff9f6d65417a63b6dc21d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:00 +0000

    Add package OctoBot-Commons/1.6.6/OctoBot_Commons-1.6.6-cp38-cp38-win_amd64.whl

---
+++ packages/OctoBot-Commons/OctoBot_Commons-1.6.6-cp38-cp38-win_amd64.whl/octobot_commons/authentication.py
@@ -0,0 +1,103 @@
+#  Drakkar-Software OctoBot
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+import abc
+import functools
+
+
+class Authenticator:
+    """
+    Abstract class to be implemented when using authenticated requests
+    """
+
+    @abc.abstractmethod
+    def login(self, username, password):
+        """
+        Used to trigger a login
+        :param username: authentication username
+        :param password: authentication password
+        :return:
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def logout(self):
+        """
+        Used to clear a logged in session
+        :return:
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_aiohttp_session(self):
+        """
+        Get the aiohttp authenticated session
+        :return:
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def is_logged_in(self):
+        """
+        :return: True when authenticated
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def ensure_token_validity(self):
+        """
+        Called before @authenticated methods to ensure authentication
+        :return:
+        """
+        raise NotImplementedError
+
+
+class FailedAuthentication(Exception):
+    """
+    Raised upon authentication failure
+    """
+
+
+class UnavailableError(Exception):
+    """
+    Raised upon website availability issues failure
+    """
+
+
+class AuthenticationError(Exception):
+    """
+    Raised upon authentication technical error, not on login/password issues
+    """
+
+
+class AuthenticationRequired(Exception):
+    """
+    Raised when an authentication is required
+    """
+
+
+def authenticated(func):
+    """
+    Annotation to required authentication for a method call
+    :param func:
+    :return:
+    """
+
+    @functools.wraps(func)
+    def wrapped(self, *args, **kwargs):
+        self.ensure_token_validity()
+        return func(self, *args, **kwargs)
+
+    return wrapped

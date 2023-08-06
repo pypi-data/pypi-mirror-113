6445a7d2970defb2b2c4dcf98336e855e9d2ba36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:02 +0000

    Add package aws-cloud-instance-list/0.0.1/aws-cloud-instance-list-0.0.1.tar.gz

---
+++ packages/aws-cloud-instance-list/aws-cloud-instance-list-0.0.1.tar.gz/aws-cloud-instance-list-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from distutils.core import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name="aws-cloud-instance-list",
+    version="0.0.1",
+    description="Aws cloud instance list",
+    author="Kirill Shitikov",
+    author_email="sh.kiruh@gmail.com",
+    packages=setuptools.find_packages(where="cloud_instance_list"),
+    install_requires=required,
+    extras_require={
+        'dev': [
+            'pytest',
+        ]
+    },
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    entry_points={
+        "console_scripts": ["cloud-instance-list=cloud_instance_list.main:info"],
+    },
+    url='https://github.com/shitikovkirill/aws-cloud-instance-list',
+    download_url='https://github.com/shitikovkirill/aws-cloud-instance-list/archive/refs/heads/main.zip',
+)

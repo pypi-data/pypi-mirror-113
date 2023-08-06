445f9c3c6151a61d6d293d6bfaca62bb6a814a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:17 +0000

    Add package lazy-text-predict/0.0.10/lazy-text-predict-0.0.10.tar.gz

---
+++ packages/lazy-text-predict/lazy-text-predict-0.0.10.tar.gz/lazy-text-predict-0.0.10/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lazy-text-predict",
+    version="0.0.10",
+    author="Edward Booker",
+    author_email="epb378@gmail.com",
+    description="Text classification automl",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lemay-ai/lazyTextPredict",
+    packages=setuptools.find_packages(),
+    install_requires =[
+    "transformers",
+    "nlp",
+    "torch",
+    "numpy",
+    "scikit_learn",
+    "sentencepiece",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires='>=3.6',)

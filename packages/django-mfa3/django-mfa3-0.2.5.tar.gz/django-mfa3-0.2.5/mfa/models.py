919b270d00e5ba5dd35ba5ee828763cff4580d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:54 +0000

    Add package django-mfa3/0.2.5/django-mfa3-0.2.5.tar.gz

---
+++ packages/django-mfa3/django-mfa3-0.2.5.tar.gz/django-mfa3-0.2.5/mfa/models.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.db import models
+
+
+class MFAKey(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    method = models.CharField(max_length=8, choices=[
+        ('FIDO2', 'FIDO2'),
+        ('TOTP', 'TOTP'),
+    ])
+    name = models.CharField(max_length=32)
+    secret = models.TextField()
+    # replay protection
+    last_code = models.CharField(max_length=32, blank=True)

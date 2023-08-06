d330f4920bef0e91ece3924bd81905b173774785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:44 +0000

    Add package LivestockCV/0.0.0.92/LivestockCV-0.0.0.92-py3-none-any.whl

---
+++ packages/LivestockCV/LivestockCV-0.0.0.92-py3-none-any.whl/LivestockCV/_core/dimensions_.py
@@ -0,0 +1,5 @@
+# Dimensions
+
+def dimensions_(img):
+	print('Width: ', img.shape[1], ' pixel units')
+	print('Height: ', img.shape[0],' pixel units') 
---
+++ packages/LivestockCV/LivestockCV-0.0.0.92-py3-none-any.whl/LivestockCV/_core/read_.py
@@ -0,0 +1,4 @@
+import cv2
+
+def read_(img):
+	cv2.imread(img)

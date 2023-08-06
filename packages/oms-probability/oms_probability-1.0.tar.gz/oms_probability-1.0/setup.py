ce2fcaab6af14e01391ac013f658f7a9cf79a339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:05 +0000

    Add package oms-probability/1.0/oms_probability-1.0.tar.gz

---
+++ packages/oms-probability/oms_probability-1.0.tar.gz/oms_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="oms_probability",
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['oms_probability'],
+      author='Hung Om',
+      author_email='hung.workspace@gmail.com',
+      zip_safe=False)

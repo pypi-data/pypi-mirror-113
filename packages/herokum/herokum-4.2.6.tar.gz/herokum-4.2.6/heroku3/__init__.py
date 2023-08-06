2bb6f7454a60dedd16a1049c632e085c409aed84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:03 +0000

    Add package herokum/4.2.6/herokum-4.2.6.tar.gz

---
+++ packages/herokum/herokum-4.2.6.tar.gz/herokum-4.2.6/heroku3/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+#  /_/ _  _ _  /_      _
+# / / /_'/ /_//\ /_/. /_//_/
+#                    /   _/
+
+
+"""
+herokum
+~~~~~~~~~
+
+:copyright: (c) 2013 by Heroku.
+:license: MIT, see LICENSE for more details.
+"""
+
+# Meta.
+
+__title__ = "herokum"
+__author__ = "Heroku"
+__copyright__ = "Copyright 2013 Heroku"
+__license__ = "MIT"
+
+__version__ = "4.2.6"
+
+# Module namespace.
+
+# Project libraries
+from .core import from_key  # noqa

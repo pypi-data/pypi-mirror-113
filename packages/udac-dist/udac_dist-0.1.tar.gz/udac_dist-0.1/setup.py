ff0d762578cc1d3c72dfff81f9a966590b4c3ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:02 +0000

    Add package udac-dist/0.1/udac_dist-0.1.tar.gz

---
+++ packages/udac-dist/udac_dist-0.1.tar.gz/udac_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udac_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udac_dist'],
+      author = 'James Lau',
+      author_email = 'jimbo_lau@yahoo.com',
+      zip_safe=False)

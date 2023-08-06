7730383c6bb79caa8118f81f7fb3d77a70badb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:16 +0000

    Add package zswag/1.1.0rc2/zswag-1.1.0rc2-py3-none-any.whl

---
+++ packages/zswag/zswag-1.1.0rc2-py3-none-any.whl/zswag/test/calc/__init__.py
@@ -0,0 +1,8 @@
+import zserio
+from os.path import dirname, abspath
+
+working_dir = dirname(abspath(__file__))
+zserio.generate(
+    zs_dir=working_dir,
+    main_zs_file="calculator.zs",
+    gen_dir=working_dir)

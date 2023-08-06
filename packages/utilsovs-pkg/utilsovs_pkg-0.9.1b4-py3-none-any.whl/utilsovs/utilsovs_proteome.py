aeb0c32e68208e2856e24705fe43ba6797896e09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:25 +0000

    Add package utilsovs-pkg/0.9.1b4/utilsovs_pkg-0.9.1b4-py3-none-any.whl

---
+++ packages/utilsovs-pkg/utilsovs_pkg-0.9.1b4-py3-none-any.whl/utilsovs/utilsovs_proteome.py
@@ -0,0 +1,11 @@
+#utilsovs_semantic.py
+from Bio import Entrez
+from .utilsovs_commons import request_API_json
+
+def request_proteomeXchange(data):
+
+    data.url ='http://proteomecentral.proteomexchange.org/cgi/GetDataset?action=search&filterstr='
+
+    data = request_API_json(data)
+
+    return data
---
+++ packages/utilsovs-pkg/utilsovs_pkg-0.9.1b4-py3-none-any.whl/utilsovs/utilsovs_semantic.py
@@ -0,0 +1,10 @@
+#utilsovs_semantic.py
+from .utilsovs_commons import request_API_json
+
+def request_SemanticScholar_API(data):
+
+    data.url ='https://api.semanticscholar.org/v1/paper/PMID:'
+
+    data = request_API_json(data)
+
+    return data

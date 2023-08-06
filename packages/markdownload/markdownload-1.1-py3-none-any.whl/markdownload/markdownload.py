7a712072986e43b2792a1556097d6beca944c9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:04 +0000

    Add package markdownload/1.1/markdownload-1.1-py3-none-any.whl

---
+++ packages/markdownload/markdownload-1.1-py3-none-any.whl/markdownload/markdownload.py
@@ -0,0 +1,17 @@
+""" This module provides functions to download the media file(s) in the markdown file(s). """
+
+
+class Downloader:
+    """ A simple Downloader for download download the media file(s) in the markdown file(s). """
+
+    def download(self, path):
+        """
+        download the media file(s) in the markdown file(s)
+
+        Parameters: 
+        path (str): The path to the markdown file(s).
+
+        Returns:
+        num: The num of markdown files processed successfully.
+        """
+        print("download")

551710de3d991c5d908e241982567980ff2c4bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:39 +0000

    Add package gslpy/0.0.0/gslpy-0.0.0.tar.gz

---
+++ packages/gslpy/gslpy-0.0.0.tar.gz/gslpy-0.0.0/gslpy/__init__.py
@@ -0,0 +1,5 @@
+"""
+GNU Scientific Library for Python
+"""
+
+__version__ = "0.0.0"
---
+++ packages/gslpy/gslpy-0.0.0.tar.gz/gslpy-0.0.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="gslpy",
+    version="0.0.0",
+    author="André Bienemann",
+    author_email="andre.bienemann@gmail.com",
+    description="GNU Scientific Library for Python",
+    packages=setuptools.find_packages(),
+)

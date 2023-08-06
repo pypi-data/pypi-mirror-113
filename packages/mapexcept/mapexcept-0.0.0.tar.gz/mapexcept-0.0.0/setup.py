3db92060a31056990018b8da91e84b911c1f91ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:45 +0000

    Add package mapexcept/0.0.0/mapexcept-0.0.0.tar.gz

---
+++ packages/mapexcept/mapexcept-0.0.0.tar.gz/mapexcept-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='mapexcept',
+    packages=["mapexcept"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com',
+    description="Please email me if you would like this project name"
+)

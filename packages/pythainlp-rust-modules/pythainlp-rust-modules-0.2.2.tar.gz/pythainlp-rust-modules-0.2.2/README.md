608f016c1ece224c97afdc74f52ade5a520a4b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:34 +0000

    Add package pythainlp-rust-modules/0.2.2/pythainlp-rust-modules-0.2.2.tar.gz

---
+++ packages/pythainlp-rust-modules/pythainlp-rust-modules-0.2.2.tar.gz/pythainlp-rust-modules-0.2.2/README.md
@@ -0,0 +1,11 @@
+# pythainlp-rust-modules is now nlpo3
+
+This package has been renamed. Use `pip install nlpo3` instead.
+
+New package: https://pypi.org/project/nlpo3/
+
+## Migration
+
+For existing code, change `import oxidized_thainlp` to `import nlpo3`.
+
+All the function names remain the same.
---
+++ packages/pythainlp-rust-modules/pythainlp-rust-modules-0.2.2.tar.gz/pythainlp-rust-modules-0.2.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import os
+
+VERSION = "0.2.2"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+setup(
+    name="pythainlp-rust-modules",
+    description="pythainlp-rust-modules is now nlpo3",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    version=VERSION,
+    install_requires=["nlpo3"],  # new package name
+    python_requires=">=3.6",
+    license="Apache-2.0",
+    url="https://github.com/PyThaiNLP/nlpo3/",
+    classifiers=["Development Status :: 7 - Inactive"],
+)

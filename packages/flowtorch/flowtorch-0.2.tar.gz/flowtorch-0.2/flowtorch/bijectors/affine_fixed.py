584ea7bd8c031dda4b9dad1e3f298ea05ec7cec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:29 +0000

    Add package flowtorch/0.2/flowtorch-0.2.tar.gz

---
+++ packages/flowtorch/flowtorch-0.2.tar.gz/flowtorch-0.2/flowtorch/bijectors/affine_fixed.py
@@ -0,0 +1,44 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# SPDX-License-Identifier: MIT
+
+import math
+from typing import Optional
+
+import torch
+from flowtorch.bijectors.base import Bijector
+
+
+class AffineFixed(Bijector):
+    r"""
+    Elementwise bijector via the affine mapping :math:`\mathbf{y} = \mu +
+    \sigma \otimes \mathbf{x}` where $\mu$ and $\sigma$ are fixed rather than
+    learnable.
+    """
+
+    # TODO: Handle non-scalar loc and scale with correct broadcasting semantics
+    def __init__(self, loc=0.0, scale=1.0) -> None:
+        super().__init__(param_fn=None)
+        self.loc = loc
+        self.scale = scale
+
+    def _forward(
+        self,
+        x: torch.Tensor,
+        context: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        return self.loc + self.scale * x
+
+    def _inverse(
+        self,
+        y: torch.Tensor,
+        context: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        return (y - self.loc) / self.scale
+
+    def _log_abs_det_jacobian(
+        self,
+        x: torch.Tensor,
+        y: torch.Tensor,
+        context: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        return torch.full_like(x, math.log(abs(self.scale)))
---
+++ packages/flowtorch/flowtorch-0.2.tar.gz/flowtorch-0.2/flowtorch/distributions/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# SPDX-License-Identifier: MIT
+
+from flowtorch.distributions.transformed_distribution import TransformedDistribution
+
+__all__ = [
+    "TransformedDistribution",
+]

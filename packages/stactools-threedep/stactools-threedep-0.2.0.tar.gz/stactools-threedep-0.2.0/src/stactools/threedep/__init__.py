299c6e24b32df6b358d422980f7107228f319f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:02 +0000

    Add package stactools-threedep/0.2.0/stactools-threedep-0.2.0.tar.gz

---
+++ packages/stactools-threedep/stactools-threedep-0.2.0.tar.gz/stactools-threedep-0.2.0/src/stactools/threedep/__init__.py
@@ -0,0 +1,15 @@
+import stactools.core
+
+from stactools.threedep.metadata import Metadata
+
+stactools.core.use_fsspec()
+
+
+def register_plugin(registry):
+    from stactools.threedep import commands
+    registry.register_subcommand(commands.create_threedep_command)
+
+
+__all__ = [Metadata]
+__version__ = "0.2.0"
+"""Library version"""

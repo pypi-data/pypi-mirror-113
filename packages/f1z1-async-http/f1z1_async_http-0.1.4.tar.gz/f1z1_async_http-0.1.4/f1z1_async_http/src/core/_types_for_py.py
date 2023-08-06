de826f9fd91b3db2325de4cba55b3481b69c108c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:34 +0000

    Add package f1z1-async-http/0.1.4/f1z1_async_http-0.1.4.tar.gz

---
+++ packages/f1z1-async-http/f1z1_async_http-0.1.4.tar.gz/f1z1_async_http-0.1.4/f1z1_async_http/src/core/_types_for_py.py
@@ -0,0 +1,11 @@
+# @Time     : 2021/5/30
+# @Project  : f1z1-g
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+from httpx import Request, Response, URL, AsyncClient
+
+HttpxClient = AsyncClient
+HttpxRequest = Request
+HttpxResponse = Response
+HttpxURL = URL

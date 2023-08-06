6c66f83f893d0e0b5969e644522ff112d552e4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:17 +0000

    Add package wix-media-platform/1.56.0/wix_media_platform-1.56.0.tar.gz

---
+++ packages/wix-media-platform/wix_media_platform-1.56.0.tar.gz/wix_media_platform-1.56.0/media_platform/service/sanitation_service/sanitation_service.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from media_platform.auth.app_authenticator import AppAuthenticator
+from media_platform.http_client.authenticated_http_client import AuthenticatedHTTPClient
+from media_platform.service.sanitation_service.sanitation_request import SanitationRequest
+from media_platform.service.media_platform_service import MediaPlatformService
+
+
+class SanitationService(MediaPlatformService):
+    def __init__(self, domain: str, authenticated_http_client: AuthenticatedHTTPClient,
+                 app_authenticator: AppAuthenticator):
+        super().__init__(domain, authenticated_http_client)
+        self.app_authenticator = app_authenticator
+
+    def sanitation_request(self) -> SanitationRequest:
+        return SanitationRequest(self._authenticated_http_client, self._base_url)

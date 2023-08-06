bc702da6062cf2f19a4067739f03aa83094630f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:21 +0000

    Add package anchorage/0.9.0.4/anchorage-0.9.0.4.tar.gz

---
+++ packages/anchorage/anchorage-0.9.0.4.tar.gz/anchorage-0.9.0.4/tests/test_local.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+import unittest
+
+from anchorage import add_local as add, server
+from anchorage.anchor_tools.local import setup, init
+
+
+class Tests(unittest.TestCase):
+
+    def test_all(self):
+        t_setup()
+        t_init()
+        t_add()
+        t_server()
+
+
+test_archive = r"C:\Users\xXY4n\Projects\Anchorage\tests\test_archive"
+
+
+def t_setup():
+    print("LOCAL ARCHIVE TESTS: SETUP")
+    if os.path.isdir(test_archive):
+        shutil.rmtree(test_archive)
+    setup(archive=test_archive)
+
+
+def t_init():
+    print("LOCAL ARCHIVE TESTS: INITIALIZATION")
+    init(archive=test_archive)
+
+
+def t_add():
+    print("LOCAL ARCHIVE TESTS: ADDITION")
+    add(url="http://larsblackmore.com/AcikmeseBlackmoreAutomatica10.pdf",
+        archive=test_archive)
+
+
+def t_server():
+    print("LOCAL ARCHIVE TESTS: SERVER")
+    server(archive=test_archive, instakill=True)

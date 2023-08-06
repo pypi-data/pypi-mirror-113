beb5648229e68d0281c826c32defb7de5a2e4637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:48 +0000

    Add package cloudrail-knowledge/0.0.68b15/cloudrail-knowledge-0.0.68b15.tar.gz

---
+++ packages/cloudrail-knowledge/cloudrail-knowledge-0.0.68b15.tar.gz/cloudrail-knowledge-0.0.68b15/cloudrail/knowledge/context/azure/webapp/azure_app_service_config.py
@@ -0,0 +1,31 @@
+from typing import Optional, List
+
+from cloudrail.knowledge.context.azure.azure_resource import AzureResource
+from cloudrail.knowledge.context.azure.constants.azure_resource_type import AzureResourceType
+from cloudrail.knowledge.context.azure.webapp.constants import FtpsState
+
+
+class AzureAppServiceConfig(AzureResource):
+    """
+        Attributes:
+            name: The name of the AppService to which this config belongs
+            ftps_state: The FTPS state defined in this config. Either AllAllowed, FTPSOnly or Disabled
+    """
+    def __init__(self, name: str, ftps_state: FtpsState) -> None:
+        super().__init__(AzureResourceType.NONE)
+        self.name: str = name
+        self.ftps_state: FtpsState = ftps_state
+
+    def get_keys(self) -> List[str]:
+        return [self.get_name()]
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_cloud_resource_url(self) -> Optional[str]:
+        return f'https://portal.azure.com/#@{self.tenant_id}/resource/subscriptions/{self.subscription_id}/resourceGroups/' \
+               f'{self.resource_group_name}/providers/Microsoft.Web/sites/{self.name}/configuration'
+
+    @property
+    def is_tagable(self) -> bool:
+        return False

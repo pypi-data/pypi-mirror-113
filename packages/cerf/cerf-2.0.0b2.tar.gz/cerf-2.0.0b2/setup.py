a38d707c953ee800561ccad62ab2f8628371a6a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:39 +0000

    Add package cerf/2.0.0b2/cerf-2.0.0b2.tar.gz

---
+++ packages/cerf/cerf-2.0.0b2.tar.gz/cerf-2.0.0b2/setup.py
@@ -0,0 +1,45 @@
+import re
+from setuptools import setup, find_packages
+
+
+def readme():
+    """Return the contents of the project README file."""
+    with open('README.md') as f:
+        return f.read()
+
+
+def get_requirements():
+    """Return a list of package requirements from the requirements.txt file."""
+    with open('requirements.txt') as f:
+        return f.read().split()
+
+
+version = re.search(r"__version__ = ['\"]([^'\"]*)['\"]", open('cerf/__init__.py').read(), re.M).group(1)
+
+setup(
+    name='cerf',
+    version=version,
+    packages=find_packages(),
+    url='https://github.com/IMMM-SFA/cerf',
+    download_url=f'https://github.com/IMMM-SFA/cerf/archive/refs/tags/v{version}.tar.gz',
+    license='BSD2-Simplified',
+    author='Chris R. Vernon; Nino Zuljevic',
+    author_email='chris.vernon@pnnl.gov',
+    description='An open-source geospatial Python package for assessing and analyzing future electricity technology capacity expansion feasibility',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    python_requires='>=3.7.*, <4',
+    include_package_data=True,
+    install_requires=get_requirements(),
+    extras_require={
+        'dev': [
+            'build~=0.5.1',
+            'nbsphinx~=0.8.6',
+            'setuptools~=57.0.0',
+            'sphinx~=4.0.2',
+            'sphinx-panels~=0.6.0',
+            'sphinx-rtd-theme~=0.5.2',
+            'twine~=3.4.1'
+        ]
+    }
+)

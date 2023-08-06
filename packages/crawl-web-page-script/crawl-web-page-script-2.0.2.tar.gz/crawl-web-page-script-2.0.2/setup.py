190ca0a4b103c00b9d1ca4e313b33d3646c9b517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:58 +0000

    Add package crawl-web-page-script/2.0.2/crawl-web-page-script-2.0.2.tar.gz

---
+++ packages/crawl-web-page-script/crawl-web-page-script-2.0.2.tar.gz/crawl-web-page-script-2.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+     name="crawl-web-page-script",  
+     version="2.0.2",                          
+     py_modules=["webcrawl_pack"],
+     python_requires='>=3.6, <4',  
+     install_requires=[
+          'requests',
+          'json',
+          'logging'
+      ],             
+)

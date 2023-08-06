6def9cdecfca76133307a54575c7c9c58baa49e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:49 +0000

    Add package odoo14-addon-remove-button-access/14.0.1.0.1/odoo14_addon_remove_button_access-14.0.1.0.1-py3-none-any.whl

---
+++ packages/odoo14-addon-remove-button-access/odoo14_addon_remove_button_access-14.0.1.0.1-py3-none-any.whl/odoo/addons/remove_button_access/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021-Coopdevs Treball SCCL (<https://coopdevs.org>)
+# - César López Ramírez - <cesar.lopez@coopdevs.org>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Remove portal button access from email templates",
+    "version": "14.0.1.0.1",
+    "depends": ["mail"],
+    "author": "Coopdevs Treball SCCL",
+    "category": "Accounting & Finance",
+    "website": "https://coopdevs.org",
+    "license": "AGPL-3",
+    "summary": """
+        Remove the button "View ..." from chatter email templates
+    """,
+    "data": ['data/mail_data.xml'],
+    "installable": True,
+}

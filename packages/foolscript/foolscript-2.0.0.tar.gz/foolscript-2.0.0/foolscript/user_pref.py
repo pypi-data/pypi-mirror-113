bb462b2a9f9ae395604dccdaed1c5da1a70b5f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:05 +0000

    Add package foolscript/2.0.0/foolscript-2.0.0.tar.gz

---
+++ packages/foolscript/foolscript-2.0.0.tar.gz/foolscript-2.0.0/foolscript/user_pref.py
@@ -0,0 +1,9 @@
+working_project_directory = "yourprojects/"
+
+# change the yourprojects/ to your directory
+
+# IMPORTANT
+
+# ADD A FORWARD SLASH (/) AFTER THE END OF THE DIRECTORY.
+
+# After changing the directory restart the terminal session for applying the changes.

f14128fcac15de561830b67198817921b5ab5491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:28 +0000

    Add package dpgen/0.10.0/dpgen-0.10.0.tar.gz

---
+++ packages/dpgen/dpgen-0.10.0.tar.gz/dpgen-0.10.0/dpgen/generator/lib/MEAM.py
@@ -0,0 +1,20 @@
+from LAMMPS import LAMMPS
+
+class MEAM_LMP (LAMMPS):
+    def __init__ (self, 
+                  paramters, 
+                  path_to_poscar) :
+        pass
+
+    def make_potential_files(self, 
+                             output_dir):
+        pass
+
+    def make_input_file(self,
+                        output_dir, 
+                        task_type, 
+                        task_param):
+        pass
+
+    def post (self):
+        pass

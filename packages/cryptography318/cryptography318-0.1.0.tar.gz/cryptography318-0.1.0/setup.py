81ad7fa9d9cdf4c4e06fb2e1f1a1eb6b55b20c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:45 +0000

    Add package cryptography318/0.1.0/cryptography318-0.1.0.tar.gz

---
+++ packages/cryptography318/cryptography318-0.1.0.tar.gz/cryptography318-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='cryptography318',
+    version='0.1.0',
+    description='A set of functions useful in cryptography318',
+    url='https://github.com/aarpyy/Cryptography',
+    author='Andrew Carpenter',
+    author_email='acarpent@oberlin.edu',
+    license='BSD 2-clause',
+    packages=['cryptography318'],
+    install_requires=['numpy',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)

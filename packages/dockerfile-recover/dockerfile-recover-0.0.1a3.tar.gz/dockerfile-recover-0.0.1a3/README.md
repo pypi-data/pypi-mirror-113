0a2452e1f67a146ea7e50564057f9da1fa63b334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:23 +0000

    Add package dockerfile-recover/0.0.1a3/dockerfile-recover-0.0.1a3.tar.gz

---
+++ packages/dockerfile-recover/dockerfile-recover-0.0.1a3.tar.gz/dockerfile-recover-0.0.1a3/README.md
@@ -0,0 +1,21 @@
+# dockerfile-recover
+dockerfile-recover is a tool to reconstruct Dockerfile by reverse engineering a docker image
+
+# Installation
+```
+pip install dockerfile-recover
+```
+
+# Usage
+
+```
+docker pull <the image you want to reverse engineer>
+dockerfile-recover <image name>
+```
+For example
+```
+dockerfile-recover python
+dockerfile-recover django
+dockerfile-recover redis
+dockerfile-recover node
+```
---
+++ packages/dockerfile-recover/dockerfile-recover-0.0.1a3.tar.gz/dockerfile-recover-0.0.1a3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dockerfile_recover', 'dockerfile_recover.parser']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.1,<9.0.0', 'docker>=5.0.0,<6.0.0', 'six>=1.16.0,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['dockerfile-recover = dockerfile_recover.__main__:main']}
+
+setup_kwargs = {
+    'name': 'dockerfile-recover',
+    'version': '0.0.1a3',
+    'description': 'dockerfile-recover is a tool to reconstruct Dockerfile by reverse engineering a docker image',
+    'long_description': '# dockerfile-recover\ndockerfile-recover is a tool to reconstruct Dockerfile by reverse engineering a docker image\n\n# Installation\n```\npip install dockerfile-recover\n```\n\n# Usage\n\n```\ndocker pull <the image you want to reverse engineer>\ndockerfile-recover <image name>\n```\nFor example\n```\ndockerfile-recover python\ndockerfile-recover django\ndockerfile-recover redis\ndockerfile-recover node\n```\n',
+    'author': None,
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/purificant/dockerfile-recover',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)

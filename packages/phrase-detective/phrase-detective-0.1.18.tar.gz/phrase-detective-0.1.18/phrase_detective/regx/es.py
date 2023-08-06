3473489dde10c01f70e5453955c9837409a53724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:13 +0000

    Add package phrase-detective/0.1.18/phrase-detective-0.1.18.tar.gz

---
+++ packages/phrase-detective/phrase-detective-0.1.18.tar.gz/phrase-detective-0.1.18/phrase_detective/regx/es.py
@@ -0,0 +1,42 @@
+"""Define regular expression usage of ``spacy.matcher.Matcher`` for ``Spanish``
+"""
+
+
+# NP noun phrase pattern
+NP = [
+  [{"POS": "DET"}, {"POS": "ADJ", "OP": "?"}, {"POS": "NOUN"}, {"POS": "ADV", "OP": "?"}, {"POS": "ADJ"}],
+  [{"POS": "DET"}, {"POS": "ADJ", "OP": "?"}, {"POS": "NOUN"}, {"POS": "ADP"}, {"POS": "DET", "OP": "?"}, {"POS": "NOUN", "DEP": "nmod"}],
+  [{"POS": "DET", "OP": "?"}, {"POS": "NUM"}, {"POS": "ADJ", "OP": "?"}, {"POS": "NOUN"}],
+  [{"POS": "DET", "OP": "+"}, {"POS": "ADJ", "OP": "?"}, {"POS": "NOUN"}],
+  [{"POS": "NOUN"}, {"POS": "ADJ"}],
+]
+
+# PP preposition phrase pattern
+PP = [
+  [{"POS": "ADP"}, {"POS": "DET", "OP": "?"}, {"POS": "ADJ", "OP": "?"}, {"POS": "NUM", "OP": "?"}, {"POS": "NOUN"}],
+  [{"POS": "ADP"}, {"POS": "ADP"}, {"POS": "DET", "OP": "?"}, {"POS": "ADJ", "OP": "?"}, {"POS": "NOUN"}, {"POS": "ADJ", "OP": "?"}],
+  [{"POS": "ADP"}, {"POS": "DET", "OP": "?"}, {"POS": "ADJ", "OP": "?"}, {"POS": "NOUN"}, {"POS": "ADJ", "OP": "?"}],
+  [{"POS": "ADP"}, {"POS": "DET", "OP": "?"}, {"POS": "PROPN"}],
+  [{"POS": "ADP"}, {"POS": "ADV"}, {"POS": "ADP", "OP": "?"}],
+  [{"POS": "ADP"}, {"POS": "PRON"}],
+  [{"POS": "ADP"}, {"POS": "ADJ"}],
+]
+
+# VERB Knowledge pattern
+VERB = [
+  [{"POS": "VERB"}],
+  [{"POS": "AUX"}],
+]
+
+VERB_PASSIVE = [
+  [{"LEMMA": "ser"}, {"TAG": {"REGEX": "VERB_.*Tense=Past|VerbForm=Part"}}],
+  [{"LOWER": "se", "DEP": "obj"}, {"TAG": {"REGEX": "VERB_.*"}}],
+]
+
+
+es_regx = {
+  "verb": VERB,
+  "verb_passive": VERB_PASSIVE,
+  "pp": PP,
+  "np": NP,
+}

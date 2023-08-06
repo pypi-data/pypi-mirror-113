00647168f2e9ad26c7ecec071e084c7be5da8754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:33 +0000

    Add package probability-dists-ds/0.1/probability_dists_ds-0.1.tar.gz

---
+++ packages/probability-dists-ds/probability_dists_ds-0.1.tar.gz/probability_dists_ds-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'probability_dists_ds',
+      version = '0.1',
+      description = 'Gaussian and Binomial Probability Distributions',
+      packages = ['probability_dists_ds'],
+      author = "Sunil Raut Kshetri",
+      author_email = "myemail@something.com",
+      zip_safe = False)

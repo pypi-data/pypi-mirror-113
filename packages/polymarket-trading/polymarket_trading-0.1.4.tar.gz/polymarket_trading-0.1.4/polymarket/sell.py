4e3e3c66981092db4e1efbfc3196b5eae30ee7ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:16 +0000

    Add package polymarket-trading/0.1.4/polymarket_trading-0.1.4.tar.gz

---
+++ packages/polymarket-trading/polymarket_trading-0.1.4.tar.gz/polymarket_trading-0.1.4/polymarket/sell.py
@@ -0,0 +1,17 @@
+from .utils import conditional_token_approve_for_all, load_evm_abi
+
+
+def sell(web3_provider, market_maker_address, return_amount, index, maximum_shares):
+    fixed_product_market_maker_address_abi = load_evm_abi('FixedProductMarketMaker.json')
+
+    fixed_return_amount = int(return_amount * (10**6))
+    fixed_maximum_shares = int(maximum_shares * (10**6))
+
+    conditional_token_approve_for_all(web3_provider, market_maker_address, True)
+    contract = web3_provider.eth.contract(address=market_maker_address, abi=fixed_product_market_maker_address_abi)
+    trx_hash = contract.functions.sell(fixed_return_amount, index, fixed_maximum_shares).transact()
+    web3_provider.eth.wait_for_transaction_receipt(trx_hash)
+
+    conditional_token_approve_for_all(web3_provider, market_maker_address, False)
+
+    return trx_hash

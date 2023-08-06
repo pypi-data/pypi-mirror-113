07b30f2ab9c7e2a8e09112968ed75b71dbcb92fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:51 +0000

    Add package surepy/0.7.0/surepy-0.7.0.tar.gz

---
+++ packages/surepy/surepy-0.7.0.tar.gz/surepy-0.7.0/surepy/enums.py
@@ -0,0 +1,67 @@
+from enum import IntEnum
+
+
+class SureEnum(IntEnum):
+    """Sure base enum."""
+
+    def __str__(self) -> str:
+        return self.name.title()  # pylint: disable=no-member
+
+
+class EntityType(SureEnum):
+    """Sure Entity Types."""
+
+    PET = 0  # artificial ID, not used by the Sure Petcare API
+    HUB = 1  # Hub
+    REPEATER = 2  # Repeater
+    PET_FLAP = 3  # Pet Door Connect
+    FEEDER = 4  # Microchip Pet Feeder Connect
+    PROGRAMMER = 5  # Programmer
+    CAT_FLAP = 6  # Cat Flap Connect
+    FEEDER_LITE = 7  # Feeder Lite
+    FELAQUA = 8  # Felaqua Connect
+
+
+class LockState(SureEnum):
+    """Sure Petcare API State IDs."""
+
+    UNLOCKED = 0
+    LOCKED_IN = 1
+    LOCKED_OUT = 2
+    LOCKED_ALL = 3
+    CURFEW = 4
+    CURFEW_LOCKED = -1
+    CURFEW_UNLOCKED = -2
+    CURFEW_UNKNOWN = -3
+
+
+class Location(SureEnum):
+    """Sure Locations."""
+
+    INSIDE = 1
+    OUTSIDE = 2
+    UNKNOWN = -1
+
+
+class FoodType(SureEnum):
+    """Sure Food Types."""
+
+    WET = 1
+    DRY = 2
+    BOTH = 3
+    UNKNOWN = -1
+
+
+class BowlPosition(SureEnum):
+    """Sure Feeder Bowl side."""
+
+    LEFT = 0
+    RIGHT = 1
+
+
+# animal related
+class Species(SureEnum):
+    """Sure Feeder Bowl side."""
+
+    CAT = 1
+    DOG = 2

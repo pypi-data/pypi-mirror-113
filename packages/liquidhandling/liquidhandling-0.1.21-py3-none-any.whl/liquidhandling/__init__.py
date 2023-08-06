adda490f3a9aa9daf79d809790d52031f188e944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:50 +0000

    Add package liquidhandling/0.1.21/liquidhandling-0.1.21-py3-none-any.whl

---
+++ packages/liquidhandling/liquidhandling-0.1.21-py3-none-any.whl/liquidhandling/__init__.py
@@ -0,0 +1,6 @@
+from hudson import SoloSoft
+from hudson import SoftLinx
+from hudson import CherryPicking
+from .Properties import Properties
+from .VolumeManager import VolumeManager
+from .Plates import *

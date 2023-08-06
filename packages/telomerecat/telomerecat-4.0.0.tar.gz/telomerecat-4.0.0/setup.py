94949ecf2ae37722fb375826f53c67261c1600d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:55 +0000

    Add package telomerecat/4.0.0/telomerecat-4.0.0.tar.gz

---
+++ packages/telomerecat/telomerecat-4.0.0.tar.gz/telomerecat-4.0.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+  name="telomerecat",
+  version='4.0.0',
+  description="Telomere Computational Analysis Tool",
+  url='https://github.com/cancerit/telomerecat',
+  author="JHR Farmery",
+  license="GPL",
+  python_requires='>= 3.6',
+  author_email="cgphelp@sanger.ac.uk",
+  packages=["telomerecat"],
+  package_dir={"telomerecat": "telomerecat"},
+  install_requires=["parabam>=3.0.0", "numpy", "pysam", "pandas", "click"],
+  include_package_data=True,
+  scripts=["./telomerecat/bin/telomerecat"],
+  entry_points={'console_scripts': ['pysam_collate=telomerecat.pysam_collate:thin_wrap'],},
+  zip_safe=False,
+
+)

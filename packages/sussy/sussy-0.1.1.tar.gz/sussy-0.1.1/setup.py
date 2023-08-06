c496470bde474730910016cf7c8cfa6cc06266c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:21 +0000

    Add package sussy/0.1.1/sussy-0.1.1.tar.gz

---
+++ packages/sussy/sussy-0.1.1.tar.gz/sussy-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = "sussy",
+    packages = ["sussy"],
+    version = "0.1.1",
+    description = "when the python developer is sus",
+    long_description="when the impostor is sus\nwhen the suspostor is imp\n\nwhen the python developer is sus\nwhen the susthon developer is py",
+    author = "Flamey",
+    author_email="morellialex08@gmail.com",
+    url = "https://github.com/ActuallyFlamey/sussy/",
+    download_url = "https://github.com/ActuallyFlamey/sussy/archive/refs/tags/0.1.0.tar.gz"
+)

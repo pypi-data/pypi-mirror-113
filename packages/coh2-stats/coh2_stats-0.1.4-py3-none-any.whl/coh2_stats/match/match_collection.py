bc3b50e4fa2a268ed2e9a00e39faaff1ec2fb2f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:37 +0000

    Add package coh2-stats/0.1.4/coh2_stats-0.1.4-py3-none-any.whl

---
+++ packages/coh2-stats/coh2_stats-0.1.4-py3-none-any.whl/coh2_stats/match/match_collection.py
@@ -0,0 +1,66 @@
+from coh2_stats.match.match_statistics import MatchStatistics
+from coh2_stats.query.coh2_io import Coh2IO
+from coh2_stats.utils.serialization import PickleSerialization
+
+class MatchCollection(PickleSerialization):
+    def __init__(self, collection_name: str, matches = []) -> str:
+        self.matches:list[MatchStatistics] = matches
+        self.collection_name = collection_name
+        super().__init__("match_collection", collection_name)
+
+    @property
+    def length(self):
+        return len(self.matches)
+
+    def save_to_csv(self):
+        io = Coh2IO("output", "csv")
+        data = '\n'.join([str(match) for match in self.matches])
+        io.write_str(data, self.collection_name)
+
+    def add(self, match):
+        self.matches.append(match)
+
+    def add_list(self, matches):
+        self.matches.extend(matches)
+
+    def filter_by_playercard_collection(self, playercard_ollection):
+        predicate = lambda match: match.detail.is_qualified(playercard_ollection)
+        self.filter(predicate)
+        return self
+
+    def filter_to_today_only(self):
+        import datetime
+        today = datetime.datetime.now()
+        today_repr = tuple(int(x) for x in today.strftime("%m/%d").split('/'))
+        predicate = lambda match: match.time.in_range_date(today_repr, today_repr)
+        self.filter(predicate)
+        return self
+
+    def filter(self, predicate):
+        result = []
+        for match in self.matches:
+            if predicate(match):
+                result.append(match)
+        self.matches = result
+        return self
+
+    def __repr__(self) -> str:
+        info = f"{self.collection_name}: totally {len(self.matches)}:\n" 
+        date_match_dict = dict()
+        for match in self.matches:
+
+            match_date = match.time.date()
+            if match_date not in date_match_dict:
+                date_match_dict[match_date] = []
+
+            date_match_dict[match_date].append(match)
+
+        import collections
+        ordered_dict = collections.OrderedDict(sorted(date_match_dict.items()))
+
+        for match_date, match_list in ordered_dict.items():
+            info += f"{match_date}: {len(match_list)}\n"
+
+        return info
+
+

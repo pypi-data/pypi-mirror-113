6f0278cc3670662398a6ee8e7142e648ce6ba8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:15 +0000

    Add package crc-ct/1.1.0rc4/crc-ct-1.1.0rc4.zip

---
+++ packages/crc-ct/crc-ct-1.1.0rc4.zip/crc-ct-1.1.0rc4/src/crc/__about__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 1994-2021 Adam Karpierz
+# Licensed under the zlib/libpng License
+# https://opensource.org/licenses/Zlib
+
+__import__("pkg_about").about("crc-ct")
+__copyright__ = f"Copyright (c) 1994-2021 {__author__}"

42997d12ed6c69f8f601b63dc9a0fc629d4f199b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:24 +0000

    Add package dse-data-loader/0.0.4/dse-data-loader-0.0.4.tar.gz

---
+++ packages/dse-data-loader/dse-data-loader-0.0.4.tar.gz/dse-data-loader-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import os
+
+VERSION = "0.0.4"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="dse-data-loader",
+    description="dse-data-loader is now stocksurferbd",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    version=VERSION,
+    install_requires=["stocksurferbd"],
+    classifiers=["Development Status :: 7 - Inactive"],
+)

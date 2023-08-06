9862766d758169b711cab06ef19ebea07c15b1ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:01 +0000

    Add package bob.ip.stereo/1.0.0/bob.ip.stereo-1.0.0.zip

---
+++ packages/bob.ip.stereo/bob.ip.stereo-1.0.0.zip/bob.ip.stereo-1.0.0/bob/ip/stereo/utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2 as cv
+
+def convert_to_uint8(img, normalize=False):
+    if img.dtype == np.uint16:
+        if normalize:
+            cv.normalize(img, img, 0, 65535, cv.NORM_MINMAX)
+        img = (img / 256).astype('uint8')
+    else:
+        if normalize:
+            cv.normalize(img, img, 0, 255, cv.NORM_MINMAX)
+    return img

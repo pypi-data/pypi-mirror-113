0a2734ea5d0955fa1fa811a01bffec6145b51402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:12 +0000

    Add package cmdcolorprint/1.0.6/cmdcolorprint-1.0.6-py3-none-any.whl

---
+++ packages/cmdcolorprint/cmdcolorprint-1.0.6-py3-none-any.whl/cmdcolorprint/__init__.py
@@ -0,0 +1,23 @@
+from . import color_print
+import sys
+
+def cprint():
+    try:
+        text=sys.argv[1]
+        color_name=sys.argv[2]
+    except:
+        text="Hello World!"
+        color_name="green"
+    color_print.Color.output_color.cprint_name(text,color_name)
+
+def colorprint(text,color_name):
+    color_print.Color.output_color.cprint_name(text,color_name)
+
+def codecprint(text,color_name):
+    color_print.Color.output_color.color_print(text,color_name)
+
+def setcolor_name(color_name):
+    color_print.Color.output_color.setcolor(color_name)
+
+def resetcolor():
+    color_print.Color.output_color.resetcolor()

3b874f809cb563cfc88148da08b6ac70f98dc9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:04 +0000

    Add package ailever/0.2.254/ailever-0.2.254-py3-none-any.whl

---
+++ packages/ailever/ailever-0.2.254-py3-none-any.whl/ailever/analysis/exploratory_data_analysis.py
@@ -0,0 +1,62 @@
+import os
+import pandas as pd
+
+
+class ExploratoryDataAnalysis:
+    def __init__(self, frame):
+        self.frame = frame
+
+    def cleaning(self):
+        pass
+
+    def frequency(self):
+        pass
+
+    def transformation(self):
+        pass
+
+    def selection(self):
+        pass
+
+    def visualization(self):
+        pass
+
+
+class Counting:
+    def __init__(self, frame, path='ExploratoryDataAnalysis'):
+        self.frame = frame
+        self.path = path
+
+    def CountUniqueValues(self, view='table', save=False, path=None):
+        if view == 'table':
+            data = list()
+            for column in self.frame.columns:
+                data.append([column, self.frame.value_counts(column).shape[0]])
+            EDAframe = pd.DataFrame(data, columns=['Column', 'NumUniqueInstance'])
+            _saving(EDAframe, save, self.path, path, 'CountColumns.csv')
+
+        elif view == 'column':
+            EDAframe = pd.DataFrame(columns=['Column', 'Instance', 'Count'])
+            for column in self.frame.columns:
+                base = self.frame[column].value_counts().reset_index().rename(columns={'index':'Instance', column:'Count'})
+                base.insert(0, 'Column', column)
+                EDAframe = EDAframe.append(base)
+            _saving(EDAframe, save, self.path, path, 'CountInstances.csv')
+
+        return EDAframe
+
+
+def _saving(frame, save, default_path, priority_path, name):
+    if save:
+        if not priority_path:
+            if not os.path.isdir(default_path):
+                os.mkdir(default_path)
+            frame.to_csv(os.path.join(default_path, name))
+            print(f'[AILEVER] The file {name} is saved at {default_path}.')
+
+        else:
+            if not os.path.isdir(priority_path):
+                os.mkdir(priority_path)
+            frame.to_csv(os.path.join(priority_path, name))
+            print(f'[AILEVER] The file {name} is saved at {priority_path}.')
+

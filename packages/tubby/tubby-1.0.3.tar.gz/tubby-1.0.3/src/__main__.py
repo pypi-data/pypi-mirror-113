b191ddc227a43a919e298b339d39036223433f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:49 +0000

    Add package tubby/1.0.3/tubby-1.0.3.tar.gz

---
+++ packages/tubby/tubby-1.0.3.tar.gz/tubby-1.0.3/src/__main__.py
@@ -0,0 +1,43 @@
+"""This module defines the behaviour of the package
+in a top-level script environment.
+
+The following command executes this script:
+
+```bash
+python -m tubby
+```
+"""
+
+
+import click
+
+
+from .analyze import analyze
+from .backup import backup
+from .download import download
+from .info import info
+from .manage import manage
+from .meta import DESCRIPTION
+from .reset import reset
+
+
+@click.group(
+    help=DESCRIPTION,
+    options_metavar="[options]",
+    subcommand_metavar="<command> [args]",
+    context_settings=dict(help_option_names=["-h", "--help"]),
+)
+def main():
+    """The main program."""
+
+
+main.add_command(download)
+main.add_command(manage)
+main.add_command(analyze)
+main.add_command(backup)
+main.add_command(reset)
+main.add_command(info)
+
+
+if __name__ == "__main__":
+    main()

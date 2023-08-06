74e9940029c3e4ab5b537f3930949208abc1d5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:55 +0000

    Add package LivestockCV/0.0.0.32/LivestockCV-0.0.0.32-py3-none-any.whl

---
+++ packages/LivestockCV/LivestockCV-0.0.0.32-py3-none-any.whl/LivestockCV/core/rotate.py
@@ -0,0 +1,31 @@
+# Rotate an image
+
+import os
+import cv2
+import numpy as np
+from LivestockCV.core import print_image
+from LivestockCV.core import plot_image
+from LivestockCV.core import params
+
+
+def rotate(img, rotation_deg):
+
+    # Here we're going to input our parameters 
+    iy, ix = np.shape(img)[:2]
+    m = cv2.getRotationMatrix2D((ix / 2, iy / 2), rotation_deg, 1)
+    cos = np.abs(m[0, 0])
+    sin = np.abs(m[0, 1])
+
+    params.device += 1
+
+    if params.debug == 'print':
+        print_image(rotated_img, os.path.join(params.debug_outdir,
+                                              str(params.device) + '_' + str(rotation_deg) + '_rotated_img.png'))
+
+    elif params.debug == 'plot':
+        if len(np.shape(img)) == 3:
+            plot_image(rotated_img)
+        else:
+            plot_image(rotated_img, cmap='gray')
+
+    return rotated_img

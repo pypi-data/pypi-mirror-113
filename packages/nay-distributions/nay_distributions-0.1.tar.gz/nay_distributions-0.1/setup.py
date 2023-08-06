e9135f8de940bf2cdc3dffdb90d5ab8698c0c991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:01 +0000

    Add package nay-distributions/0.1/nay_distributions-0.1.tar.gz

---
+++ packages/nay-distributions/nay_distributions-0.1.tar.gz/nay_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nay_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['nay_distributions'],
+      zip_safe=False)

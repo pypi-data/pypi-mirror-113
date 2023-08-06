c02879c9aa0527ef69e3741c4da0b5af49588263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:42 +0000

    Add package Tinge/0.0.2/Tinge-0.0.2-py3-none-any.whl

---
+++ packages/Tinge/Tinge-0.0.2-py3-none-any.whl/tinge/utils.py
@@ -0,0 +1,31 @@
+class InvalidColorError(Exception):
+    """Exception class for invalid colors"""
+
+    pass
+
+
+# foreground colors
+COLORS = {
+    "black": "\u001b[30m",
+    "red": "\u001b[31m",
+    "green": "\u001b[32m",
+    "yellow": "\u001b[33m",
+    "blue": "\u001b[34m",
+    "magenta": "\u001b[35m",
+    "cyan": "\u001b[36m",
+    "white": "\u001b[37m",
+    "reset": "\u001b[0m",
+}
+
+# background colors
+ON_COLORS = {
+    "grey": "\u001b[40m",
+    "red": "\u001b[41m",
+    "green": "\u001b[42m",
+    "yellow": "\u001b[43m",
+    "blue": "\u001b[44m",
+    "magenta": "\u001b[45m",
+    "cyan": "\u001b[46m",
+    "white": "\u001b[47m",
+    "": "",
+}

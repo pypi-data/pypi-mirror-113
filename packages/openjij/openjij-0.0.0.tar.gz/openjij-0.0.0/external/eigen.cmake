fe4045071cddb4bc091078f8628947a0498c045e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:51 +0000

    Add package openjij/0.0.0/openjij-0.0.0.tar.gz

---
+++ packages/openjij/openjij-0.0.0.tar.gz/openjij-0.0.0/external/eigen.cmake
@@ -0,0 +1,24 @@
+include(FetchContent)
+
+#### Eigen ####
+FetchContent_Declare(
+    eigen
+    GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
+    GIT_TAG         3.3.9
+    )
+set(EIGEN_CPP_STANDARD -std=c++11)
+FetchContent_MakeAvailable(eigen)
+
+add_library(eigen_lib INTERFACE)
+target_include_directories(eigen_lib INTERFACE ${eigen_SOURCE_DIR})
+target_compile_definitions(eigen_lib INTERFACE EIGEN_MPL2_ONLY)
+if (APPLE)
+    if(BLAS_FOUND AND LAPACK_FOUND) 
+      target_compile_definitions(eigen_lib INTERFACE EIGEN_USE_BLAS=ON)
+      target_compile_definitions(eigen_lib INTERFACE EIGEN_USE_LAPACKE=ON)
+    endif()
+endif()
+
+
+
+

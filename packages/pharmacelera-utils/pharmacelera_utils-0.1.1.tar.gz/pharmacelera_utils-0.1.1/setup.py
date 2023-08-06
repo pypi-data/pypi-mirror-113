ca167573dd0829632a8348324cbb1b44e7afc4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:11 +0000

    Add package pharmacelera-utils/0.1.1/pharmacelera_utils-0.1.1.tar.gz

---
+++ packages/pharmacelera-utils/pharmacelera_utils-0.1.1.tar.gz/pharmacelera_utils-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pharmacelera_utils",
+    version="0.1.1",
+    description="Pharmacelera script to execute experiments",
+    url="https://bitbucket.org/pharmacelera/pharmqsar-examples.git",
+    author="Pharmacelera developers",
+    author_email="info@pharmacelera.com",
+    license="Mozilla Public License Version 2.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests==2.25.1",
+        "PyYAML==5.4.1",
+        "boto3==1.17.76"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    py_modules=[
+        "launch",
+        "services",
+        "utils",
+        "__init__",
+    ],
+    entry_points={"console_scripts": ["pharmacelera-launch=src.launch:run"]},
+
+)

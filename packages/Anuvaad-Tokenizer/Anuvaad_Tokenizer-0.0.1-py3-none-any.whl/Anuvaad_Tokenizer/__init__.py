a351ec2ea6c39bfbff32968f7a23bbdc724b2554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:27 +0000

    Add package Anuvaad-Tokenizer/0.0.1/Anuvaad_Tokenizer-0.0.1-py3-none-any.whl

---
+++ packages/Anuvaad-Tokenizer/Anuvaad_Tokenizer-0.0.1-py3-none-any.whl/Anuvaad_Tokenizer/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.0.1'
+from Anuvaad_Tokenizer.AnuvaadEnTokenizer import AnuvaadEnTokenizer
+from Anuvaad_Tokenizer.AnuvaadEnTokenizer import SentenceEndLangVars
+from Anuvaad_Tokenizer.AnuvaadKnTokenizer import AnuvaadKnTokenizer
+from Anuvaad_Tokenizer.AnuvaadKnTokenizer import SentenceEndLangVars
+from Anuvaad_Tokenizer.AnuvaadHiTokenizer import AnuvaadHiTokenizer
+from Anuvaad_Tokenizer.AnuvaadHiTokenizer import SentenceEndLangVars

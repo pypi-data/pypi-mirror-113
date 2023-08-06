84a248527907b5484efd6a4543fb64847005858f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:58 +0000

    Add package email-login/0.0.0/email_login-0.0.0.tar.gz

---
+++ packages/email-login/email_login-0.0.0.tar.gz/email_login-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='email_login',
+    packages=["email_login"],
+    version='0.0.0',
+    author='Perzan',
+    author_email='PerzanDevelopment@gmail.com',
+    description="Please email me if you would like this project transferred"
+)

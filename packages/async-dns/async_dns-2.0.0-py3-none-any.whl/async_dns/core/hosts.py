dd4d96c108f60e1423ea5378620faceaddc63978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:29 +0000

    Add package async-dns/2.0.0/async_dns-2.0.0-py3-none-any.whl

---
+++ packages/async-dns/async_dns-2.0.0-py3-none-any.whl/async_dns/core/hosts.py
@@ -0,0 +1,30 @@
+import os
+
+from .address import Address
+from .record import Record
+
+if os.name == 'nt':
+    hosts_file = os.path.expandvars(r'%windir%\System32\drivers\etc\hosts')
+elif os.name == 'posix':
+    hosts_file = '/etc/hosts'
+else:
+    hosts_file = None
+
+
+def parse_hosts_file(filename=None):
+    if filename is None: filename = hosts_file
+    filename = os.path.expanduser(filename)
+    if not os.path.isfile(filename):
+        return
+    with open(filename, 'r') as f:
+        for line in f:
+            items = line.strip().split('#')[0].split()
+            try:
+                it = iter(items)
+                addr = Address.parse(next(it))
+            except StopIteration:
+                pass
+            else:
+                for name in it:
+                    if isinstance(addr.ip_type, int):
+                        yield name, addr.ip_type, (addr.hostinfo.hostname, )
---
+++ packages/async-dns/async_dns-2.0.0-py3-none-any.whl/async_dns/server/__main__.py
@@ -0,0 +1,41 @@
+'''
+This module starts a DNS server according to console arguments.
+'''
+import argparse
+import logging
+import os
+
+from . import run_forever, start_dns_server
+from ..core import logger
+
+
+def main():
+    '''Start a DNS server from command line.'''
+    parser = argparse.ArgumentParser(prog='python3 -m async_dns.server',
+                                     description='DNS server by Gerald.')
+    parser.add_argument('-b',
+                        '--bind',
+                        default=':53',
+                        help='the address for the server to bind')
+    parser.add_argument(
+        '--hosts',
+        default='local',
+        help=
+        'the path of a hosts file, `none` to disable hosts, `local` to read from local hosts file'
+    )
+    parser.add_argument(
+        '-x',
+        '--proxy',
+        nargs='*',
+        default=None,
+        help=
+        'the proxy DNS servers, `none` to serve as a recursive server, `default` to proxy to default nameservers'
+    )
+    args = parser.parse_args()
+    logging.basicConfig(level=os.environ.get('LOGLEVEL', logging.INFO))
+    logger.info('DNS server v2 - by Gerald')
+    run_forever(
+        start_dns_server(bind=args.bind, hosts=args.hosts, proxies=args.proxy))
+
+
+main()

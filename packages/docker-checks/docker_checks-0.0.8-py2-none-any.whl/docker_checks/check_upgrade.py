50050cc5b1b177933f10065a1ce53586531fc7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:12 +0000

    Add package docker-checks/0.0.8/docker_checks-0.0.8-py2-none-any.whl

---
+++ packages/docker-checks/docker_checks-0.0.8-py2-none-any.whl/docker_checks/check_upgrade.py
@@ -0,0 +1,13 @@
+import logging
+import os
+dockerfileContent = open("Dockerfile", "r").readlines()
+
+def checkUpgradePackage():
+    list = ['apt upgrade','yum upgrade']
+    for index, line in enumerate(dockerfileContent):
+        for keyword in list:
+            if keyword in line:
+                logging.warning(' Line %s: upgrade actions should be restricted as it impacts creation of immutable, consistent builds', index)
+
+if __name__ == "__main__":
+    checkUpgradePackage()

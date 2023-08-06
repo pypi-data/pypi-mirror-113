1e59f5f3a8e9f7752586fbc72449610df9d872fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:44 +0000

    Add package Tinge/0.0.4/Tinge-0.0.4-py3-none-any.whl

---
+++ packages/Tinge/Tinge-0.0.4-py3-none-any.whl/tinge/__init__.py
@@ -0,0 +1,16 @@
+from .cprint import colored, warn, error, info, success, bold, underline, italic
+from .utils import InvalidColorError
+
+
+__version__ = "0.0.4"
+__all__ = [
+    "colored",
+    "warn",
+    "error",
+    "info",
+    "success",
+    "bold",
+    "underline",
+    "italic",
+    "InvalidColorError",
+]

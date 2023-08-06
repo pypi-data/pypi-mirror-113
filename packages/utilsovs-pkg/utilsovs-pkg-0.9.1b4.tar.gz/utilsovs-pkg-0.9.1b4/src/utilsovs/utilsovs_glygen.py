c7e9d0178169e33cb6d5b1ef48629eef640f405d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:25 +0000

    Add package utilsovs-pkg/0.9.1b4/utilsovs-pkg-0.9.1b4.tar.gz

---
+++ packages/utilsovs-pkg/utilsovs-pkg-0.9.1b4.tar.gz/utilsovs-pkg-0.9.1b4/src/utilsovs/utilsovs_glygen.py
@@ -0,0 +1,13 @@
+#utilsovs_semantic.py
+from Bio import Entrez
+from .utilsovs_commons import request_API_json
+
+def request_GlyGen_API(data):
+
+    data.url ='https://api.glygen.org/directsearch/protein/?query='
+
+    data.id = '{"uniprot_canonical_ac":"'+data.id+'"}'
+
+    data = request_API_json(data)
+
+    return data
---
+++ packages/utilsovs-pkg/utilsovs-pkg-0.9.1b4.tar.gz/utilsovs-pkg-0.9.1b4/src/utilsovs/utilsovs_oglcnacDB.py
@@ -0,0 +1,11 @@
+#utilsovs_semantic.py
+from Bio import Entrez
+from .utilsovs_commons import request_API_json
+
+def request_oglcnacDB_API(data):
+
+    data.url ='https://www.oglcnac.mcw.edu/api/v1/proteins/?query_protein='
+
+    data = request_API_json(data)
+
+    return data

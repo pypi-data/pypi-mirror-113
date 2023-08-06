a24b42a37d2af6548131e8e2c032240128cc71c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:51 +0000

    Add package convectors/0.0.9/convectors-0.0.9-py3-none-any.whl

---
+++ packages/convectors/convectors-0.0.9-py3-none-any.whl/convectors/layers.py
@@ -0,0 +1,17 @@
+from .classifier import (MLP, SVM, AdaBoost, Keras, RandomForest, Transformer,
+                         XGBoost)
+from .embedding import CountVectorizer, Doc2Vec, OneHot, Sequence, TfIdf
+from .linguistics import (Contract, FindAll, Lemmatizer, Phrase, Snowball, Sub,
+                          Tokenize)
+from .multi import Merge
+from .preprocessing import Normalize
+from .reduction import NMF, PCA, SVD, UMAP
+from .special import SplitHashtag
+
+__all__ = [
+    "Tokenize", "Snowball", "Phrase", "Sub", "Contract", "FindAll",
+    "TfIdf", "CountVectorizer", "Sequence", "OneHot", "Doc2Vec",
+    "SVD", "PCA", "NMF", "UMAP", "Lemmatizer",
+    "RandomForest", "AdaBoost", "SVM", "MLP", "Keras", "Transformer",
+    "XGBoost", "Merge", "Normalize", "SplitHashtag"
+]

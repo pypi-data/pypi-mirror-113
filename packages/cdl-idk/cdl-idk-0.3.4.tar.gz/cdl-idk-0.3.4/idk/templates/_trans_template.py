fa67bf5b2eec2e64a4f13e16f6a792f1454cced2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:14 +0000

    Add package cdl-idk/0.3.4/cdl-idk-0.3.4.tar.gz

---
+++ packages/cdl-idk/cdl-idk-0.3.4.tar.gz/cdl-idk-0.3.4/idk/templates/_trans_template.py
@@ -0,0 +1,15 @@
+from idk.io import TranscribeData
+
+
+def transcribe(payload: dict) -> TranscribeData:
+    """Your code goes under here!
+
+    Args:
+        payload (dict): This dictionary is the one returned the 'transform' method of your _DeveloperInsight_ class.
+
+    Returns:
+        str: A transcription of the results of your insight. Think about whether the contents of the insight payload 
+             would change the way you phrase the transcription. 
+    """
+
+    return None  # Needs to return a string!

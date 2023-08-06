3b1488ecf7a91934515e7a3f318f6704cdd4e0fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:16 +0000

    Add package dash-mp-components/0.3.27/dash_mp_components-0.3.27.tar.gz

---
+++ packages/dash-mp-components/dash_mp_components-0.3.27.tar.gz/dash_mp_components-0.3.27/dash_mp_components/DataBlock.py
@@ -0,0 +1,45 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class DataBlock(Component):
+    """A DataBlock component.
+Display a single record of data in a horizontal block element
+with an optional expanded view to show additional data columns.
+
+Keyword arguments:
+
+- id (string; optional):
+    The ID used to identify this component in Dash callbacks.
+
+- className (string; optional):
+    Class name(s) to append to the component's default class
+    (mpc-data-block).
+
+- columns (list; optional):
+    Optional configuration for the columns in the data object. Follows
+    the same format as the columns prop in the SearchUI component.
+
+- data (dict; optional):
+    Object (i.e. dictionary) of data to be displayed in the block.
+
+- expanded (boolean; optional):
+    Control the initial expanded state of the block. @default False."""
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, className=Component.UNDEFINED, data=Component.UNDEFINED, columns=Component.UNDEFINED, expanded=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'className', 'columns', 'data', 'expanded']
+        self._type = 'DataBlock'
+        self._namespace = 'dash_mp_components'
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'className', 'columns', 'data', 'expanded']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+        for k in []:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+        super(DataBlock, self).__init__(**args)
---
+++ packages/dash-mp-components/dash_mp_components-0.3.27.tar.gz/dash_mp_components-0.3.27/dash_mp_components/_imports_.py
@@ -0,0 +1,63 @@
+from .JsonView import JsonView
+from .MatSidebar import MatSidebar
+from .CameraContextProvider import CameraContextProvider
+from .CrystalToolkitScene import CrystalToolkitScene
+from .Download import Download
+from .GraphComponent import GraphComponent
+from .Markdown import Markdown
+from .NavbarDropdown import NavbarDropdown
+from .Scrollspy import Scrollspy
+from .PeriodicContext import PeriodicContext
+from .PeriodicContextTable import PeriodicContextTable
+from .PeriodicElement import PeriodicElement
+from .PeriodicFilter import PeriodicFilter
+from .PeriodicTableInput import PeriodicTableInput
+from .BibCard import BibCard
+from .BibFilter import BibFilter
+from .BibjsonCard import BibjsonCard
+from .BibtexButton import BibtexButton
+from .CrossrefCard import CrossrefCard
+from .DataBlock import DataBlock
+from .DownloadButton import DownloadButton
+from .DownloadDropdown import DownloadDropdown
+from .Formula import Formula
+from .GlobalSearchBar import GlobalSearchBar
+from .MaterialsInput import MaterialsInput
+from .PublicationButton import PublicationButton
+from .SearchUI import SearchUI
+from .Select import Select
+from .SynthesisRecipeCard import SynthesisRecipeCard
+from .Tooltip import Tooltip
+
+__all__ = [
+    "JsonView",
+    "MatSidebar",
+    "CameraContextProvider",
+    "CrystalToolkitScene",
+    "Download",
+    "GraphComponent",
+    "Markdown",
+    "NavbarDropdown",
+    "Scrollspy",
+    "PeriodicContext",
+    "PeriodicContextTable",
+    "PeriodicElement",
+    "PeriodicFilter",
+    "PeriodicTableInput",
+    "BibCard",
+    "BibFilter",
+    "BibjsonCard",
+    "BibtexButton",
+    "CrossrefCard",
+    "DataBlock",
+    "DownloadButton",
+    "DownloadDropdown",
+    "Formula",
+    "GlobalSearchBar",
+    "MaterialsInput",
+    "PublicationButton",
+    "SearchUI",
+    "Select",
+    "SynthesisRecipeCard",
+    "Tooltip"
+]

dc5ef3a3a8b8126aba2b5059911cb0fbf3db1186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:39 +0000

    Add package firstock/0.2/firstock-0.2.tar.gz

---
+++ packages/firstock/firstock-0.2.tar.gz/firstock-0.2/setup.py
@@ -0,0 +1,36 @@
+"""Setup script for realpython-reader"""
+
+import os.path
+from setuptools import setup
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+# This call to setup() does all the work
+setup(
+    name="firstock",
+    version="0.2",
+    description="A functional python wrapper for firstock trading api",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="http://developers.thefirstock.com/api/",
+    author="Firsstock",
+    author_email="support@thefirstock.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["firstock"],
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "pandas"
+    ],
+)

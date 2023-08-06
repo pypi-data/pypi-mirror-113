1f694d8015e24a3d0ef0bff54503926ba2c6538f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:37 +0000

    Add package VecDB/0.2.1/VecDB-0.2.1-py3-none-any.whl

---
+++ packages/VecDB/VecDB-0.2.1-py3-none-any.whl/vecdb/batch/batch_insert.py
@@ -0,0 +1,34 @@
+"""Batch operations
+"""
+from typing import Callable
+from ..api.client import APIClient
+from .chunk import Chunker
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from functools import partial
+from ..concurrency import multithread, multiprocess
+
+class BatchInsert(APIClient, Chunker):
+    def insert_documents(self, dataset_id: str, docs: list, 
+        bulk_fn: Callable=None, verbose: bool=True,
+        chunksize: int=20, max_workers:int =8, *args, **kwargs):
+        """
+        Insert a list of documents with multi-threading automatically
+        enabled.
+        """
+        print(f"You are currently inserting into {dataset_id}")
+        print(f"You can track your stats and progress via our dashboard at https://playground.getvectorai.com/collections/dashboard/stats/?collection={dataset_id}")
+        def bulk_insert_func(docs):
+            return self.datasets.bulk_insert(
+                dataset_id,
+                docs, *args, **kwargs)
+        
+        if bulk_fn is not None:
+            return multiprocess(
+                func=bulk_fn,
+                iterables=docs,
+                post_func_hook=bulk_insert_func,
+                max_workers=max_workers,
+                chunksize=chunksize)
+
+        return multithread(bulk_insert_func, docs, 
+            max_workers=max_workers, chunksize=chunksize)

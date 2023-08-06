3bf9d73ab3f81d70c9c3ce0dea1cee6e0ca4160f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:51 +0000

    Add package age-verify-compliance/0.0.1/age_verify_compliance-0.0.1.tar.gz

---
+++ packages/age-verify-compliance/age_verify_compliance-0.0.1.tar.gz/age_verify_compliance-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='age_verify_compliance',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='me',
+    author_email='me@example.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)

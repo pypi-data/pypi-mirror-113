f9f34f219f435a9c2771392cd5ef66610e0f5f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:28 +0000

    Add package PyCurseForge/1.0.0.post1/PyCurseForge-1.0.0.post1.tar.gz

---
+++ packages/PyCurseForge/PyCurseForge-1.0.0.post1.tar.gz/PyCurseForge-1.0.0.post1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+version = "1.0.0-1"
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name='PyCurseForge',
+    install_requires=['requests==2.26.0'],
+    author='XuaTheGrate',
+    url='https://github.com/XuaTheGrate/PyCurseForge',
+    version=version,
+    packages=['curseforge'],
+    license='MIT',
+    description='A python wrapper for the (hidden) CurseForge API',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.8.0',
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Games/Entertainment",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Typing :: Typed"
+    ]
+)

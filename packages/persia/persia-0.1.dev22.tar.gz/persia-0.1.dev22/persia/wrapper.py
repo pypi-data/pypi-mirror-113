7a0cddd5591c9ba437f3aa3a482d70f73f1394af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:32 +0000

    Add package persia/0.1.dev22/persia-0.1.dev22.tar.gz

---
+++ packages/persia/persia-0.1.dev22.tar.gz/persia-0.1.dev22/persia/wrapper.py
@@ -0,0 +1,35 @@
+import traceback
+
+from contextlib import contextmanager
+
+from persia.utils import block
+from persia.logger import get_default_logger
+
+logger = get_default_logger()
+
+
+@contextmanager
+def persia_ctx(
+    exit_when_meet_exception: bool = False,
+    catch_exception: bool = True,
+    verbose: bool = True,
+):
+    r"""Return a context warpper to process the error
+
+    Arguments:
+        exit_when_meet_exception (bool, optional): whether block the process when exit the contxt
+        catch_exception (bool, optinal): catch the exception or not when occur the exception
+        verbose (bool, optional): output the exception
+    """
+    try:
+        yield
+    except Exception as e:
+        if verbose:
+            exception_str = traceback.format_exc()
+            logger.error("\n" + exception_str)
+
+        if not catch_exception:
+            raise e
+
+    if not exit_when_meet_exception:
+        block()

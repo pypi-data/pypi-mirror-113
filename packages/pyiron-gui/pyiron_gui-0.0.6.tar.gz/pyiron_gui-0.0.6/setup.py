e9d661be5c93acca49c8689ef4391db2fd58083f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:59 +0000

    Add package pyiron-gui/0.0.6/pyiron_gui-0.0.6.tar.gz

---
+++ packages/pyiron-gui/pyiron_gui-0.0.6.tar.gz/pyiron_gui-0.0.6/setup.py
@@ -0,0 +1,44 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+
+import versioneer
+
+setup(
+    name='pyiron_gui',
+    version=versioneer.get_version(),
+    description='Repository for GUI plugins to the pyiron IDE.',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/pyiron/pyiron_gui',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='siemer@mpie.de',
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
+    install_requires=[
+        'pyiron_base>=0.2.19',
+        'pyiron_atomistics>=0.2.20',
+        'ipywidgets>=7.6.3',
+        'matplotlib>=3.4.2',
+        'mendeleev>=0.7.0',
+        'numpy>=1.21.1',
+        'pandas>=1.3.0',
+    ]
+)

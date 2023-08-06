5e27cd7c1d17a7a7a0ccef7cbd20728594bdcd91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:24 +0000

    Add package herbatis/0.0.5/herbatis-0.0.5.tar.gz

---
+++ packages/herbatis/herbatis-0.0.5.tar.gz/herbatis-0.0.5/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='herbatis',
+    version='0.0.5',
+    author='Mr.Zhang',
+    author_email='zhang.pengbird@gmail.com',
+    url='https://zhuanlan.zhihu.com/p/26159930',
+    description=u'A mybatis of python',
+    packages=['test'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+        ]
+    }
+)

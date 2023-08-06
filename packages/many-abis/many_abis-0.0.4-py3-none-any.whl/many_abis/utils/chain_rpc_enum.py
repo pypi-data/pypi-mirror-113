7870406293a0f797a8a6313d27d524764c4a27b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:02 +0000

    Add package many-abis/0.0.4/many_abis-0.0.4-py3-none-any.whl

---
+++ packages/many-abis/many_abis-0.0.4-py3-none-any.whl/many_abis/utils/chain_rpc_enum.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class CHAIN_RPC_API:
+    ETH:str = 'https://cloudflare-eth.com'
+    BSC:str = 'https://bsc-dataseed1.defibit.io' 
+    HECO:str = 'https://http-mainnet-node.huobichain.com'
+    MATIC:str = 'https://rpc-mainnet.maticvigil.com'
+    KCC:str = 'https://rpc-mainnet.kcc.network'

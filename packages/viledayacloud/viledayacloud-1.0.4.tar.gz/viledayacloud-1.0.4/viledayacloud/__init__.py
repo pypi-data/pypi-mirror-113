97b03ca5449fb772f32fc6f949164dcd33bad26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:20 +0000

    Add package viledayacloud/1.0.4/viledayacloud-1.0.4.tar.gz

---
+++ packages/viledayacloud/viledayacloud-1.0.4.tar.gz/viledayacloud-1.0.4/viledayacloud/__init__.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+"""
+viledayacloud
+"""
+
+__version__ = "1.0.4"
+
+from typing import Tuple
+
+from .common import (getsecret as getsecret)
+
+__all__: Tuple[str, ...] = (
+        "getsecret",
+        )

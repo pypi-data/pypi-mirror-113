2f14f11751aa228be43d3dbfc23345c0c60f214c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:34 +0000

    Add package DeleteFilesRapidly/0.0.1/DeleteFilesRapidly-0.0.1-py3-none-any.whl

---
+++ packages/DeleteFilesRapidly/DeleteFilesRapidly-0.0.1-py3-none-any.whl/DeleteFilesRapidly/DeleteFilesWithThreading.py
@@ -0,0 +1,38 @@
+import os
+from threading import Thread
+from multiprocessing import cpu_count
+from queue import *
+
+
+def worker_def(A):
+    q = A[0]
+    while True:
+        item = q.get()
+        if item is None:
+            break
+        else:
+            file = item
+            os.remove(file)
+            q.task_done()
+
+
+def delete_files(path):
+    thread_count = int(cpu_count()*0.8)
+    q = Queue(maxsize=thread_count)
+    A = (q,)
+    threads = []
+    for worker in range(thread_count):
+        t = Thread(target=worker_def, args=(A,))
+        t.start()
+        threads.append(t)
+    for file in os.listdir(path):
+        q.put(os.path.join(path, file))
+    for i in range(thread_count):
+        q.put(None)
+    for t in threads:
+        t.join()
+    return None
+
+
+if __name__ == '__main__':
+    pass

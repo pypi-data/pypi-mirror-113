0a27ceee6f0171106384f50b0bb2ddf061f25587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:22 +0000

    Add package pydltools/0.0.1/pydltools-0.0.1.tar.gz

---
+++ packages/pydltools/pydltools-0.0.1.tar.gz/pydltools-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="pydltools",
+    version="0.0.1",
+    author="lzk901372",
+    author_email="lzk773629528@gmail.com",
+    description="Ensemble of tools for Python Deep Learning",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)

dfbf57e4a4594876d22db81e0970bf81f84ac308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:36 +0000

    Add package cutty/0.7.0/cutty-0.7.0.tar.gz

---
+++ packages/cutty/cutty-0.7.0.tar.gz/cutty-0.7.0/src/cutty/services/create.py
@@ -0,0 +1,82 @@
+"""Create a project from a Cookiecutter template."""
+import pathlib
+from collections.abc import Mapping
+from types import MappingProxyType
+from typing import Optional
+
+import platformdirs
+from lazysequence import lazysequence
+
+from cutty.filestorage.adapters.cookiecutter import createcookiecutterstorage
+from cutty.filesystems.domain.purepath import PurePath
+from cutty.repositories.adapters.storage import getdefaultrepositoryprovider
+from cutty.templates.adapters.cookiecutter.binders import bindcookiecuttervariables
+from cutty.templates.adapters.cookiecutter.config import findhooks
+from cutty.templates.adapters.cookiecutter.config import findpaths
+from cutty.templates.adapters.cookiecutter.config import loadcookiecutterconfig
+from cutty.templates.adapters.cookiecutter.render import createcookiecutterrenderer
+from cutty.templates.domain.bindings import Binding
+from cutty.templates.domain.renderfiles import renderfiles
+
+
+def create(
+    template: str,
+    *,
+    extra_context: Mapping[str, str] = MappingProxyType({}),
+    no_input: bool = False,
+    checkout: Optional[str] = None,
+    outputdir: Optional[pathlib.Path] = None,
+    directory: Optional[pathlib.PurePosixPath] = None,
+    overwrite_if_exists: bool = False,
+    skip_if_file_exists: bool = False,
+    outputdirisproject: bool = False,
+    createrepository: bool = True,
+) -> None:
+    """Generate a project from a Cookiecutter template."""
+    cachedir = pathlib.Path(platformdirs.user_cache_dir("cutty"))
+    templatedir = getdefaultrepositoryprovider(cachedir)(template, revision=checkout)
+
+    if directory is not None:
+        templatedir = templatedir.joinpath(*directory.parts)
+
+    if outputdir is None:
+        outputdir = pathlib.Path.cwd()
+
+    config = loadcookiecutterconfig(template, templatedir)
+    render = createcookiecutterrenderer(templatedir, config)
+    bindings = bindcookiecuttervariables(
+        config.variables,
+        render,
+        interactive=not no_input,
+        bindings=[Binding(key, value) for key, value in extra_context.items()],
+    )
+
+    projectfiles = lazysequence(
+        renderfiles(findpaths(templatedir, config), render, bindings)
+    )
+    if not projectfiles:  # pragma: no cover
+        return
+
+    strip = 0 if directory is None else len(directory.parts)
+
+    if outputdirisproject:
+        projectdir = outputdir
+        strip += 1
+    else:
+        projectdir = outputdir / projectfiles[0].path.parts[strip]
+
+    hookfiles = lazysequence(renderfiles(findhooks(templatedir), render, bindings))
+
+    with createcookiecutterstorage(
+        outputdir,
+        projectdir,
+        overwrite_if_exists,
+        skip_if_file_exists,
+        hookfiles,
+        createrepository,
+    ) as storage:
+        for projectfile in projectfiles.release():
+            if strip:
+                path = PurePath(*projectfile.path.parts[strip:])
+                projectfile = projectfile.withpath(path)
+            storage.add(projectfile)
---
+++ packages/cutty/cutty-0.7.0.tar.gz/cutty-0.7.0/src/cutty/services/update.py
@@ -0,0 +1,111 @@
+"""Update a project with changes from its Cookiecutter template."""
+import hashlib
+import json
+import tempfile
+from collections.abc import Iterator
+from collections.abc import Mapping
+from pathlib import Path
+from types import MappingProxyType
+from typing import Optional
+
+import pygit2
+
+from cutty.compat.contextlib import contextmanager
+from cutty.filestorage.adapters.observers.git import commit
+from cutty.filestorage.adapters.observers.git import LATEST_BRANCH
+from cutty.filestorage.adapters.observers.git import UPDATE_MESSAGE
+from cutty.services.create import create
+
+
+def getprojecttemplate(projectdir: Path) -> str:
+    """Return the location of the project template."""
+    context = getprojectcontext(projectdir)
+    return context["_template"]
+
+
+def getprojectcontext(projectdir: Path) -> dict[str, str]:
+    """Return the Cookiecutter context of the project."""
+    text = (projectdir / ".cookiecutter.json").read_text()
+    data = json.loads(text)
+    return {
+        key: value
+        for key, value in data.items()
+        if isinstance(key, str) and isinstance(value, str)
+    }
+
+
+def checkoutemptytree(repositorypath: Path) -> None:
+    """Check out an empty tree from the repository."""
+    repository = pygit2.Repository(repositorypath)
+    oid = repository.TreeBuilder().write()
+    repository.checkout_tree(repository[oid])
+
+
+@contextmanager
+def createworktree(
+    repositorypath: Path,
+    branch: str,
+    *,
+    checkout: bool = True,
+) -> Iterator[Path]:
+    """Create a worktree for the branch in the repository."""
+    repository = pygit2.Repository(repositorypath)
+
+    with tempfile.TemporaryDirectory() as directory:
+        name = hashlib.blake2b(branch.encode(), digest_size=32).hexdigest()
+        path = Path(directory) / name
+        worktree = repository.add_worktree(name, path, repository.branches[branch])
+
+        if not checkout:
+            # Emulate `--no-checkout` by checking out an empty tree after the fact.
+            checkoutemptytree(path)
+
+        yield path
+
+    # Prune with `force=True` because libgit2 thinks `worktree.path` still exists.
+    # https://github.com/libgit2/libgit2/issues/5280
+    worktree.prune(True)
+
+
+def cherrypick(repositorypath: Path, reference: str) -> None:
+    """Cherry-pick the commit onto the current branch."""
+    repository = pygit2.Repository(repositorypath)
+    oid = repository.references[reference].resolve().target
+    repository.cherrypick(oid)
+
+    if repository.index.conflicts:
+        paths = {
+            side.path
+            for _, ours, theirs in repository.index.conflicts
+            for side in (ours, theirs)
+            if side is not None
+        }
+        raise RuntimeError(f"Merge conflicts: {', '.join(paths)}")
+
+    commit(repository, message=UPDATE_MESSAGE)
+    repository.state_cleanup()
+
+
+def update(
+    *,
+    projectdir: Optional[Path] = None,
+    extra_context: Mapping[str, str] = MappingProxyType({}),
+    no_input: bool = False,
+) -> None:
+    """Update a project with changes from its Cookiecutter template."""
+    if projectdir is None:
+        projectdir = Path.cwd()
+
+    template = getprojecttemplate(projectdir)
+    context = getprojectcontext(projectdir)
+
+    with createworktree(projectdir, LATEST_BRANCH, checkout=False) as worktree:
+        create(
+            template,
+            outputdir=worktree,
+            outputdirisproject=True,
+            extra_context={**context, **extra_context},
+            no_input=no_input,
+        )
+
+    cherrypick(projectdir, f"refs/heads/{LATEST_BRANCH}")

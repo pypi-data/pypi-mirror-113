c0996909bdae0742e4695f423c069a5a3da4ebec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:25 +0000

    Add package jsvn-qa-utils-rest/1.0.1/jsvn_qa_utils_rest-1.0.1.tar.gz

---
+++ packages/jsvn-qa-utils-rest/jsvn_qa_utils_rest-1.0.1.tar.gz/jsvn_qa_utils_rest-1.0.1/jsvn_qa_utils_rest/__init__.py
@@ -0,0 +1,1 @@
+from src.rest_client import RestClient
---
+++ packages/jsvn-qa-utils-rest/jsvn_qa_utils_rest-1.0.1.tar.gz/jsvn_qa_utils_rest-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='jsvn_qa_utils_rest',
+    version='1.0.1',
+    description='rest client with allure and cerberus',
+    packages=['jsvn_qa_utils_rest'],
+    author_email='codejseven@gmail.com',
+    zip_safe=False
+)

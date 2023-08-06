48ee007e2be1a6a0b431f11a3ef25a9bf09e5534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:54 +0000

    Add package st-material-table/0.0.4/st_material_table-0.0.4-py3-none-any.whl

---
+++ packages/st-material-table/st_material_table-0.0.4-py3-none-any.whl/st_material_table/frontend/build/precache-manifest.744afaeb1552df18439b54195d915046.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef6cdeab7ff33aae18628662369043e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "43fd811a8cb7084f9957",
+    "url": "./static/js/2.d0448f1b.chunk.js"
+  },
+  {
+    "revision": "61af748d7030040b3d9e7f68f29fbc01",
+    "url": "./static/js/2.d0448f1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce5354b88cc59c32379",
+    "url": "./static/js/main.b8058af5.chunk.js"
+  },
+  {
+    "revision": "f3458c042c75ce411c2d",
+    "url": "./static/js/runtime-main.928d0424.js"
+  }
+]);

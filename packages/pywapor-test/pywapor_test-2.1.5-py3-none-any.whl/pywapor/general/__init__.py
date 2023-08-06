213d5c0baba0d5dba96a2bb768658bac8c693439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:41 +0000

    Add package pywapor-test/2.1.5/pywapor_test-2.1.5-py3-none-any.whl

---
+++ packages/pywapor-test/pywapor_test-2.1.5-py3-none-any.whl/pywapor/general/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Tim Hessels
+Module: WAPOR/Functions
+"""
+
+from pywapor.general import landcover_converter, outputs, variables, input_data_reqs, plot
+
+__all__ = ['landcover_converter', 'outputs', 'variables', 'input_data_reqs', 'plot']
+
+__version__ = '0.1'

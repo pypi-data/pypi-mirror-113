fccdb3c7b0199194cf54e00165fac34065c3fa3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:40 +0000

    Add package pytablewriter/0.62.0/pytablewriter-0.62.0.tar.gz

---
+++ packages/pytablewriter/pytablewriter-0.62.0.tar.gz/pytablewriter-0.62.0/pytablewriter/typehint/__init__.py
@@ -0,0 +1,17 @@
+from typepy import (
+    Binary,
+    Bool,
+    Bytes,
+    DateTime,
+    Dictionary,
+    Infinity,
+    Integer,
+    IpAddress,
+    List,
+    Nan,
+    NoneType,
+    NullString,
+    RealNumber,
+    String,
+)
+from typepy.type import AbstractType

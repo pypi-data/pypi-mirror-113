477ca8d20cd8c9943752073c13caca274b307728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:59 +0000

    Add package document-utils/1.0.2/document_utils-1.0.2-py3-none-any.whl

---
+++ packages/document-utils/document_utils-1.0.2-py3-none-any.whl/doc_utils/doc_utils.py
@@ -0,0 +1,8 @@
+from .write_utils import DocWriteUtils
+
+class DocUtils(DocWriteUtils):
+    """Class for all document utilities. 
+    Primarily should be used as a mixin for future functions
+    but can be a standalone.
+    # TODO: Extend to Chunk Doc Reading and Chunk Doc Writing
+    """

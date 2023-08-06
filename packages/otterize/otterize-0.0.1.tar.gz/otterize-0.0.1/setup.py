e09e931d68bd09772dd3028a38a4451088b3e3ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:30 +0000

    Add package otterize/0.0.1/otterize-0.0.1.tar.gz

---
+++ packages/otterize/otterize-0.0.1.tar.gz/otterize-0.0.1/setup.py
@@ -0,0 +1,24 @@
+"""Setup script"""
+
+import os.path
+from setuptools import setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+setup(
+    name="otterize",
+    version="0.0.1",
+    author="Otterize",
+    author_email="pypi@otterize.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["otterize"],
+    include_package_data=True,
+)

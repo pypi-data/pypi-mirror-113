58d041790807feae429be81ae56b237c4d8e5216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:31 +0000

    Add package trashparse/0.1.4/trashparse-0.1.4.tar.gz

---
+++ packages/trashparse/trashparse-0.1.4.tar.gz/trashparse-0.1.4/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name = 'trashparse',
+  packages = ['trashparse'],
+  version = '0.1.4',
+  license='MIT',
+  description = 'simple tool for analyzing Windows Recycle.Bin',
+  author = 'hanasuru',
+  author_email = 'faqih.insani@ymail.com',
+  url = 'https://github.com/hanasuru/TrashParse',
+  download_url = 'https://github.com/hanasuru/TrashParse/archive/0.1.3.tar.gz',
+  keywords = ['forensic', 'recycle-bin', 'windows'],
+  install_requires=['prettytable'],
+  classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+  ],
+  scripts=['bin/trashparse'],
+)

cfe75b7d24291a84a9900fc2e7690aadd51ee7c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:14 +0000

    Add package anchorage/0.9.0.0/anchorage-0.9.0.0.tar.gz

---
+++ packages/anchorage/anchorage-0.9.0.0.tar.gz/anchorage-0.9.0.0/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="anchorage",
+    version="0.9.0.0",
+    author="Antonio Lopez Rivera",
+    author_email="antonlopezr99@gmail.com",
+    description="Anchor your little piece of internet.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/antonlopezr/anchorage",
+    entry_points={
+        'console_scripts': ['mybinary=anchorage.cli:interface']
+    },
+    packages=['anchorage'],
+    install_requires=[
+        "numpy",
+        "rapidjson",
+        "toml",
+        "Python-Alexandria",
+        "archivenow",
+        "internetarchive",
+        "tqdm",
+        "pyfiglet",
+        "PyInquirer",
+        "prompt-toolkit==1.0.14",
+        "lz4"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)

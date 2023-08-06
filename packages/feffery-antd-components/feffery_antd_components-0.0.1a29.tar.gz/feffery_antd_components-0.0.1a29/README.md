0f0eb0ef75339bd8415fc65562ae20f978cbd993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:52 +0000

    Add package feffery-antd-components/0.0.1a29/feffery_antd_components-0.0.1a29.tar.gz

---
+++ packages/feffery-antd-components/feffery_antd_components-0.0.1a29.tar.gz/feffery_antd_components-0.0.1a29/README.md
@@ -0,0 +1,63 @@
+# feffery-antd-components
+`Plotly Dash`第三方组件库，将`Antd`中的常用组件带入`Dash`的世界🥳，最新测试版本：`0.0.1a28`
+
+## 1 最新测试版本安装方式（有完整在线文档说明）
+
+```bash
+pip install feffery-antd-components==0.0.1a28
+```
+
+## 2 最新开发版本安装方式（无完整在线文档说明）
+
+```bash
+pip install git+https://github.com/CNFeffery/feffery-antd-components.git
+```
+
+## 3 已实现部件目录
+
+### 3.1 最新测试版本
+
+- 日期选择框：`AntdDatePicker`
+- 日期范围选择框：`AntdDateRangePicker`
+- 分割线：`AntdDivider`
+- 按钮：`AntdButton`
+- 下拉选择：`AntdSelect`
+- 树形控件：`AntdTree`
+- 表格控件：`AntdTable`
+- 锚点：`AntdAnchor`
+- 穿梭框：`AntdTransfer`
+- 滑杆输入：`AntdSlider`
+- 步骤条：`AntdSteps`
+- 导航菜单：`AntdMenu`
+
+### 3.2 最新开发版本
+
+- 折叠面板：`AntdCollapse`
+- 栅格系统
+  - `AntdRow`
+  - `AntdCol`
+- 布局
+  - `AntdLayout`
+  - `AntdHeader`
+  - `AntdContent`
+  - `AntdFooter`
+  - `AntdSider`
+- 对话框：`AntdModal`
+- 警告提示：`AntdAlert`
+- 通知提醒框：`AntdNotification`
+- 全局提示：`AntdMessage`
+- 标签：`AntdTag`
+
+## 3 说明文档
+
+<a href='http://121.40.46.113:8050/feffery-antd-docs/index' target='_blank'>在线交互式说明文档</a>
+
+## 4 TODO计划
+
+- 文字提示：`AntdTooltip`
+- 徽标数：`AntdBadge`
+- 标签页：`AntdTabs`
+- 描述列表：`AntdDescriptions`
+- 空状态：`AntdEmpty`
+- 气泡卡片：`AntdPopover`
+- 回到顶部：`AntdBackTop`
---
+++ packages/feffery-antd-components/feffery_antd_components-0.0.1a29.tar.gz/feffery_antd_components-0.0.1a29/feffery_antd_components/_imports_.py
@@ -0,0 +1,55 @@
+from .AntdAlert import AntdAlert
+from .AntdAnchor import AntdAnchor
+from .AntdButton import AntdButton
+from .AntdCollapse import AntdCollapse
+from .AntdDatePicker import AntdDatePicker
+from .AntdDateRangePicker import AntdDateRangePicker
+from .AntdDivider import AntdDivider
+from .AntdMenu import AntdMenu
+from .AntdMessage import AntdMessage
+from .AntdModal import AntdModal
+from .AntdNotification import AntdNotification
+from .AntdSelect import AntdSelect
+from .AntdSlider import AntdSlider
+from .AntdSpace import AntdSpace
+from .AntdSteps import AntdSteps
+from .AntdTable import AntdTable
+from .AntdTag import AntdTag
+from .AntdTransfer import AntdTransfer
+from .AntdTree import AntdTree
+from .AntdCol import AntdCol
+from .AntdRow import AntdRow
+from .AntdContent import AntdContent
+from .AntdFooter import AntdFooter
+from .AntdHeader import AntdHeader
+from .AntdLayout import AntdLayout
+from .AntdSider import AntdSider
+
+__all__ = [
+    "AntdAlert",
+    "AntdAnchor",
+    "AntdButton",
+    "AntdCollapse",
+    "AntdDatePicker",
+    "AntdDateRangePicker",
+    "AntdDivider",
+    "AntdMenu",
+    "AntdMessage",
+    "AntdModal",
+    "AntdNotification",
+    "AntdSelect",
+    "AntdSlider",
+    "AntdSpace",
+    "AntdSteps",
+    "AntdTable",
+    "AntdTag",
+    "AntdTransfer",
+    "AntdTree",
+    "AntdCol",
+    "AntdRow",
+    "AntdContent",
+    "AntdFooter",
+    "AntdHeader",
+    "AntdLayout",
+    "AntdSider"
+]

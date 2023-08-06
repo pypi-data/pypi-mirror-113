7489d16caa399eb14de369764f4e14c97ce0409a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:29 +0000

    Add package vidXs/2.5/vidXs-2.5-py3-none-any.whl

---
+++ packages/vidXs/vidXs-2.5-py3-none-any.whl/vidXs/__init__.py
@@ -0,0 +1,6 @@
+from vidXs.streaming import StreamingServer
+from vidXs.streaming import CameraClient
+from vidXs.streaming import VideoClient
+from vidXs.streaming import ScreenShareClient
+from vidXs.audio import AudioSender
+from vidXs.audio import AudioReceiver

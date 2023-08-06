6a8f484208874ef598c956d1fc3195a925dcb04c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:04 +0000

    Add package docker-checks/0.0.6/docker_checks-0.0.6.tar.gz

---
+++ packages/docker-checks/docker_checks-0.0.6.tar.gz/docker_checks-0.0.6/docker_checks/checks.py
@@ -0,0 +1,14 @@
+import sys
+print(sys.path)
+from check_add import checkAddStatement
+from check_from import checkValidImage, checkLatestTag
+from check_healthcheck import checkHealth
+from check_upgrade import checkUpgradePackage
+
+
+if __name__ == "__main__":
+    checkAddStatement()
+    checkValidImage()
+    checkLatestTag()
+    checkHealth()
+    checkUpgradePackage()

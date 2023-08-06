76956e3a3a984e8a6360a0f1d080c445135a0d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:07 +0000

    Add package py3mailer/1.0.4/py3mailer-1.0.4-py3-none-any.whl

---
+++ packages/py3mailer/py3mailer-1.0.4-py3-none-any.whl/pymailer/__about__.py
@@ -0,0 +1,9 @@
+__version__ = '1.0.4'
+__author__ = 'Shubham Sharma'
+__email__ = 'shubhamjsharma10@gmail.com'
+__description__ = 'A simple and convinient way to send emails in python'
+__author__ = "The Python Packaging Authority"
+__email__ = "admin@pypi.org"
+
+__license__ = "Apache License, Version 2.0"
+__copyright__ = "2018 %s" % __author__

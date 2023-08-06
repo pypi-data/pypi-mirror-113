ee8a3bb6d5d3a016c153bef6aab759d701e04943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:16 +0000

    Add package mailrise/1.1.0/mailrise-1.1.0-py2.py3-none-any.whl

---
+++ packages/mailrise/mailrise-1.1.0-py2.py3-none-any.whl/mailrise/config.py
@@ -0,0 +1,183 @@
+"""
+This is the YAML configuration parser for Mailrise.
+"""
+
+from __future__ import annotations
+
+import io
+import os
+import typing as typ
+from dataclasses import dataclass
+from enum import Enum
+from logging import Logger
+from string import Template
+
+import apprise  # type: ignore
+import yaml
+from apprise.common import NotifyType  # type: ignore
+
+
+DEFAULT_ASSET = apprise.AppriseAsset(
+    app_id='Mailrise',
+    app_desc='Mailrise SMTP Notification Relay',
+    app_url='https://mailrise.xyz',
+    html_notify_map={
+        NotifyType.INFO: '#2e6e99',
+        NotifyType.SUCCESS: '#2e992e',
+        NotifyType.WARNING: '#99972e',
+        NotifyType.FAILURE: '#993a2e'
+    },
+    theme=None,
+    default_extension='.png',
+    image_url_mask='https://raw.githubusercontent.com/YoRyan/mailrise/main/'
+                   'src/mailrise/asset/mailrise-{TYPE}-{XY}{EXTENSION}',
+    image_url_logo='https://raw.githubusercontent.com/YoRyan/mailrise/main/'
+                   'src/mailrise/asset/mailrise-logo.png',
+    image_path_mask=os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            'asset',
+            'mailrise-{TYPE}-{XY}{EXTENSION}'
+        )
+    )
+)
+
+
+class ConfigFileError(Exception):
+    """Exception raised for invalid configuration files.
+
+    Attributes:
+        message: The reason the configuration file is invalid.
+    """
+    message: str
+
+    def __init__(self, message: str) -> None:
+        self.message = message
+
+
+class TLSMode(Enum):
+    """Specifies a TLS encryption operating mode."""
+    OFF = 'no TLS'
+    ONCONNECT = 'TLS on connect'
+    STARTTLS = 'STARTTLS, optional'
+    STARTTLSREQUIRE = 'STARTTLS, required'
+
+
+@dataclass
+class Sender:
+    """A configured target for Apprise notifications.
+
+    Attributes:
+        apprise: The Apprise instance.
+        title_template: The template string for notification title texts.
+        body_template: The template string for notification body texts.
+        body_format: The content type for notifications. If None, this will be
+            auto-detected from the body parts of emails.
+    """
+    apprise: apprise.Apprise
+    title_template: Template
+    body_template: Template
+    body_format: typ.Optional[apprise.NotifyFormat]
+
+
+@dataclass
+class MailriseConfig:
+    """Configuration data for a Mailrise instance.
+
+    Attributes:
+        logger: The logger, which is used to record interesting events.
+        listen_host: The network address to listen on.
+        listen_port: The network port to listen on.
+        tls_mode: The TLS encryption mode.
+        tls_certfile: The path to the TLS certificate chain file.
+        tls_keyfile: The path to the TLS key file.
+        smtp_hostname: The advertised SMTP server hostname.
+        senders: A dictionary of notification targets. The key is the name of
+            the configuration, and the value is the Sender instance itself.
+    """
+    logger: Logger
+    listen_host: str
+    listen_port: int
+    tls_mode: TLSMode
+    tls_certfile: typ.Optional[str]
+    tls_keyfile: typ.Optional[str]
+    smtp_hostname: typ.Optional[str]
+    senders: dict[str, Sender]
+
+
+def load_config(logger: Logger, f: io.TextIOWrapper) -> MailriseConfig:
+    """Loads configuration data from a YAML file.
+
+    Args:
+        logger: The logger, which will be passed to the `MailriseConfig` instance.
+        f: The file handle to load YAML from.
+
+    Returns:
+        The `MailriseConfig` instance.
+
+    Raises:
+        ConfigFileError: The configuration file is invalid.
+    """
+    yml = yaml.safe_load(f)
+    if not isinstance(yml, dict):
+        raise ConfigFileError("root node not a mapping")
+
+    yml_listen = yml.get('listen', {})
+
+    yml_tls = yml.get('tls', {})
+    try:
+        tls_mode = TLSMode[yml_tls.get('mode', 'off').upper()]
+    except KeyError:
+        raise ConfigFileError('invalid TLS operating mode')
+    tls_certfile = yml_tls.get('certfile', None)
+    tls_keyfile = yml_tls.get('keyfile', None)
+    if tls_mode != TLSMode.OFF and not (tls_certfile and tls_keyfile):
+        raise ConfigFileError(
+            'TLS enabled, but certificate and key files not specified')
+
+    yml_smtp = yml.get('smtp', {})
+
+    yml_configs = yml.get('configs', [])
+    if not isinstance(yml_configs, dict):
+        raise ConfigFileError("'configs' node not a mapping")
+    senders = {key: _load_sender(config) for key, config in yml_configs.items()}
+
+    logger.info('Loaded configuration with %d recipient(s)', len(senders))
+    return MailriseConfig(
+        logger=logger,
+        listen_host=yml_listen.get('host', ''),
+        listen_port=yml_listen.get('port', 8025),
+        tls_mode=tls_mode,
+        tls_certfile=tls_certfile,
+        tls_keyfile=tls_keyfile,
+        smtp_hostname=yml_smtp.get('hostname', None),
+        senders=senders
+    )
+
+
+def _load_sender(config: dict[str, typ.Any]) -> Sender:
+    if not isinstance(config, dict):
+        raise ConfigFileError("apprise config node not a mapping")
+
+    # Extract Mailrise-specific values.
+    mr_config = config.get('mailrise', {})
+    config.pop('mailrise', None)
+    title_template = mr_config.get('title_template', '$subject ($from)')
+    body_template = mr_config.get('body_template', '$body')
+    body_format = mr_config.get('body_format', None)
+    if not any(body_format == c for c in (None,
+                                          apprise.NotifyFormat.TEXT,
+                                          apprise.NotifyFormat.HTML,
+                                          apprise.NotifyFormat.MARKDOWN)):
+        raise ConfigFileError(f"invalid apprise notification format: {body_format}")
+
+    aconfig = apprise.AppriseConfig(asset=DEFAULT_ASSET)
+    aconfig.add_config(yaml.safe_dump(config), format='yaml')
+    apobj = apprise.Apprise(aconfig)
+
+    return Sender(
+        apprise=apobj,
+        title_template=Template(title_template),
+        body_template=Template(body_template),
+        body_format=body_format
+    )
---
+++ packages/mailrise/mailrise-1.1.0-py2.py3-none-any.whl/mailrise/smtp.py
@@ -0,0 +1,281 @@
+"""
+This is the SMTP server functionality for Mailrise.
+"""
+
+from __future__ import annotations
+
+import asyncio
+import email.policy
+import functools
+import os
+import re
+import typing as typ
+from asyncio import get_running_loop
+from dataclasses import dataclass
+from email.message import EmailMessage
+from email.parser import BytesParser
+from email.utils import parseaddr
+from tempfile import NamedTemporaryFile
+
+from mailrise.config import MailriseConfig
+
+import apprise  # type: ignore
+from aiosmtpd.smtp import Envelope, Session, SMTP
+from apprise.attachment import AttachBase  # type: ignore
+from apprise.common import ContentLocation  # type: ignore
+
+
+class RecipientError(Exception):
+    """Exception raised for invalid recipient email addresses.
+
+    Attributes:
+        message: The reason the recipient is invalid.
+    """
+    message: str
+
+    def __init__(self, message: str) -> None:
+        self.message = message
+
+
+class AppriseNotifyFailure(Exception):
+    """Exception raised when Apprise fails to deliver a notification.
+
+    Note: Apprise does not provide any information about the reason for the
+    failure."""
+    pass
+
+
+@dataclass
+class Recipient:
+    """The routing information encoded into a recipient address.
+
+    Attributes:
+        config_key: An index into the `configs` dictionary.
+        notify_type: The type of notification to send.
+    """
+    config_key: str
+    notify_type: apprise.NotifyType
+
+
+def parsercpt(addr: str) -> Recipient:
+    """Parses an email address into a `Recipient`.
+
+    Args:
+        addr: The email address to parse.
+
+    Returns:
+        The `Recipient` instance.
+    """
+    _, email = parseaddr(addr)
+    rx_types = r'((?:\.(?:info|success|warning|failure))?)'
+    rx = f'(?:"([^"@\\.]*){rx_types}"|([^@\\.]*){rx_types})@mailrise\\.xyz$'
+    match = re.search(rx, email, re.IGNORECASE)
+    if match is None:
+        raise RecipientError(f"'{email}' is not a valid mailrise recipient")
+    quoted = match.group(1) is not None
+    key = match.group(1) if quoted else match.group(3)
+    ntypes = (match.group(2) if quoted else match.group(4)).lower()
+
+    ntype = apprise.NotifyType.INFO
+    if ntypes == '.info':
+        pass
+    elif ntypes == '.success':
+        ntype = apprise.NotifyType.SUCCESS
+    elif ntypes == '.warning':
+        ntype = apprise.NotifyType.WARNING
+    elif ntypes == '.failure':
+        ntype = apprise.NotifyType.FAILURE
+
+    return Recipient(config_key=key, notify_type=ntype)
+
+
+@dataclass
+class AppriseHandler:
+    """The aiosmtpd handler for Mailrise. Dispatches Apprise notifications.
+
+    Attributes:
+        config: This server's Mailrise configuration.
+    """
+    config: MailriseConfig
+
+    async def handle_RCPT(self, server: SMTP, session: Session, envelope: Envelope,
+                          address: str, rcpt_options: list[str]) -> str:
+        try:
+            rcpt = parsercpt(address)
+        except RecipientError as e:
+            return f'550 {e.message}'
+        if rcpt.config_key not in self.config.senders:
+            return '551 recipient does not exist in configuration file'
+        self.config.logger.info('Accepted recipient: %s', address)
+        envelope.rcpt_tos.append(address)
+        return '250 OK'
+
+    async def handle_DATA(self, server: SMTP, session: Session, envelope: Envelope) \
+            -> str:
+        assert isinstance(envelope.content, bytes)
+        parser = BytesParser(policy=email.policy.default)
+        message = parser.parsebytes(envelope.content)
+        assert isinstance(message, EmailMessage)
+        notification = parsemessage(message)
+        self.config.logger.info('Accepted email, subject: %s', notification.subject)
+
+        rcpts = (parsercpt(addr) for addr in envelope.rcpt_tos)
+        aws = (notification.submit(self.config, rcpt) for rcpt in rcpts)
+        try:
+            await asyncio.gather(*aws)
+        except AppriseNotifyFailure:
+            return '450 failed to send notification'
+        return '250 OK'
+
+
+@dataclass
+class Attachment:
+    """Represents an email attachment.
+
+    Attributes:
+        data: The contents of the attachment.
+        filename: The filename of the attachment as it was set by the sender.
+    """
+    data: bytes
+    filename: str
+
+
+@dataclass
+class EmailNotification:
+    """Represents an email accepted for notifying.
+
+    Attributes:
+        subject: The email subject.
+        from_: The email sender address.
+        body: The contents of the email.
+        body_format: The type of the contents of the email.
+        attachments: The email attachments.
+    """
+    subject: str
+    from_: str
+    body: str
+    body_format: apprise.NotifyFormat
+    attachments: list[Attachment]
+
+    async def submit(self, config: MailriseConfig, rcpt: Recipient) -> None:
+        """Turns the email into an Apprise notification and submits it.
+
+        Args:
+            config: The Mailrise configuration to use.
+            rcpt: The recipient data to use.
+
+        Raises:
+            AppriseNotifyFailure: Apprise failed to submit the notification.
+        """
+        sender = config.senders[rcpt.config_key]
+        mapping = {
+            'subject': self.subject,
+            'from': self.from_,
+            'body': self.body,
+            'config': rcpt.config_key,
+            'type': rcpt.notify_type
+        }
+        attachbase = [AttachMailrise(config, attach) for attach in self.attachments]
+        notify = functools.partial(
+            sender.apprise.notify,
+            title=sender.title_template.safe_substitute(mapping),
+            body=sender.body_template.safe_substitute(mapping),
+            # Use the configuration body format if specified.
+            body_format=sender.body_format or self.body_format,
+            notify_type=rcpt.notify_type,
+            attach=apprise.AppriseAttachment(attachbase)
+        )
+        res = await get_running_loop().run_in_executor(None, notify)
+        # NOTE: This should probably be called by Apprise itself, but it isn't?
+        for ab in attachbase:
+            ab.invalidate()
+        if not res:
+            raise AppriseNotifyFailure()
+
+
+def parsemessage(msg: EmailMessage) -> EmailNotification:
+    """Parses an email message into an `EmailNotification`.
+
+    Args:
+        msg: The email message.
+
+    Returns:
+        The `EmailNotification` instance.
+    """
+    body_part = msg.get_body()
+    body: str
+    body_format: apprise.NotifyFormat
+    if isinstance(body_part, EmailMessage):
+        body = body_part.get_content().strip()
+        body_format = \
+            (apprise.NotifyFormat.HTML if body_part.get_content_subtype() == 'html'
+             else apprise.NotifyFormat.TEXT)
+    else:
+        body = ''
+        body_format = apprise.NotifyFormat.TEXT
+    attachments = [_parseattachment(part) for part in msg.iter_attachments()
+                   if isinstance(part, EmailMessage)]
+    return EmailNotification(
+        subject=msg.get('Subject', '[no subject]'),
+        from_=msg.get('From', '[no sender]'),
+        body=body,
+        body_format=body_format,
+        attachments=attachments
+    )
+
+
+def _parseattachment(part: EmailMessage) -> Attachment:
+    return Attachment(data=part.get_content(), filename=part.get_filename(''))
+
+
+class AttachMailrise(AttachBase):
+    """An Apprise attachment type that wraps `Attachment`.
+
+    Data is stored in temporary files for upload.
+
+    Args:
+        config: The Mailrise configuration to use.
+        attach: The `Attachment` instance.
+    """
+    detected_name: typ.Optional[str]
+    download_path: typ.Optional[str]
+
+    location = ContentLocation.LOCAL
+
+    _mrfile = None  # Satisfy mypy by initializing as an Optional.
+
+    def __init__(self, config: MailriseConfig,
+                 attach: Attachment, **kwargs: typ.Any) -> None:
+        super().__init__(**kwargs)
+        self._mrconfig = config
+        self._mrattach = attach
+
+    def download(self) -> bool:
+        self.invalidate()
+
+        tfile = NamedTemporaryFile(delete=False)
+        tfile.write(self._mrattach.data)
+        tfile.close()
+        self._mrfile = tfile
+        self.download_path = tfile.name
+        self.detected_name = self._mrattach.filename
+
+        return True  # Indicates the "download" was successful.
+
+    def invalidate(self) -> None:
+        tfile = self._mrfile
+        if tfile:
+            try:
+                os.remove(tfile.name)
+            except (FileNotFoundError, OSError):
+                self._mrconfig.logger.info(
+                    'Failed to delete attachment file: %s', tfile.name)
+            self._mrfile = None
+        super().invalidate()
+
+    def url(self, **kwargs: typ.Any) -> str:
+        return f'mailrise://{hex(id(self))}'
+
+    @staticmethod
+    def parse_url(url: str, verify_host: bool = True) -> typ.Dict[str, typ.Any]:
+        return {}

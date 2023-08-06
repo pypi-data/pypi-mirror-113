39246d76c4f5769fae5273413803905b67d0774e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:48 +0000

    Add package licsber/2.7.0/licsber-2.7.0.tar.gz

---
+++ packages/licsber/licsber-2.7.0.tar.gz/licsber-2.7.0/licsber/shell/__init__.py
@@ -0,0 +1,12 @@
+from .dir_ops import archive
+from .dir_ops import clean_ds_store
+from .dir_ops import count_dir
+from .dir_ops import empty_dir
+from .dir_ops import flatten_dir
+from .dir_ops import rename
+from .dir_ops import walk_files
+from .memobird import memobird
+
+
+def licsber():
+    print('Hello, Licsber.')

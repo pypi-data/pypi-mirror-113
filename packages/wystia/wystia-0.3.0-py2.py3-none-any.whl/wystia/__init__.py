ab3249c8fc421559a6824e36ecd2581b592cacb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:14 +0000

    Add package wystia/0.3.0/wystia-0.3.0-py2.py3-none-any.whl

---
+++ packages/wystia/wystia-0.3.0-py2.py3-none-any.whl/wystia/__init__.py
@@ -0,0 +1,31 @@
+"""
+A Python wrapper library for the Wistia API
+
+
+Docs:
+    - https://wistia.com/support/developers/data-api
+    - https://wistia.com/support/developers/upload-api
+    - https://wistia.com/support/developers/embed-options
+
+"""
+__all__ = ['WistiaDataApi',
+           'WistiaEmbedApi',
+           'WistiaUploadApi',
+           'WistiaHelper']
+
+import logging
+
+from .api_data import WistiaDataApi
+from .api_embed import WistiaEmbedApi
+from .api_upload import WistiaUploadApi
+from .api_helper import WistiaHelper
+
+
+__author__ = 'Ritvik Nag'
+__email__ = 'rv.kvetch@gmail.com'
+__version__ = '0.3.0'
+
+
+# Set up logging to ``/dev/null`` like a library is supposed to.
+#   https://docs.python.org/3.8/howto/logging.html#configuring-logging-for-a-library
+logging.getLogger('wystia').addHandler(logging.NullHandler())

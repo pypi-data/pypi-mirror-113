a2b0d0ba4032e2e007bd70f981a7467425ae5315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:59 +0000

    Add package ec2objects/0.0.5/ec2objects-0.0.5.tar.gz

---
+++ packages/ec2objects/ec2objects-0.0.5.tar.gz/ec2objects-0.0.5/ec2objects/__init__.py
@@ -0,0 +1,11 @@
+from .ec2common.ec2exceptions import ParamNotFound as ParamNotFound
+from .ec2common.ec2exceptions import RequestType as RequestType
+from .ec2object.region import RegionManager as RegionManager
+from .ec2object.region import Region as Region
+from .ec2object.image import ImageManager as ImageManager
+from .ec2object.image import Image as Image
+
+###API TESTS####DELETE
+# from .ec2api.images import Images as Images
+
+################
---
+++ packages/ec2objects/ec2objects-0.0.5.tar.gz/ec2objects-0.0.5/ec2objects/ec2common/ec2exceptions.py
@@ -0,0 +1,23 @@
+class ParamNotFound(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class RequestType(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class HostEndpoint(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class APIKeys(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class RegionNotFound(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)

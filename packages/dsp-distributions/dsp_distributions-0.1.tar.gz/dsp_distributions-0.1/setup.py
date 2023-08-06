8dbe3c87af7330ff2e2748f6f8905e0c1a466e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:09 +0000

    Add package dsp-distributions/0.1/dsp_distributions-0.1.tar.gz

---
+++ packages/dsp-distributions/dsp_distributions-0.1.tar.gz/dsp_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsp_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsp_distributions'],
+      author='David Stefan Pedemonte',
+      author_email='david@pedemonte.net',
+      zip_safe=False)

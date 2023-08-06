049e0e350173e31e0e32b13e768f4a31acde2a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:09 +0000

    Add package teamscale-cli/7.1.0/teamscale-cli-7.1.0.tar.gz

---
+++ packages/teamscale-cli/teamscale-cli-7.1.0.tar.gz/teamscale-cli-7.1.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+setup(
+    name="teamscale-cli",
+    version="7.1.0",
+    author="Thomas Kinnen - CQSE GmbH",
+    author_email="kinnen@cqse.eu",
+    description=("Client for performing precommit analysis with Teamscale."),
+    license="Apache",
+    keywords="teamscale client precommit",
+    url="https://github.com/cqse/teamscale-cli",
+    packages=['teamscale_precommit_client'],
+    long_description="Command line client for performing precommit analysis with Teamscale.",
+    classifiers=[
+        "Topic :: Utilities",
+    ],
+    entry_points={
+        'console_scripts': [
+            'teamscale-cli=teamscale_precommit_client.precommit_client:run'
+        ]
+    },
+    install_requires=[
+        'teamscale-client==6.7.1',
+        'gitpython==2.1.15',
+
+        # Required for gitpython, build fails without specifying a fixed version.
+        # 2.0.6 is the latest version working with python 2.7 according to
+        # https://github.com/gitpython-developers/gitdb/issues/61#issuecomment-590275845
+        'gitdb2==2.0.6',
+
+        # Required to compile to a native binary
+        'nuitka==0.6.14.7'
+    ],
+    tests_require=[
+        'teamscale-client',
+        'pytest',
+        'responses',
+        'mock'
+    ],
+    setup_requires=["pytest-runner"]
+)

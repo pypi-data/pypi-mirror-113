527b740aa8f637d96d180faeb7baa177d0d09d12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:15 +0000

    Add package circuitpython-stubs/7.0.0a5.dev148/circuitpython-stubs-7.0.0a5.dev148.tar.gz

---
+++ packages/circuitpython-stubs/circuitpython-stubs-7.0.0a5.dev148.tar.gz/circuitpython-stubs-7.0.0a5.dev148/ulab/numpy/transform/__init__.pyi
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from typing import Union
+
+import ulab
+
+def dot(
+    m1: ulab.numpy.ndarray, m2: ulab.numpy.ndarray
+) -> Union[ulab.numpy.ndarray, float]:
+    """
+    :param ~ulab.numpy.ndarray m1: a matrix, or a vector
+    :param ~ulab.numpy.ndarray m2: a matrix, or a vector
+
+    Computes the product of two matrices, or two vectors. In the letter case, the inner product is returned."""
+    ...

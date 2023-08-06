a8d7d62ae1911231dd4ce4e0ab171b6c5aeceded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:57 +0000

    Add package aet/0.1.3/aet-0.1.3.tar.gz

---
+++ packages/aet/aet-0.1.3.tar.gz/aet-0.1.3/aet/commands.py
@@ -0,0 +1,147 @@
+import getpass
+import pkg_resources
+import click
+import os
+import json
+import yaml
+import sys
+
+from .client import Client
+from .helpers import (
+    load_token, default_project_dir, default_target_dir,
+    save_credentials
+)
+from .loggers import log_response
+
+
+class Context(object):
+    pass
+
+
+@click.group()
+@click.option(
+    '--verbose/--no-verbose',
+    default=False,
+    help="Print more verbose output"
+)
+@click.pass_context
+def cli(ctx, verbose):
+    token = load_token()
+    ctx.obj = Context()
+    ctx.obj.token = token
+    ctx.obj.verbose = verbose
+
+
+@cli.group()
+@click.pass_context
+def alerts(ctx):
+    """
+    Commands for managing alerts
+    """
+
+    pass
+
+
+@alerts.command()
+@click.pass_context
+def list(ctx):
+    client = Client(token=ctx.obj.token)
+    resp = client.get('/notifications')
+    log_response(resp, verbose=True)
+
+
+@cli.command()
+def version():
+    """
+    Print version information
+    """
+
+    print(pkg_resources.require('aet')[0].version)
+
+
+@cli.command()
+@click.option(
+    '--target-dir',
+    help="Which directory to look in for the compiled dbt assets "
+         "file (run_results.json, manifest.json)",
+    default=default_target_dir
+)
+@click.option(
+    '--project-dir',
+    default=default_project_dir,
+    help="Which directory to look in for the dbt_project.yml file"
+)
+@click.pass_context
+def push(ctx, target_dir, project_dir):
+    """
+    Send alerts about the latest run or test command
+    """
+
+    client = Client(token=ctx.obj.token)
+    run_results_file = os.path.join(target_dir, 'run_results.json')
+    manifest_file = os.path.join(target_dir, 'manifest.json')
+    dbt_project_file = os.path.join(project_dir, 'dbt_project.yml')
+
+    run_results_json = json.loads(open(run_results_file, 'r').read())
+    manifest_json = json.loads(open(manifest_file, 'r').read())
+    dbt_project_json = yaml.load(open(dbt_project_file, 'r').read(),
+                                 Loader=yaml.FullLoader)
+
+    data = {
+        'run_results': run_results_json,
+        'manifest': manifest_json,
+        'dbt_project': dbt_project_json
+    }
+
+    resp = client.post('/artifacts', data=data)
+    log_response(resp, message="Successfully uploaded artifacts.",
+                 verbose=ctx.obj.verbose)
+
+
+@cli.command()
+@click.pass_context
+def login(ctx):
+    """
+    Log into the aet service
+    """
+
+    client = Client(token=ctx.obj.token)
+
+    email = input("Enter email: ")
+    password = getpass.getpass("Enter password: ")
+
+    resp = client.post('/login', data={
+        'email': email,
+        'password': password
+    })
+    if resp.ok:
+        save_credentials(resp.json())
+    log_response(resp, message="Successfully logged in",
+                 verbose=ctx.obj.verbose)
+
+
+@cli.command()
+@click.pass_context
+def register(ctx):
+    """
+    Register a new account
+    """
+
+    client = Client(token=ctx.obj.token)
+
+    email = input("Enter email: ")
+    password = getpass.getpass("Enter password: ")
+    password2 = getpass.getpass("Confirm password: ")
+
+    if password != password2:
+        print("Passwords do match")
+        sys.exit(1)
+
+    resp = client.post('/users', data={
+        'email': email,
+        'password': password
+    })
+
+    if resp.ok:
+        save_credentials(resp.json())
+    log_response(resp, message="Successfully created account")

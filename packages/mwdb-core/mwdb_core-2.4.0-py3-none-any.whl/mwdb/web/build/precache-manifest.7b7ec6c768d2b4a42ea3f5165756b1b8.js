6806e545c21416c060dad71c6a6f653905f6dda6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:59 +0000

    Add package mwdb-core/2.4.0/mwdb_core-2.4.0-py3-none-any.whl

---
+++ packages/mwdb-core/mwdb_core-2.4.0-py3-none-any.whl/mwdb/web/build/precache-manifest.7b7ec6c768d2b4a42ea3f5165756b1b8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403470be864dff940417d2dbe2844b45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dcae727056c8807e7d9",
+    "url": "/static/css/4.0e969c87.chunk.css"
+  },
+  {
+    "revision": "bfa8ebe4fb14a4a1d93c",
+    "url": "/static/css/main.25283c0c.chunk.css"
+  },
+  {
+    "revision": "a6042ec2db49e1abac10",
+    "url": "/static/js/2.8a4bd6d4.chunk.js"
+  },
+  {
+    "revision": "edfbe277726d10a5bfa522516f56c50f",
+    "url": "/static/js/2.8a4bd6d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ef30a7b0b9165ae3fb",
+    "url": "/static/js/3.33decaad.chunk.js"
+  },
+  {
+    "revision": "16ff3d125bdc83af9ed50364d7b45315",
+    "url": "/static/js/3.33decaad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dcae727056c8807e7d9",
+    "url": "/static/js/4.eda8d879.chunk.js"
+  },
+  {
+    "revision": "4daa210f83e030451929218c399eac15",
+    "url": "/static/js/4.eda8d879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cc192c3c4119eaeab65",
+    "url": "/static/js/5.a967ecef.chunk.js"
+  },
+  {
+    "revision": "28e46d39bd31afe245d9",
+    "url": "/static/js/6.f39aeeee.chunk.js"
+  },
+  {
+    "revision": "bfa8ebe4fb14a4a1d93c",
+    "url": "/static/js/main.f3b947ee.chunk.js"
+  },
+  {
+    "revision": "e526d8af5a7815d618d4",
+    "url": "/static/js/runtime-main.0d5be36d.js"
+  },
+  {
+    "revision": "b21df568d6dd40d16a1f30ac4eb81763",
+    "url": "/static/media/logo.b21df568.png"
+  }
+]);
---
+++ packages/mwdb-core/mwdb_core-2.4.0-py3-none-any.whl/mwdb/web/build/static/js/5.a967ecef.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmwdb-web"]=this["webpackJsonpmwdb-web"]||[]).push([[5],{1178:function(b,n){},1180:function(b,n){}}]);
+//# sourceMappingURL=5.a967ecef.chunk.js.map

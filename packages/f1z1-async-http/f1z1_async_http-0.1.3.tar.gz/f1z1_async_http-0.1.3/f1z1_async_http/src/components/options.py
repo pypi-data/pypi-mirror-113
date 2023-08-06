1be840638517cd858539ffe32eff7ceaa8c2ae9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:39 +0000

    Add package f1z1-async-http/0.1.3/f1z1_async_http-0.1.3.tar.gz

---
+++ packages/f1z1-async-http/f1z1_async_http-0.1.3.tar.gz/f1z1_async_http-0.1.3/f1z1_async_http/src/components/options.py
@@ -0,0 +1,101 @@
+# @Time     : 2021/5/30
+# @Project  : f1z1-g
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.comf
+from collections import defaultdict, OrderedDict
+from typing import Any, Dict, Optional
+
+
+class IRequestOption(object):
+
+    def set(self, key, value) -> None:
+        raise NotImplementedError("")
+
+    def get(self, key, default=None):
+        raise NotImplementedError("")
+
+    def as_dict(self) -> Optional[Dict]:
+        raise NotImplementedError("")
+
+
+class IRequestOptions(object):
+
+    def add_cookies(self, key, value):
+        raise NotImplementedError("")
+
+    def add_data(self, key, value):
+        raise NotImplementedError("")
+
+    def add_headers(self, key, value):
+        raise NotImplementedError("")
+
+    def add_json(self, key, value):
+        raise NotImplementedError("")
+
+    def add_params(self, key, value):
+        raise NotImplementedError("")
+
+    def as_dict(self) -> Dict[str, Any]:
+        raise NotImplementedError("")
+
+
+class RequestOption(IRequestOption):
+    __slots__ = ["_option"]
+
+    def __init__(self):
+        self._option = OrderedDict()
+
+    def get(self, key, default=None):
+        return self._option.get(key, default)
+
+    def set(self, key, value) -> None:
+        self[key] = value
+
+    def as_dict(self):
+        if not self._option:
+            return None
+        return {key: value for key, value in self._option.items()}
+
+    def __setitem__(self, key, value):
+        self._option[key] = value
+
+    def __getitem__(self, key):
+        return self._option[key]
+
+
+class RequestOptions(IRequestOptions):
+    __slots__ = ["_options"]
+
+    def __init__(self):
+        self._options = defaultdict(RequestOption)
+
+    def add_cookies(self, key, value):
+        self._set("cookies", key, value)
+
+    def add_data(self, key, value):
+        self._set("data", key, value)
+
+    def add_headers(self, key, value):
+        self._set("headers", key, value)
+
+    def add_json(self, key, value):
+        self._set("json", key, value)
+
+    def add_params(self, key, value):
+        self._set("params", key, value)
+
+    def as_dict(self) -> Dict[str, Any]:
+        if not self._options:
+            return {}
+
+        return {
+            key: item.as_dict()
+            for key, item in self._options.items()
+        }
+
+    def _set(self, option: str, key, value):
+        self[option].set(key, value)
+
+    def __getitem__(self, key) -> IRequestOption:
+        return self._options[key]
---
+++ packages/f1z1-async-http/f1z1_async_http-0.1.3.tar.gz/f1z1_async_http-0.1.3/f1z1_async_http/src/core/factory.py
@@ -0,0 +1,85 @@
+# @Time     : 2021/5/30
+# @Project  : f1z1-g
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+from httpx import AsyncClient, URL, Timeout, Limits
+from f1z1_common import ReaderFactory, PathTypes, Encoding
+
+from ..components.conf import IConfig, DefaultConfig, ConfigReader, ConfigGenerator
+from ..components.interceptors import IInterceptors
+from ..components.message import IRequestMessage, RequestMessageQueue
+from ..components.notifier import AsyncRequestNotifier, AsyncResponseNotifier, notify
+from .client import IAsyncHttpClient, AsyncHttpClient
+
+
+class IConfigFactory(object):
+
+    @classmethod
+    def create(cls, file: PathTypes, node: str) -> IConfig:
+        raise NotImplementedError()
+
+
+class ConfigFactory(IConfigFactory):
+
+    @classmethod
+    def create(cls, file: PathTypes, node: str) -> IConfig:
+        reader = ReaderFactory.create(file, encoding=Encoding.UTF_8)
+        config_reader = ConfigReader(reader, node)
+        generator = ConfigGenerator(config_reader)
+        return generator.generate()
+
+
+class IAsyncHttpClientFactory(object):
+
+    def create(self) -> IAsyncHttpClient:
+        raise NotImplementedError()
+
+
+class AsyncHttpClientFactory(IAsyncHttpClientFactory):
+
+    def __init__(self, interceptors: IInterceptors, config: IConfig = None):
+        self._interceptors = interceptors
+        self._config = DefaultConfig() if not config else config
+
+    def create(self) -> IAsyncHttpClient:
+        rmq = RequestMessageQueue()
+        client = self._create_async_client_from(rmq)
+        return AsyncHttpClient(client, rmq)
+
+    def _create_async_client_from(self, message: IRequestMessage):
+        config = self._config
+        return AsyncClient(
+            base_url=self._create_base_url(config),
+            limits=self._create_limits(config),
+            timeout=self._create_timeout(config),
+            event_hooks=self._events_hooks(message)
+        )
+
+    def _create_base_url(self, config: IConfig):
+        return URL(config.base_url)
+
+    def _events_hooks(self, message: IRequestMessage):
+        interceptors = self._interceptors
+        return {
+            "request": [
+                notify(AsyncRequestNotifier(
+                    interceptors.request, message=message
+                ))
+            ],
+
+            "response": [
+                notify(AsyncResponseNotifier(
+                    interceptors.response, message
+                ))
+            ]
+        }
+
+    def _create_limits(self, config: IConfig):
+        return Limits(
+            max_connections=config.max_connection,
+            max_keepalive_connections=config.max_keepalive
+        )
+
+    def _create_timeout(self, config: IConfig):
+        return Timeout(config.timeout)

3d17f99a08495caf3511ed291f59a134dd5dacc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:40 +0000

    Add package njinn/0.3.2/njinn-0.3.2-py3-none-any.whl

---
+++ packages/njinn/njinn-0.3.2-py3-none-any.whl/njinn/models/__init__.py
@@ -0,0 +1,11 @@
+# Submodels need to be imported first
+from .config import Config
+from .group_member import GroupMember
+from .njinn_group import NjinnGroup
+from .task_execution import TaskExecution
+from .execution import Execution
+from .workflow import Workflow
+from .webhooklog import WebhookLog
+from .webhook import Webhook
+from .rule import Rule
+from .schedule import Schedule

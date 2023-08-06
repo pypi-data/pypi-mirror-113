e0e150f071d153628f5fe9f519fb0ab0aade2cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:52 +0000

    Add package GravityLab/2.2.7/GravityLab-2.2.7.tar.gz

---
+++ packages/GravityLab/GravityLab-2.2.7.tar.gz/GravityLab-2.2.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from setuptools import Extension
+from Cython.Distutils import build_ext
+import numpy
+
+extensions = [Extension("gravitylab.core.solvers", ["gravitylab/core/solvers.pyx"])]
+cmdclass = {"build_ext" : build_ext}
+
+setup(
+    name="GravityLab",
+    version="2.2.7",
+    packages=find_packages(),
+    license="MIT",
+    author="Kushaal Kumar Pothula",
+    author_email="kushaalkumar.astronomer@gmail.com",
+    url = "https://github.com/Kushaalkumar-pothula/gravitylab",
+    description="Fast astrophysical N-body simulator",
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    cmdclass=cmdclass,
+    ext_modules=extensions,
+    include_dirs=[numpy.get_include()],
+    install_requires = [
+        'numpy>=1.20.0',
+        'matplotlib>=3.3.2'
+    ]
+)

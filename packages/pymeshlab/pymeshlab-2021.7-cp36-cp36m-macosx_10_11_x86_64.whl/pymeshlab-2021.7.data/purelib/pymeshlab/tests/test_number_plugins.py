222534bff0d0a147a5363620789d013b3d379f14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:35 +0000

    Add package pymeshlab/2021.7/pymeshlab-2021.7-cp36-cp36m-macosx_10_11_x86_64.whl

---
+++ packages/pymeshlab/pymeshlab-2021.7-cp36-cp36m-macosx_10_11_x86_64.whl/pymeshlab-2021.7.data/purelib/pymeshlab/tests/test_number_plugins.py
@@ -0,0 +1,14 @@
+import pymeshlab as ml
+
+# temp
+from sys import platform
+#
+
+
+def test_number_plugins():
+    print('\n')
+    n_plugins = ml.number_plugins()
+
+    ml.print_plugin_list()
+
+    assert(n_plugins == 52)

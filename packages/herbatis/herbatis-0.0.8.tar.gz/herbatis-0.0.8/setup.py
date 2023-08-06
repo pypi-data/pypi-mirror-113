f823615e752a5aede86961a799f98f6b3db977f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:31 +0000

    Add package herbatis/0.0.8/herbatis-0.0.8.tar.gz

---
+++ packages/herbatis/herbatis-0.0.8.tar.gz/herbatis-0.0.8/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='herbatis',
+    version='0.0.8',
+    author='Mr.Zhang',
+    author_email='zhang.pengbird@gmail.com',
+    url='https://zhuanlan.zhihu.com/p/26159930',
+    description=u'A mybatis of python',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+        ]
+    }
+)

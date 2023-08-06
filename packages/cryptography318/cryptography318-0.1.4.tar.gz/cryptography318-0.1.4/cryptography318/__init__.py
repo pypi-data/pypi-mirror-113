aa0b728c81adc28c221b6a505c0b875309171f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:37 +0000

    Add package cryptography318/0.1.4/cryptography318-0.1.4.tar.gz

---
+++ packages/cryptography318/cryptography318-0.1.4.tar.gz/cryptography318-0.1.4/cryptography318/__init__.py
@@ -0,0 +1,9 @@
+from cryptography318.crypto_functions import *
+from cryptography318.matrix import *
+from cryptography318.bailliepsw_helper import *
+from cryptography318.prime import *
+import setuptools.version
+
+
+__version__ = setuptools.version.__version__
+__author__ = 'Andrew Carpenter'
---
+++ packages/cryptography318/cryptography318-0.1.4.tar.gz/cryptography318-0.1.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='cryptography318',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.1.4',
+    description='A set of functions useful in cryptography.',
+    url='https://github.com/aarpyy/Cryptography',
+    author='Andrew Carpenter',
+    author_email='acarpent@oberlin.edu',
+    packages=['cryptography318'],
+    install_requires=['numpy'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)

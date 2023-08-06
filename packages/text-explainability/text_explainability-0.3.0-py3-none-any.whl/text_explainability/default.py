6d6e22baee02d4e08e7dd59be193e304c11ea292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:03 +0000

    Add package text-explainability/0.3.0/text_explainability-0.3.0-py3-none-any.whl

---
+++ packages/text-explainability/text_explainability-0.3.0-py3-none-any.whl/text_explainability/default.py
@@ -0,0 +1,6 @@
+class Readable:
+    """Ensure that a class has a readable representation."""
+
+    def __repr__(self):
+        public_vars = ', '.join([f'{k}={v}' for k, v in vars(self).items() if not k.startswith('_')])
+        return f'{self.__class__.__name__}({public_vars})'

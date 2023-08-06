9e723cbed9ced1c11c0981bdb06e7b0884d4e1f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:30 +0000

    Add package intake-dremio/0.1.0a6/intake_dremio-0.1.0a6-py2.py3-none-any.whl

---
+++ packages/intake-dremio/intake_dremio-0.1.0a6-py2.py3-none-any.whl/intake_dremio/dremio_cat.py
@@ -0,0 +1,65 @@
+from intake.catalog.base import Catalog
+from intake.catalog.local import LocalCatalogEntry
+from pyarrow import flight
+
+from . import __version__
+from .intake_dremio import (
+    DremioSource, DremioClientAuthMiddlewareFactory, HttpDremioClientAuthHandler,
+    process_uri
+)
+
+
+class DremioCatalog(Catalog):
+
+    name = 'dremio_cat'
+
+    __version__ = __version__
+
+    _sql_expr = 'select * from INFORMATION_SCHEMA."TABLES"'
+
+    def __init__(self, uri, username=None, password=None, tls=False, cert=None, **kwargs):
+        self._tls = tls
+        self._certs = cert
+        self._uri = uri
+        self._protocol, self._hostname, self._user, self._password = process_uri(
+            uri, tls=tls, user=username, password=password
+        )
+        super(DremioCatalog, self).__init__(**kwargs)
+
+    def _load(self):
+        client_auth_middleware = DremioClientAuthMiddlewareFactory()
+        connection_args = {'middleware': [client_auth_middleware]}
+        if self._tls:
+            connection_args["tls_root_certs"] = self._certs
+        client = flight.FlightClient(
+            f'{self._protocol}://{self._hostname}',
+            **connection_args
+        )
+        try:
+            bearer_token = client.authenticate_basic_token(self._user, self._password)
+            headers = [bearer_token]
+        except Exception as e:
+            if self._tls:
+                raise e
+            client.authenticate(HttpDremioClientAuthHandler(self._user, self._password))
+            headers = []
+        flight_desc = flight.FlightDescriptor.for_command(self._sql_expr)
+        options = flight.FlightCallOptions(headers=headers)
+        flight_info = client.get_flight_info(flight_desc, options)
+        reader = client.do_get(flight_info.endpoints[0].ticket, options)
+        self._dataframe = reader.read_pandas()
+        for _, row in self._dataframe.iterrows():
+            self._create_entry(row)
+
+    def _create_entry(self, row):
+        name = f'{row.TABLE_SCHEMA}."{row.TABLE_NAME}"'
+        description = f'Dremio {row.TABLE_TYPE} {name} from {self._hostname}'
+        args = {
+            'uri': self._uri,
+            'sql_expr': f'select * from {name}'
+        }
+        e = LocalCatalogEntry(name, description, 'dremio', True,
+                              args, {}, {}, {}, "", getenv=False,
+                              getshell=False)
+        e._plugin = [DremioSource]
+        self._entries[name] = e

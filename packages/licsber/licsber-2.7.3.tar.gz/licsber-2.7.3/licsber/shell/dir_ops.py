f88a1366422412bebaece38a7d3f86ef0bdf68dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:02 +0000

    Add package licsber/2.7.3/licsber-2.7.3.tar.gz

---
+++ packages/licsber/licsber-2.7.3.tar.gz/licsber-2.7.3/licsber/shell/dir_ops.py
@@ -0,0 +1,148 @@
+import os
+import shutil
+import sys
+
+from licsber.utils import Meta
+from licsber.utils import cal_time
+
+
+def _check_exist(start):
+    if not start:
+        start = os.getcwd() if len(sys.argv) == 1 else os.path.join(os.getcwd(), sys.argv[1])
+
+    if not os.path.exists(start):
+        print(f'目录 {start} 不存在, 请检查.')
+        exit(-1)
+
+    print(f'目录 {start} :')
+    return start
+
+
+@cal_time(output=True)
+def rename(start=None):
+    start = _check_exist(start)
+    start = os.path.abspath(start)
+    clean_ds_store(start)
+
+    for filename in os.listdir(start):
+        filepath = os.path.join(start, filename)
+        if os.path.isfile(filepath):
+            meta = Meta(filepath)
+            suffix = os.path.splitext(filepath)[1]
+            sha1 = meta.sha1()
+            dst_name = f"{sha1}.{suffix}" if not suffix.startswith('.') else f"{sha1}{suffix}"
+            dst_path = os.path.join(start, dst_name if suffix else sha1)
+            os.rename(filepath, dst_path)
+
+
+@cal_time(output=True)
+def archive(start=None):
+    start = _check_exist(start)
+    start = os.path.abspath(start)
+
+    res = 'Key,Filename,Size,SHA1,CRC32\n'
+    for root, dirs, files in os.walk(start):
+        for file in files:
+            filepath = os.path.join(root, file)
+            filepath = os.path.relpath(filepath)
+            meta = Meta(filepath)
+            basename, size, sha1, crc32 = meta.meta()
+            res += f"{filepath},{basename},{size},{sha1},{crc32}\n"
+
+    with open(os.path.join(start, 'tree.licsber.csv'), 'w') as f:
+        f.write(res)
+
+
+def walk_files(res: list, start):
+    for i in os.listdir(start):
+        path = os.path.join(start, i)
+        if os.path.isdir(path):
+            walk_files(res, path)
+        else:
+            res.append(path)
+
+
+@cal_time(output=True)
+def flatten_dir(start=None):
+    start = _check_exist(start)
+    start = os.path.abspath(start)
+
+    all = []
+    walk_files(all, start)
+
+    start_len = len(start)
+    for i in all:
+        dst = i[start_len + 1:]
+        dst = dst.replace(os.sep, '-')
+        dst = os.path.join(start, dst)
+        os.rename(i, dst)
+        print(f"移动: {dst}")
+    for i in os.listdir(start):
+        path = os.path.join(start, i)
+        if os.path.isdir(path):
+            print(f"删除: {path}")
+            shutil.rmtree(path)
+
+
+@cal_time(output=True)
+def count_dir(start=None):
+    start = _check_exist(start)
+    nums = {
+        'file': 0,
+        'dir': 0
+    }
+
+    def _walk(p):
+        for i in os.listdir(p):
+            path = os.path.join(p, i)
+            if os.path.isdir(path):
+                nums['dir'] += 1
+                _walk(path)
+            else:
+                nums['file'] += 1
+
+    _walk(start)
+    print(f'共有{nums["dir"]}个子目录.')
+    print(f'共有{nums["file"]}个文件.')
+
+
+@cal_time(output=True)
+def empty_dir(start=None):
+    """
+    递归删除空文件夹, 如果当前目录删除完也是空的, 一起删除.
+    :param start: 开始路径.
+    :return:
+    """
+    start = _check_exist(start)
+
+    count = 0
+    all_dirs = []
+    for root, dirs, files in os.walk(start, topdown=False):
+        for i in files:
+            filepath = os.path.join(root, i)
+            meta_path = os.path.join(root, '._' + i)
+            if i == '.DS_Store':
+                os.remove(filepath)
+                print(f"删除.DS_Store: {root}")
+            elif os.path.exists(meta_path):
+                meta = open(meta_path, 'rb').read()
+                if b'This resource fork intentionally left blank' in meta:
+                    os.remove(meta_path)
+                print(f"删除元文件: {meta_path}")
+        all_dirs.append(root)
+
+    for root in all_dirs:
+        if not os.listdir(root):
+            count += 1
+            print(f"删除目录: {root}")
+            os.rmdir(root)
+
+    print(f"共删除{count}个空目录.")
+
+
+def clean_ds_store(start):
+    for root, dirs, files in os.walk(start):
+        for i in files:
+            if i == '.DS_Store':
+                print(f"删除.DS_Store: {root}")
+                os.remove(os.path.join(root, i))
---
+++ packages/licsber/licsber-2.7.3.tar.gz/licsber-2.7.3/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='licsber',
+    version='2.7.3',
+    author='Licsber',
+    author_email='licsber@gmail.com',
+    url='https://www.cnblogs.com/licsber/',
+    description=u'个人娱乐工具箱.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pymongo',
+        'requests',
+        'beautifulsoup4',
+        'pycryptodome',
+        'numpy',
+        'minio',
+        'matplotlib',
+        'opencv-python',
+        'h5py',
+        'paddlepaddle~=2.1.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'licsber=licsber:licsber',
+            'count-dir=licsber:count_dir',
+            'flatten-dir=licsber:flatten_dir',
+            'empty-dir=licsber:empty_dir',
+            'memobird=licsber:memobird',
+            'archive=licsber:archive',
+            'rename=licsber:rename',
+        ],
+    },
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: OS Independent',
+    ],
+    license='GPLv3',
+)

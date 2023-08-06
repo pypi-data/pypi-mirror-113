7a614afb48444a2258fb14aa8b29b889f6843cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:39 +0000

    Add package crocoddyl/1.8.0/crocoddyl-1.8.0-1-cp27-cp27mu-manylinux_2_24_x86_64.whl

---
+++ packages/crocoddyl/crocoddyl-1.8.0-1-cp27-cp27mu-manylinux_2_24_x86_64.whl/crocoddyl/examples/quadrotor.py
@@ -0,0 +1,89 @@
+import os
+import sys
+
+import crocoddyl
+import pinocchio
+import numpy as np
+import example_robot_data
+
+WITHDISPLAY = 'display' in sys.argv or 'CROCODDYL_DISPLAY' in os.environ
+WITHPLOT = 'plot' in sys.argv or 'CROCODDYL_PLOT' in os.environ
+
+hector = example_robot_data.load('hector')
+robot_model = hector.model
+
+target_pos = np.array([1, 0, 1])
+target_quat = pinocchio.Quaternion(1, 0, 0, 0)
+
+state = crocoddyl.StateMultibody(robot_model)
+d_cog = 0.1525
+cf = 6.6e-5
+cm = 1e-6
+u_lim = 5
+l_lim = 0.1
+tau_f = np.array([[0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], [1.0, 1.0, 1.0, 1.0], [0.0, d_cog, 0.0, -d_cog],
+                  [-d_cog, 0.0, d_cog, 0.0], [-cm / cf, cm / cf, -cm / cf, cm / cf]])
+
+actModel = crocoddyl.ActuationModelMultiCopterBase(state, tau_f)
+
+runningCostModel = crocoddyl.CostModelSum(state, actModel.nu)
+terminalCostModel = crocoddyl.CostModelSum(state, actModel.nu)
+
+# Costs
+xResidual = crocoddyl.ResidualModelState(state, state.zero(), actModel.nu)
+xActivation = crocoddyl.ActivationModelWeightedQuad(np.array([0.1] * 3 + [1000.] * 3 + [1000.] * robot_model.nv))
+uResidual = crocoddyl.ResidualModelControl(state, actModel.nu)
+xRegCost = crocoddyl.CostModelResidual(state, xActivation, xResidual)
+uRegCost = crocoddyl.CostModelResidual(state, uResidual)
+goalTrackingResidual = crocoddyl.ResidualModelFramePlacement(state, robot_model.getFrameId("base_link"),
+                                                             pinocchio.SE3(target_quat.matrix(), target_pos),
+                                                             actModel.nu)
+goalTrackingCost = crocoddyl.CostModelResidual(state, goalTrackingResidual)
+runningCostModel.addCost("xReg", xRegCost, 1e-6)
+runningCostModel.addCost("uReg", uRegCost, 1e-6)
+runningCostModel.addCost("trackPose", goalTrackingCost, 1e-2)
+terminalCostModel.addCost("goalPose", goalTrackingCost, 100)
+
+dt = 3e-2
+runningModel = crocoddyl.IntegratedActionModelEuler(
+    crocoddyl.DifferentialActionModelFreeFwdDynamics(state, actModel, runningCostModel), dt)
+terminalModel = crocoddyl.IntegratedActionModelEuler(
+    crocoddyl.DifferentialActionModelFreeFwdDynamics(state, actModel, terminalCostModel), dt)
+
+# Creating the shooting problem and the FDDP solver
+T = 33
+problem = crocoddyl.ShootingProblem(np.concatenate([hector.q0, np.zeros(state.nv)]), [runningModel] * T, terminalModel)
+fddp = crocoddyl.SolverFDDP(problem)
+
+fddp.setCallbacks([crocoddyl.CallbackLogger(), crocoddyl.CallbackVerbose()])
+
+cameraTF = [-0.03, 4.4, 2.3, -0.02, 0.56, 0.83, -0.03]
+if WITHDISPLAY and WITHPLOT:
+    display = crocoddyl.GepettoDisplay(hector, 4, 4, cameraTF)
+    fddp.setCallbacks([crocoddyl.CallbackLogger(), crocoddyl.CallbackVerbose(), crocoddyl.CallbackDisplay(display)])
+elif WITHDISPLAY:
+    display = crocoddyl.GepettoDisplay(hector, 4, 4, cameraTF)
+    fddp.setCallbacks([crocoddyl.CallbackVerbose(), crocoddyl.CallbackDisplay(display)])
+elif WITHPLOT:
+    fddp.setCallbacks([crocoddyl.CallbackLogger(), crocoddyl.CallbackVerbose()])
+else:
+    fddp.setCallbacks([crocoddyl.CallbackVerbose()])
+
+# Solving the problem with the FDDP solver
+fddp.solve()
+
+# Plotting the entire motion
+if WITHPLOT:
+    log = fddp.getCallbacks()[0]
+    crocoddyl.plotOCSolution(log.xs, log.us, figIndex=1, show=False)
+    crocoddyl.plotConvergence(log.costs, log.u_regs, log.x_regs, log.stops, log.grads, log.steps, figIndex=2)
+
+# Display the entire motion
+if WITHDISPLAY:
+    display = crocoddyl.GepettoDisplay(hector)
+    hector.viewer.gui.addXYZaxis('world/wp', [1., 0., 0., 1.], .03, 0.5)
+    hector.viewer.gui.applyConfiguration(
+        'world/wp',
+        target_pos.tolist() + [target_quat[0], target_quat[1], target_quat[2], target_quat[3]])
+
+    display.displayFromSolver(fddp)

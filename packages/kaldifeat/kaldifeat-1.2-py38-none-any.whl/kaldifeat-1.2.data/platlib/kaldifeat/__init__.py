4e1afe97c3106fe2008de6113851396f7a39ca29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:49 +0000

    Add package kaldifeat/1.2/kaldifeat-1.2-py38-none-any.whl

---
+++ packages/kaldifeat/kaldifeat-1.2-py38-none-any.whl/kaldifeat-1.2.data/platlib/kaldifeat/__init__.py
@@ -0,0 +1,11 @@
+import torch
+from _kaldifeat import (
+    FbankOptions,
+    FrameExtractionOptions,
+    MelBanksOptions,
+    MfccOptions,
+)
+
+from .fbank import Fbank
+from .mfcc import Mfcc
+__version__ = '1.2'

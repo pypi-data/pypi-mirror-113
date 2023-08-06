672b92d07b611a695a8ef9e12cbcca50b588cf81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:19 +0000

    Add package plom/0.6.5/plom-0.6.5-py3-none-any.whl

---
+++ packages/plom/plom-0.6.5-py3-none-any.whl/plom/scripts/server.py
@@ -0,0 +1,400 @@
+#!/usr/bin/env python3
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2020 Andrew Rechnitzer
+# Copyright (C) 2020-2021 Colin B. Macdonald
+# Copyright (C) 2020 Dryden Wiebe
+# Copyright (C) 2021 Morgan Arnold
+# Copyright (C) 2021 Nicholas J H Lai
+
+"""Command line tool to start Plom servers."""
+
+__copyright__ = "Copyright (C) 2020-2021 Andrew Rechnitzer, Colin B. Macdonald et al"
+__credits__ = "The Plom Project Developers"
+__license__ = "AGPL-3.0-or-later"
+
+import argparse
+import csv
+import os
+import shutil
+from pathlib import Path
+import sys
+from textwrap import fill, dedent
+
+if sys.version_info >= (3, 7):
+    import importlib.resources as resources
+else:
+    import importlib_resources as resources
+
+import plom
+from plom import __version__
+from plom import Default_Port
+from plom.server import specdir, confdir
+from plom.server import build_server_directories, check_server_directories
+from plom.server import create_server_config, create_blank_predictions
+from plom.server import parse_user_list, build_canned_users
+from plom.server import build_self_signed_SSL_keys
+
+
+server_instructions = """Overview of running the Plom server:
+
+  0. Make a new directory and change into it.
+
+  1. Run '%(prog)s init' - creates sub-directories and config files.
+
+  2. Run '%(prog)s users' - creates a template user list for you to edit.
+
+  3. Run '%(prog)s users <filename>' - parses user list for server.
+
+       3a. Optionally you can delete the plain-text passwords.
+
+  4. Add a specfile to '{specdir}': 'plom-build' can do this..
+
+  5. Now you can start the server with '%(prog)s launch'.
+""".format(
+    specdir=specdir
+)
+
+
+class PlomServerConfigurationError(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+def checkSpecAndDatabase():
+    if specdir.exists():
+        print("Directory '{}' is present.".format(specdir))
+    else:
+        print(
+            "Cannot find '{}' directory - have you run 'plom-server init' yet?".format(
+                specdir
+            )
+        )
+        exit(1)
+
+    if (specdir / "verifiedSpec.toml").exists():
+        print("Test specification present.")
+    else:
+        print(
+            "Cannot find the test specification. Have you run 'plom-build' yet?. Aborting."
+        )
+        exit(1)
+
+    if (specdir / "plom.db").exists():
+        print("Database present: using existing database.")
+    else:
+        print("Database not yet present: it will be created on first run.")
+        # TODO: or should `plom-server init` create it?")
+
+    if (specdir / "classlist.csv").exists():
+        print("Classlist present.")
+    else:
+        print("Cannot find the classlist: expect it later...")
+
+
+def doLatexChecks():
+    from plom.textools import texFragmentToPNG
+    from plom.server import pageNotSubmitted
+
+    os.makedirs("pleaseCheck", exist_ok=True)
+
+    # TODO: big ol' GETCWD here: we're trying to get rid of those
+    # check build of fragment
+    cdir = os.getcwd()
+    keepfiles = ("checkThing.png", "pns.0.0.0.png")
+    ct = os.path.join(cdir, "pleaseCheck", keepfiles[0])
+    pns = os.path.join(cdir, specdir, "pageNotSubmitted.pdf")
+    qns = os.path.join(cdir, specdir, "questionNotSubmitted.pdf")
+
+    fragment = r"\( \mathbb{Z} / \mathbb{Q} \) The cat sat on the mat and verified \LaTeX\ worked okay for plom."
+
+    if not texFragmentToPNG(fragment, ct):
+        raise PlomServerConfigurationError(
+            "Error latex'ing fragment. Please check your latex distribution."
+        )
+
+    # build template pageNotSubmitted.pdf just in case needed
+    if not pageNotSubmitted.build_not_submitted_page(pns):
+        raise PlomServerConfigurationError(
+            "Error building 'pageNotSubmitted.pdf' template page. Please check your latex distribution."
+        )
+    # build template pageNotSubmitted.pdf just in case needed
+    if not pageNotSubmitted.build_not_submitted_question(qns):
+        raise PlomServerConfigurationError(
+            "Error building 'questionNotSubmitted.pdf' template page. Please check your latex distribution."
+        )
+
+    # Try building a replacement for missing page.
+    if not pageNotSubmitted.build_test_page_substitute(0, 0, 0):
+        raise PlomServerConfigurationError(
+            "Error building replacement for missing test page."
+        )
+    # Try building a replacement for missing page.
+    if not pageNotSubmitted.build_homework_question_substitute(0, 0):
+        raise PlomServerConfigurationError(
+            "Error building replacement for missing homework question."
+        )
+
+    shutil.move(keepfiles[1], os.path.join("pleaseCheck", keepfiles[1]))
+    print(
+        fill(
+            dedent(
+                """
+                Simple latex checks done.  If you feel the need, then please
+                examine '{}' and '{}' in the directory 'pleaseCheck'.  The
+                first should be a short latex'd fragment with some mathematics
+                and text, while the second should be a mostly blank page with
+                'page not submitted' stamped across it.  It is safe delete
+                both files and the directory.
+                """.format(
+                    *keepfiles
+                )
+            )
+        )
+    )
+
+
+def initialiseServer(port):
+    print("Build required directories")
+    build_server_directories()
+    print("Building self-signed SSL key for server")
+    try:
+        build_self_signed_SSL_keys()
+    except FileExistsError as err:
+        print(f"Skipped SSL keygen - {err}")
+
+    print("Copy server networking configuration template into place.")
+    try:
+        create_server_config(port=port)
+    except FileExistsError as err:
+        print(f"Skipping server config - {err}")
+    else:
+        print(
+            "You may want to update '{}' with the correct name (or IP) and "
+            "port of your server.".format(confdir / "serverDetails.toml")
+        )
+
+    print("Build blank predictionlist for identifying.")
+    try:
+        create_blank_predictions()
+    except FileExistsError as err:
+        print(f"Skipping prediction list - {err}")
+
+    print(
+        "Do latex checks and build 'pageNotSubmitted.pdf', 'questionNotSubmitted.pdf' in case needed"
+    )
+    doLatexChecks()
+
+
+#################
+
+
+def processUsers(userFile, demo, auto, numbered):
+    """Deal with processing and/or creation of username lists.
+
+    Behaviour different depending on the args.
+
+    args:
+        userFile (str/pathlib.Path): a filename of usernames/passwords
+            for the server.
+        demo (bool): make canned demo with known usernames/passwords.
+        auto (int or None): number of autogenerate usernames and passwords.
+        numbered (bool): autogenerate usernames like "user03" and pwds.
+
+    return:
+        None
+    """
+    confdir.mkdir(exist_ok=True)
+    userlist = confdir / "userList.json"
+    # if we have been passed a userFile then process it and return
+    if userFile:
+        print("Processing user file '{}' to {}".format(userFile, userlist))
+        if userlist.exists():
+            print("WARNING - overwriting existing {} file.".format(userlist))
+        parse_user_list(userFile)
+        return
+
+    rawfile = Path("userListRaw.csv")
+    # otherwise we have to make one for the user - check if one already there.
+    if rawfile.exists():
+        raise FileExistsError(
+            "File {} already exists.  Remove and try again.".format(rawfile)
+        )
+
+    if demo:
+        print(
+            "Creating a demo user list at {}. "
+            "** DO NOT USE ON REAL SERVER **".format(rawfile)
+        )
+        cl = resources.read_binary(plom, "templateUserList.csv")
+        with open(rawfile, "wb") as fh:
+            fh.write(cl)
+        parse_user_list(rawfile)
+        return
+
+    if auto is not None:
+        print(
+            "Creating an auto-generated {0} user list at '{1}'\n"
+            "Please edit as you see fit and then rerun 'plom-server users {1}'".format(
+                "numbered" if numbered else "named",
+                rawfile,
+            )
+        )
+        # grab required users and regular users
+        lst = build_canned_users(auto, numbered)
+        with open(rawfile, "w+") as fh:
+            writer = csv.writer(fh, quoting=csv.QUOTE_NONNUMERIC)
+            writer.writerow(["user", "password"])
+            for row in lst:
+                writer.writerow(row)
+        return
+
+    if not userFile:
+        print(
+            "Creating '{}' - please edit passwords for 'manager', 'scanner', 'reviewer', and then add one or more normal users and their passwords. Note that passwords must be at least 4 characters.".format(
+                rawfile
+            )
+        )
+        cl = resources.read_binary(plom, "templateUserList.csv")
+        with open(rawfile, "wb") as fh:
+            fh.write(cl)
+
+
+def checkServerConfigured():
+    if not (confdir / "serverDetails.toml").exists():
+        print("Server configuration file not present. Have you run 'plom-server init'?")
+        exit(1)
+
+    if not (confdir / "userList.json").exists():
+        print("Processed userlist is not present. Have you run 'plom-server users'?")
+        exit(1)
+
+    if not (
+        (confdir / "plom.key").exists() and (confdir / "plom-selfsigned.crt").exists()
+    ):
+        print("SSL keys not present. Have you run 'plom-server init'?")
+        exit(1)
+
+    if (specdir / "predictionlist.csv").exists():
+        print("Predictionlist present.")
+    else:
+        print(
+            "Cannot find the predictionlist. Have you run 'plom-server init' yet? Aborting."
+        )
+        exit(1)
+
+
+def launchTheServer(masterToken):
+    from plom.server import theServer
+
+    check_server_directories()
+    # check database, spec and classlist in place
+    checkSpecAndDatabase()
+    # check serverConf and userlist present (also check predictionlist).
+    checkServerConfigured()
+
+    theServer.launch(masterToken)
+
+
+def check_positive(arg):
+    if int(arg) < 0:
+        raise ValueError
+    return int(arg)
+
+
+#################
+
+parser = argparse.ArgumentParser(
+    epilog="Use '%(prog)s <subcommand> -h' for detailed help.\n\n"
+    + server_instructions,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+)
+parser.add_argument("--version", action="version", version="%(prog)s " + __version__)
+sub = parser.add_subparsers(
+    dest="command", description="Perform various server-related tasks."
+)
+
+spI = sub.add_parser(
+    "init",
+    help="Initialise server",
+    description="""
+      Initializes the current working directory in preparation for
+      starting a Plom server.  Creates sub-directories and config files.
+    """,
+)
+spI.add_argument(
+    "--port",
+    type=int,
+    help=f"Use alternative port (defaults to {Default_Port} if omitted)",
+)
+
+spU = sub.add_parser(
+    "users",
+    help="Create user accounts",
+    description="""
+      Manipulate users accounts.  With no arguments, produce a template
+      file for you to edit, with passwords displayed in plain text.
+      Given a filename, parses a plain-text user list, performs some
+      simple sanity checks and then hashes the passwords a file for the
+      server.
+    """,
+)
+spR = sub.add_parser(
+    "launch", help="Start the server", description="Start the Plom server."
+)
+spR.add_argument(
+    "masterToken",
+    nargs="?",
+    help="""A 32 hex-digit string used to encrypt tokens in the database.
+        If you do not supply one then the server will create one.
+        If you record the token somewhere you can hot-restart the server
+        (i.e., restart the server without requiring users to log-off and
+        log-in again).""",
+)
+
+spU.add_argument(
+    "userlist",
+    nargs="?",
+    help="Process the given userlist file OR if none given then produce a template.",
+)
+
+grp = spU.add_mutually_exclusive_group()
+grp.add_argument(
+    "--demo",
+    action="store_true",
+    help="""
+        Use fixed prepopulated demo userlist and passwords.
+        **DO NOT USE THIS ON REAL SERVER**
+    """,
+)
+grp.add_argument(
+    "--auto",
+    type=int,
+    metavar="N",
+    help="Auto-generate a random user list of N users with real-ish usernames.",
+)
+
+spU.add_argument(
+    "--numbered",
+    action="store_true",
+    help='Use numbered usernames, e.g. "user17", for the autogeneration.',
+)
+
+
+def main():
+    args = parser.parse_args()
+
+    if args.command == "init":
+        initialiseServer(args.port)
+    elif args.command == "users":
+        processUsers(args.userlist, args.demo, args.auto, args.numbered)
+    elif args.command == "launch":
+        launchTheServer(args.masterToken)
+    else:
+        parser.print_help()
+
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/plom/plom-0.6.5-py3-none-any.whl/plom/server/theServer.py
@@ -0,0 +1,274 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2018-2021 Andrew Rechnitzer
+# Copyright (C) 2019-2021 Colin B. Macdonald
+# Copyright (C) 2020 Dryden Wiebe
+# Copyright (C) 2020 Vala Vakilian
+
+from datetime import datetime
+import json
+import logging
+from pathlib import Path
+import ssl
+import subprocess
+import tempfile
+
+import toml
+from aiohttp import web
+
+from plom import __version__
+from plom import Plom_API_Version as serverAPI
+from plom import Default_Port
+from plom import SpecVerifier
+from plom import specdir
+from plom.db import PlomDB
+from plom.server import confdir, check_server_directories
+
+from .authenticate import Authority
+
+
+from .plomServer.routesUserInit import UserInitHandler
+from .plomServer.routesUpload import UploadHandler
+from .plomServer.routesID import IDHandler
+from .plomServer.routesMark import MarkHandler
+from .plomServer.routesRubric import RubricHandler
+from .plomServer.routesReport import ReportHandler
+
+
+# TODO: move this to the class?  otherwise, importing this file does logging?
+# TODO: shortname in this?
+logfile = datetime.now().strftime("plomserver-%Y%m%d_%H-%M-%S.log")
+# 5 is to keep debug/info lined up
+logging.basicConfig(
+    format="%(asctime)s %(levelname)5s:%(name)s\t%(message)s",
+    datefmt="%b%d %H:%M:%S",
+    handlers=[
+        logging.FileHandler(logfile),
+        logging.StreamHandler(),
+    ],
+)
+log = logging.getLogger("server")
+# We will reset this later after we read the config
+logging.getLogger().setLevel("Debug".upper())
+
+
+class Server:
+    def __init__(self, db, masterToken):
+        log.debug("Initialising server")
+        try:
+            self.testSpec = SpecVerifier.load_verified()
+            log.info("existing spec loaded")
+        except FileNotFoundError:
+            self.testSpec = None
+            log.error("spec file not found -- use 'plom-build' to create one")
+            raise
+        self.authority = Authority(masterToken)
+        self.DB = db
+        self.API = serverAPI
+        self.Version = __version__
+        print(
+            "Server launching with masterToken = '{}' {}".format(
+                self.authority.get_master_token(),
+                type(self.authority.get_master_token()),
+            )
+        )
+        self.tempDirectory = tempfile.TemporaryDirectory()
+        # Give directory correct permissions.
+        subprocess.check_call(["chmod", "o-r", self.tempDirectory.name])
+        self.load_users()
+
+    def load_users(self):
+        """Load the users from json file, add them to the database and checks pwd hashes.
+
+        It does simple sanity checks of pwd hashes to see if they have changed.
+        """
+
+        if (confdir / "userList.json").exists():
+            with open(confdir / "userList.json") as data_file:
+                # load list of users + pwd hashes
+                userList = json.load(data_file)
+                # for each name check if in DB by asking for the hash of its pwd
+                for uname in userList.keys():
+                    passwordHash = self.DB.getUserPasswordHash(uname)
+                    if passwordHash is None:  # not in list
+                        self.DB.createUser(uname, userList[uname])
+                    else:
+                        if passwordHash != userList[uname]:
+                            log.warning("User {} password has changed.".format(uname))
+                        self.DB.setUserPasswordHash(userList[uname], passwordHash)
+            log.debug("Loading users")
+        else:
+            # Cannot find users - give error and quit out.
+            log.error("Cannot find user/password file - aborting.")
+            quit()
+
+    from .plomServer.serverUserInit import (
+        validate,
+        checkPassword,
+        checkUserEnabled,
+        createModifyUser,
+        InfoShortName,
+        info_spec,
+        reloadUsers,
+        giveUserToken,
+        setUserEnable,
+        closeUser,
+    )
+    from .plomServer.serverUpload import (
+        doesBundleExist,
+        createNewBundle,
+        sidToTest,
+        addTestPage,
+        addHWPage,
+        addLPage,
+        processHWUploads,
+        processTUploads,
+        processLUploads,
+        addUnknownPage,
+        addCollidingPage,
+        replaceMissingTestPage,
+        replaceMissingHWQuestion,
+        removeAllScannedPages,
+        getUnknownPageNames,
+        getDiscardNames,
+        getCollidingPageNames,
+        getUnknownImage,
+        getCollidingImage,
+        getDiscardImage,
+        getTPageImage,
+        getHWPageImage,
+        getEXPageImage,
+        getLPageImage,
+        getQuestionImages,
+        getAllTestImages,
+        checkTPage,
+        removeUnknownImage,
+        removeCollidingImage,
+        unknownToTestPage,
+        unknownToHWPage,
+        unknownToExtraPage,
+        collidingToTestPage,
+        discardToUnknown,
+    )
+    from .plomServer.serverID import (
+        IDprogressCount,
+        IDgetNextTask,
+        IDgetDoneTasks,
+        IDgetImage,
+        IDgetImageFromATest,
+        IDclaimThisTask,
+        IDdidNotFinish,
+        id_paper,
+        ID_id_paper,
+        IDdeletePredictions,
+        IDrunPredictions,
+        IDreviewID,
+    )
+    from .plomServer.serverMark import (
+        MgetAllMax,
+        MprogressCount,
+        MgetQuestionMax,
+        MgetDoneTasks,
+        MgetNextTask,
+        MlatexFragment,
+        MclaimThisTask,
+        MdidNotFinish,
+        MrecordMark,
+        MreturnMarkedTask,
+        MgetImages,
+        MgetOriginalImages,
+        MsetTag,
+        MgetWholePaper,
+        MreviewQuestion,
+        MrevertTask,
+    )
+    from .plomServer.serverRubric import (
+        McreateRubric,
+        MgetRubrics,
+        MmodifyRubric,
+        MgetUserRubricPanes,
+        MsaveUserRubricPanes,
+    )
+    from .plomServer.serverReport import (
+        RgetUnusedTests,
+        RgetScannedTests,
+        RgetIncompleteTests,
+        RgetCompleteHW,
+        RgetMissingHWQ,
+        RgetProgress,
+        RgetQuestionUserProgress,
+        RgetMarkHistogram,
+        RgetMarked,
+        RgetIdentified,
+        RgetCompletionStatus,
+        RgetOutToDo,
+        RgetStatus,
+        RgetSpreadsheet,
+        RgetCoverPageInfo,
+        RgetOriginalFiles,
+        RgetAnnotatedFiles,
+        RgetMarkReview,
+        RgetIDReview,
+        RgetAnnotatedImage,
+        RgetUserList,
+        RgetUserDetails,
+    )
+
+
+def get_server_info():
+    """Read the server info from config file."""
+
+    serverInfo = {"server": "127.0.0.1", "port": Default_Port}
+    try:
+        with open(confdir / "serverDetails.toml") as data_file:
+            serverInfo = toml.load(data_file)
+            logging.getLogger().setLevel(serverInfo["LogLevel"].upper())
+            log.debug("Server details loaded: {}".format(serverInfo))
+    except FileNotFoundError:
+        log.warning("Cannot find server details, using defaults")
+    # Special treatment for chatty modules
+    # TODO: nicer way to do this?
+    if serverInfo["LogLevel"].upper() == "INFO":
+        logging.getLogger("aiohttp.access").setLevel("WARNING")
+    return serverInfo
+
+
+def launch(masterToken=None):
+    """Launches the Plom server.
+
+    args:
+        masterToken (str): a 32 hex-digit string used to encrypt tokens
+            in the database.  Not needed on server unless you want to
+            hot-restart the server without requiring users to log-off
+            and log-in again.  If None, a new token is created.
+    """
+    log.info("Plom Server {} (communicates with api {})".format(__version__, serverAPI))
+    check_server_directories()
+    server_info = get_server_info()
+    examDB = PlomDB(Path(specdir) / "plom.db")
+    peon = Server(examDB, masterToken)
+    userIniter = UserInitHandler(peon)
+    uploader = UploadHandler(peon)
+    ider = IDHandler(peon)
+    marker = MarkHandler(peon)
+    rubricker = RubricHandler(peon)
+    reporter = ReportHandler(peon)
+
+    # construct the web server
+    app = web.Application()
+    log.info("Setting up routes")
+    userIniter.setUpRoutes(app.router)
+    uploader.setUpRoutes(app.router)
+    ider.setUpRoutes(app.router)
+    marker.setUpRoutes(app.router)
+    rubricker.setUpRoutes(app.router)
+    reporter.setUpRoutes(app.router)
+    log.info("Loading ssl context")
+    sslContext = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+    sslContext.check_hostname = False
+    sslContext.load_cert_chain(confdir / "plom-selfsigned.crt", confdir / "plom.key")
+    log.info("Start the server!")
+    try:
+        web.run_app(app, ssl_context=sslContext, port=server_info["port"])
+    except KeyboardInterrupt:
+        # Above seems to have its own Ctrl-C handler so this never happens?
+        log.info("Closing down via keyboard interrupt")

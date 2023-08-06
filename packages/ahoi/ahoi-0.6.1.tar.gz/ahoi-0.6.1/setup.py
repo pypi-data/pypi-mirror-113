3acf052fe3fc2e4e0ec918dab36fc8dab65c092e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:31 +0000

    Add package ahoi/0.6.1/ahoi-0.6.1.tar.gz

---
+++ packages/ahoi/ahoi-0.6.1.tar.gz/ahoi-0.6.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages, Extension
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="ahoi",
+    version="0.6.1",
+    packages=["ahoi"],
+    install_requires=["numpy", "tqdm"],
+    python_requires=">2.7",
+    ext_modules=[
+        Extension(
+            "ahoi.ahoi_scan",
+            sources=["src/ahoi_scan.c"],
+            extra_compile_args=["-std=c99"],
+        )
+    ],
+    author="Nikolai Hartmann",
+    author_email="nikoladze@posteo.de",
+    description="Brute-force scan for rectangular cuts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/nikoladze/ahoi",
+)

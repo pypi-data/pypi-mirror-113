ffea7df6bef8432d61b5df5aad98c9d24a2fd3c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:52 +0000

    Add package figrecipes/0.0.5/figrecipes-0.0.5.tar.gz

---
+++ packages/figrecipes/figrecipes-0.0.5.tar.gz/figrecipes-0.0.5/README.md
@@ -0,0 +1,14 @@
+# FigRecipes
+
+![overview](./docs_rst/_static/overview.png)
+
+A repo to help with interactive plotting in data mining applications. 
+
+Originally packaged with the [matminer code](https://github.com/hackingmaterials/matminer).
+
+- [Source](https://github.com/hackingmaterials/figrecipes)
+- [Docs](https://hackingmaterials.github.io/figrecipes)
+- [Matminer code](https://github.com/hackingmaterials/matminer)
+
+
+Find examples on the [matminer examples repo](https://github.com/hackingmaterials/matminer_examples). 
---
+++ packages/figrecipes/figrecipes-0.0.5.tar.gz/figrecipes-0.0.5/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+module_dir = os.path.dirname(os.path.abspath(__file__))
+
+reqs_file = os.path.join(module_dir, "requirements.txt")
+with open(reqs_file, "r") as f:
+    reqs_raw = f.read()
+reqs_list = [r.replace("==", ">=") for r in reqs_raw.split("\n")]
+
+if __name__ == "__main__":
+    setup(
+        name='figrecipes',
+        version='0.0.5',
+        description='figrecipes is a tool for quickly creating interactive plots for data science.',
+        long_description=open(os.path.join(module_dir, 'README.md')).read(),
+        url='https://github.com/hackingmaterials/figrecipes',
+        author='Anubhav Jain',
+        author_email='anubhavster@gmail.com',
+        license='modified BSD',
+        packages=find_packages(),
+        package_data={},
+        zip_safe=False,
+        install_requires=reqs_list,
+        extras_require={},
+        classifiers=['Programming Language :: Python :: 2.7',
+                     'Development Status :: 4 - Beta',
+                     'Intended Audience :: Science/Research',
+                     'Intended Audience :: System Administrators',
+                     'Intended Audience :: Information Technology',
+                     'Operating System :: OS Independent',
+                     'Topic :: Other/Nonlisted Topic',
+                     'Topic :: Scientific/Engineering'],
+        test_suite='figrecipes',
+        scripts=[]
+    )

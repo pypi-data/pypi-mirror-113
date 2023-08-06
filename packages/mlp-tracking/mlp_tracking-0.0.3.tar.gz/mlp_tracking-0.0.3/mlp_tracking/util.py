63b4ce7f00451959e883b6701549c61d7c9ea69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:32 +0000

    Add package mlp-tracking/0.0.3/mlp_tracking-0.0.3.tar.gz

---
+++ packages/mlp-tracking/mlp_tracking-0.0.3.tar.gz/mlp_tracking-0.0.3/mlp_tracking/util.py
@@ -0,0 +1,5 @@
+import time
+
+
+def get_current_time():
+    return int(round(time.time() * 1000))
---
+++ packages/mlp-tracking/mlp_tracking-0.0.3.tar.gz/mlp_tracking-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mlp_tracking",
+    version="0.0.3",
+    author="hazoth, LowinLi",
+    author_email="hazoth@hotmail.com, idealway9@gmail.com",
+    description="mlp_tracking",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["confluent_kafka==1.7.0", "ml_platform_client>=0.3.18.5"],
+    keywords="kafka issue tracking",
+)

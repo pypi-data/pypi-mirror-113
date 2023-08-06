778d360a5298b4614b3b866cddecb036ed75e77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:23 +0000

    Add package a_3y/2.6.4.1/a_3y-2.6.4.1.tar.gz

---
+++ packages/a_3y/a_3y-2.6.4.1.tar.gz/a_3y-2.6.4.1/py/__init__.py
@@ -0,0 +1,9 @@
+# a_3y
+# __init__.py
+from colorama import init
+from datetime import datetime as sj
+from time import sleep as dd
+from math import pi as pai1
+from sys import exit as tc
+from pdsystem import * #The moudle of Maker:神秘的·  ps:需要pip下载,在终端输入pip install pdsystem
+start=sj.now()

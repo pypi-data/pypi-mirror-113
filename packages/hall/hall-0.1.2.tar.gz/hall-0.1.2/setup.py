f96e846cfcfaf2cf83c4090eba56d0a31416ddf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:20 +0000

    Add package hall/0.1.2/hall-0.1.2.tar.gz

---
+++ packages/hall/hall-0.1.2.tar.gz/hall-0.1.2/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hall']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['mpmath>=1.2,<2.0']
+
+entry_points = \
+{'console_scripts': ['check = scripts.poetry:check',
+                     'docs = scripts.poetry:docs',
+                     'reformat = scripts.poetry:reformat']}
+
+setup_kwargs = {
+    'name': 'hall',
+    'version': '0.1.2',
+    'description': 'Hall - Probability theory using pythonic and (almost) mathematical notation.',
+    'long_description': '# Hall\n\nProbability theory using pythonic and (almost) mathematical notation.\n\n## A simple example: Monty ~~Python~~ Hall \n\n`TODO`\n\n## Contributing\n\nEnsure you have [poetry](https://python-poetry.org/docs/#installation) installed, then\n\n```bash\npoetry install\n```\n',
+    'author': 'Joren Hammudoglu',
+    'author_email': 'jhammudoglu@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

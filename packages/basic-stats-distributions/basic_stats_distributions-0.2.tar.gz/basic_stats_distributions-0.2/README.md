e97e9d2b3583d41e3031434b68d1390cfee15962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:21 +0000

    Add package basic-stats-distributions/0.2/basic_stats_distributions-0.2.tar.gz

---
+++ packages/basic-stats-distributions/basic_stats_distributions-0.2.tar.gz/basic_stats_distributions-0.2/README.md
@@ -0,0 +1,15 @@
+# basic-stats-distributions package
+
+Summary of the package
+
+# Files
+
+Explanation of files in the package
+
+# Installation
+
+How to install the package
+
+# Example Usecase
+
+How to use the package
---
+++ packages/basic-stats-distributions/basic_stats_distributions-0.2.tar.gz/basic_stats_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='basic_stats_distributions',
+      version='0.2',
+      description='Basic statistical distributions',
+      packages=['basic_stats_distributions'],
+      author='Dhyanil Mehta',
+      author_email='dhyanilm2399@gmail.com',
+      zip_safe=False)

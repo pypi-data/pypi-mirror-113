5c83410887c839f80090ec5c455a3a5b546df626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:38 +0000

    Add package hoshi/0.4.4/hoshi-0.4.4-py3-none-any.whl

---
+++ packages/hoshi/hoshi-0.4.4-py3-none-any.whl/hoshi/sets.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+
+The Language Sets Module (:mod:`hoshi.sets`)
+============================================
+
+This module provides a few convenient lists of locales which might
+generally apply in geopolitical regions. If you wish to add to these sets
+or create new ones, please feel free to create an issue or a pull request
+on github.
+
+"""
+
+#: A list of major Indian languages. Known to be incomplete.
+indian_languages = [
+    'en_IN',  # English
+    'hi_IN',  # Hindi
+    'te_IN',  # Telugu
+    'ta_IN',  # Tamil
+    'bn_IN',  # Bengali
+    'pa_IN',  # Punjabi
+    'ur_IN',  # Urdu
+    'kn_IN',  # Kannada
+    'or_IN',  # Oriya
+    'gu_IN',  # Gujarati
+    'ml_IN',  # Malayalam
+]

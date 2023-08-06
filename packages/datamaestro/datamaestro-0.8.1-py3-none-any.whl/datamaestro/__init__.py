af0419c78d3d6ce9d8b6a9e78dcddf0a49962732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:58 +0000

    Add package datamaestro/0.8.1/datamaestro-0.8.1-py3-none-any.whl

---
+++ packages/datamaestro/datamaestro-0.8.1-py3-none-any.whl/datamaestro/__init__.py
@@ -0,0 +1,8 @@
+from .context import Context, Repository, get_dataset, prepare_dataset
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    __version__ = None

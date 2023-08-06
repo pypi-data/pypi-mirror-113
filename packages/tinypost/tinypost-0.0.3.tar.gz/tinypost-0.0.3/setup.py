7c284a6dd2f8db2453ee0f721e753e0d3cf97550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:06 +0000

    Add package tinypost/0.0.3/tinypost-0.0.3.tar.gz

---
+++ packages/tinypost/tinypost-0.0.3.tar.gz/tinypost-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os import path
+
+long_description = ""
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+VERSION = "0.0.3"
+DESCRIPTION = "An infix-to-postfix expression parser for evaluating math expressions."
+
+setup(
+    name="tinypost",
+    version=VERSION,
+    author="Nathan Abraham",
+    author_email="abnathan123@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    keywords=["python", "math", "shunting-yard",
+              "parser", "expression evaluation"],
+    classifiers=["Operating System :: Unix",
+                 "Operating System :: MacOS :: MacOS X",
+                 "Operating System :: Microsoft :: Windows",
+                 ]
+)
---
+++ packages/tinypost/tinypost-0.0.3.tar.gz/tinypost-0.0.3/tinypost/_functions.py
@@ -0,0 +1,9 @@
+import math
+
+
+def _log(x: float):
+    return math.log(x)
+
+
+def _max_two(a: float, b: float):
+    return max(a, b)

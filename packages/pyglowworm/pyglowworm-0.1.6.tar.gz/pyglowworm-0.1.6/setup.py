69851e3af9c68939de0881cb5f47ef6c9af85cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:18 +0000

    Add package pyglowworm/0.1.6/pyglowworm-0.1.6.tar.gz

---
+++ packages/pyglowworm/pyglowworm-0.1.6.tar.gz/pyglowworm-0.1.6/setup.py
@@ -0,0 +1,31 @@
+# encoding: utf-8
+"""
+@file: setup.py
+@desc:
+@author: zhenguo
+@time: 2021/7/19
+"""
+
+# !/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="pyglowworm",
+    version="0.1.6",
+    author="zhenguo",
+    author_email="guozhennianhua@163.com",
+    description="to assist finding examples for api of all kinds of libraries",
+    long_description_content_type='text/markdown',
+    long_description=open("README.md").read(),
+    license="MIT",
+    url="https://github.com/jackzhenguo/python-small-examples",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

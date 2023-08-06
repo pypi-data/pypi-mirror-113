c985e32bed3441ec5f709c324b1b8345cb5e110b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:02 +0000

    Add package cc-py-commons/0.2.27/cc_py_commons-0.2.27.tar.gz

---
+++ packages/cc-py-commons/cc_py_commons-0.2.27.tar.gz/cc_py_commons-0.2.27/cc_py_commons/sns/booking_assistant_flow_notification.py
@@ -0,0 +1,26 @@
+import json
+
+from cc_py_commons.sns.sns_service import SnsService
+from cc_py_commons.utils.logger import logger
+
+class BookingAssistantFlowNotification:
+
+  def __init__(self, app_config):
+    self._app_config = app_config
+
+  def send(self, user_id, load, params):
+    message = {
+      'userId': user_id,
+      'loadDTO': load,
+      'payload': params,
+      'subject': f'{self._app_config.BOOKING_ASSISTANT_SNS_SUBJECT}',
+      'className': f'{self._app_config.BOOKING_ASSISTANT_SNS_CLASS_NAME}'
+    }
+    logger.debug(f"sending messsage {message} to {self._app_config.BOOKING_ASSISTANT_SNS_TOPIC_ARN}")
+
+    try:
+      sns_service = SnsService()
+      sns_service.send(self._app_config.BOOKING_ASSISTANT_SNS_TOPIC_ARN,
+        self._app_config.BOOKING_ASSISTANT_SNS_SUBJECT, json.dumps(message))
+    except Exception as e:
+      logger.exception(f"failed to send message to {self._app_config.BOOKING_ASSISTANT_SNS_TOPIC_ARN}: {message} - {e}", e)

6a2e4ee731836a9896db24bc170aef246ee2fa95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:08 +0000

    Add package aiidalab-eln/0.1.0a1/aiidalab_eln-0.1.0a1-py3-none-any.whl

---
+++ packages/aiidalab-eln/aiidalab_eln-0.1.0a1-py3-none-any.whl/aiidalab_eln/base_connector.py
@@ -0,0 +1,42 @@
+"""Module that defines the base class for ELN connetors."""
+import ipywidgets as ipw
+import traitlets
+
+
+class ElnConnector(ipw.VBox):
+    """Base class for the ELN connectors."""
+
+    eln_instance = traitlets.Unicode()
+    eln_type = traitlets.Unicode()
+
+    def __init__(self, **kwargs):
+        """Connect to an ELN
+
+        Args:
+            eln_instance (str): URL which points to the eln instance.
+            user (str): ELN user.
+            token (str): ELN access token (or user password).
+            eln_type (str): ELN type, e.g. "cheminfo".
+        """
+        super().__init__(**kwargs)
+
+    def connect(self):
+        raise NotImplementedError(
+            f"{self.__class__.__name__} does not implement the 'connect' method"
+        )
+
+    @property
+    def is_connected(self):
+        raise NotImplementedError(
+            f"{self.__class__.__name__} does not implement the 'is_connected' method"
+        )
+
+    def export_data_object(self, data_object):
+        raise NotImplementedError(
+            f"{self.__class__.__name__} does not implement the 'send_data_object' method"
+        )
+
+    def get_config(self):
+        raise NotImplementedError(
+            f"{self.__class__.__name__} does not implement the 'get_config' method"
+        )
---
+++ packages/aiidalab-eln/aiidalab_eln-0.1.0a1-py3-none-any.whl/aiidalab_eln/cheminfo/exporter.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""Export AiiDA objects to the Cheminfo ELN."""
+
+from pytojcamp import from_dict
+
+
+def export_isotherm(
+    sample_manager,
+    isotherm,
+    adsorptive: str,
+    filename: str = None,
+    aiidalab_instance: str = "https://aiidalab-demo.materialscloud.org",
+):
+    """Export Isotherm object."""
+    source_info = {
+        "uuid": isotherm.uuid,
+        "url": aiidalab_instance,
+        "name": "Isotherm simulated using the isotherm app on AiiDAlab",
+    }
+    meta = {
+        "adsorptive": adsorptive,
+        "temperature": isotherm["temperature"],
+        "method": "GCMC",
+    }
+    jcamp = from_dict(
+        {
+            "x": {
+                "data": isotherm["isotherm"]["pressure"],
+                "unit": isotherm["isotherm"]["pressure_unit"],
+                "type": "INDEPENDENT",
+            },
+            "y": {
+                "data": isotherm["isotherm"]["loading_absolute_average"],
+                "unit": isotherm["isotherm"]["loading_absolute_unit"],
+                "type": "DEPENDENT",
+            },
+        },
+        data_type="Adsorption Isotherm",
+        meta=meta,
+    )
+    name = f"{isotherm.uuid}.jcamp" if filename is None else f"{filename}.jcamp"
+    sample_manager.put_spectrum(
+        spectrum_type="isotherm",
+        name=name,
+        filecontent=jcamp,
+        metadata=meta,
+        source_info=source_info,
+    )
+
+
+def export_cif(
+    sample_manager,
+    cifdata,
+    filename: str = None,
+    aiidalab_instance: str = "https://aiidalab-demo.materialscloud.org",
+):
+    """Export CIF object."""
+
+    source_info = {
+        "uuid": cifdata.uuid,
+        "url": aiidalab_instance,
+        "name": "Structure optimized on AiiDAlab",
+    }
+
+    sample_manager.put_spectrum(
+        spectrum_type="xray",
+        name=f"{cifdata.uuid}.cif" if filename is None else f"{filename}.cif",
+        filecontent=cifdata._prepare_cif(),  # pylint: disable=protected-access
+        source_info=source_info,
+    )

bc7f684fa3bca4b35b37da512588411d805d2ff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:11 +0000

    Add package unittest-parallel/1.4.3/unittest-parallel-1.4.3.tar.gz

---
+++ packages/unittest-parallel/unittest-parallel-1.4.3.tar.gz/unittest-parallel-1.4.3/setup.py
@@ -0,0 +1,54 @@
+# Licensed under the MIT License
+# https://github.com/craigahobbs/unittest-parallel/blob/main/LICENSE
+
+import os
+
+from setuptools import setup
+
+MODULE_NAME = 'unittest_parallel'
+PACKAGE_NAME = 'unittest-parallel'
+
+def main():
+    # Read the readme for use as the long description
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as readme_file:
+        long_description = readme_file.read()
+
+    # Do the setup
+    setup(
+        name=PACKAGE_NAME,
+        description='Parallel unit test runner with coverage support',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        version='1.4.3',
+        author='Craig Hobbs',
+        author_email='craigahobbs@gmail.com',
+        keywords='test unittest coverage parallel',
+        url='https://github.com/craigahobbs/' + PACKAGE_NAME,
+        license='MIT',
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Topic :: Software Development :: Testing',
+            'Topic :: Utilities'
+        ],
+        package_dir={'': 'src'},
+        packages=[MODULE_NAME],
+        install_requires=[
+            'coverage >= 5.1'
+        ],
+        entry_points={
+            'console_scripts': [
+                PACKAGE_NAME + ' = ' + MODULE_NAME + '.main:main'
+            ]
+        }
+    )
+
+if __name__ == '__main__':
+    main()

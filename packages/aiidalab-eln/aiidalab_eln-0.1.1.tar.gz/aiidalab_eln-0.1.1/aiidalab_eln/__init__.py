1350f762f11a211b4db8839e4a0ac00a570e4b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:44 +0000

    Add package aiidalab-eln/0.1.1/aiidalab_eln-0.1.1.tar.gz

---
+++ packages/aiidalab-eln/aiidalab_eln-0.1.1.tar.gz/aiidalab_eln-0.1.1/aiidalab_eln/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Provide an ELN connector."""
+from .cheminfo import CheminfoElnConnector
+
+
+def get_eln_connector(eln_type: str = "cheminfo"):
+    """Provide ELN connector of a selected type."""
+    if eln_type == "cheminfo":
+        return CheminfoElnConnector
+    raise Exception(f"The selected ELN connector type ({eln_type}) is not known.")
+
+
+__version__ = "0.1.1"

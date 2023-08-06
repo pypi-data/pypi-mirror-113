a401046e3d5d598f2ddfbe70a0b4954096f226ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:35 +0000

    Add package dsnd-binomial-probability/0.1/dsnd_binomial_probability-0.1.tar.gz

---
+++ packages/dsnd-binomial-probability/dsnd_binomial_probability-0.1.tar.gz/dsnd_binomial_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_binomial_probability',
+      version='0.1',
+      description='Binomial distributions',
+      packages=['dsnd_binomial_probability'],
+      zip_safe=False)

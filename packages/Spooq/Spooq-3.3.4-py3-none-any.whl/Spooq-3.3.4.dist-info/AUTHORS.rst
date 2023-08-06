74ae46527249388507a0a1f199f10a85d499c824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:13 +0000

    Add package Spooq/3.3.4/Spooq-3.3.4-py3-none-any.whl

---
+++ packages/Spooq/Spooq-3.3.4-py3-none-any.whl/Spooq-3.3.4.dist-info/AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* David Hohensinn <breaka@gmx.at>, <david.hohensinn@runtastic.com>
+
+Contributors
+------------
+
+* Philip Butting <phb@runtastic.com>
---
+++ packages/Spooq/Spooq-3.3.4-py3-none-any.whl/spooq/transformer/__init__.py
@@ -0,0 +1,15 @@
+from .newest_by_group import NewestByGroup
+from .mapper import Mapper
+from .exploder import Exploder
+from .threshold_cleaner import ThresholdCleaner
+from .enum_cleaner import EnumCleaner
+from .sieve import Sieve
+
+__all__ = [
+    "NewestByGroup",
+    "Mapper",
+    "Exploder",
+    "ThresholdCleaner",
+    "EnumCleaner",
+    "Sieve",
+]

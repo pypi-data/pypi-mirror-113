8f061eeceaeb86f9bc477fefe998da7bf427ed5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:49 +0000

    Add package f1z1-async-http/0.1.7/f1z1_async_http-0.1.7.tar.gz

---
+++ packages/f1z1-async-http/f1z1_async_http-0.1.7.tar.gz/f1z1_async_http-0.1.7/f1z1_async_http/src/components/notifier.py
@@ -0,0 +1,53 @@
+# @Time     : 2021/7/16
+# @Project  : f1z1-g
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+from abc import ABCMeta, abstractmethod
+from typing import TypeVar
+
+from httpx import Request, Response
+
+from .interceptors import IInterceptorManager
+from .message import IRequestMessage
+
+M = TypeVar("M")
+
+
+class AbstractAsyncNotifier(metaclass=ABCMeta):
+    """
+    异步消息通知器
+    """
+
+    def __init__(self, interceptors: IInterceptorManager, message: IRequestMessage):
+        self._interceptors = interceptors
+        self._rm = message
+
+    @abstractmethod
+    async def notify(self, message: M) -> bool:
+        raise NotImplementedError("")
+
+
+class AsyncRequestNotifier(AbstractAsyncNotifier):
+
+    async def notify(self, message: Request) -> bool:
+        self._rm.add(message)
+        print("add", self._rm)
+        for fn in self._interceptors:
+            await fn(message)
+
+
+class AsyncResponseNotifier(AbstractAsyncNotifier):
+
+    async def notify(self, message: Response) -> bool:
+        for fn in self._interceptors:
+            await fn(message)
+        # print("pop before", self._rm)
+        self._rm.pop()
+
+
+def notify(notifier: AbstractAsyncNotifier):
+    async def execute(value):
+        return await notifier.notify(value)
+
+    return execute

caa40307b84e301bab428a58ca9193f476ca180a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:35 +0000

    Add package fixedwidth-transformer/0.0.1.post1626689689/fixedwidth_transformer-0.0.1.post1626689689-py3-none-any.whl

---
+++ packages/fixedwidth-transformer/fixedwidth_transformer-0.0.1.post1626689689-py3-none-any.whl/library/exceptions.py
@@ -0,0 +1,52 @@
+
+class ConfigError(Exception):
+    pass
+
+
+class MissingConfigError(Exception):
+    """
+    Exception that's thrown when configuration is missing or the required segment is not found.
+    """
+    pass
+
+
+class InvalidConfigError(Exception):
+    """
+    Exception that's thrown when a loaded yaml configuration cannot be parsed or read
+    """
+    def __init__(self, msg="Failed to parse data. Please ensure config format is valid"):
+        super().__init__(msg)
+
+
+class ProcessingError(Exception):
+    pass
+
+
+class FileError(Exception):
+    pass
+
+
+class AppenderError(Exception):
+    pass
+
+
+class ValidationError(Exception):
+    segment: str
+    fieldName: str
+    failCount: int
+    recordCount: int
+
+    def __init__(self, msg: str, segment: str, field_name: str, fail_count: int, record_count: int) -> None:
+        self.segment = segment
+        self.fieldName = field_name
+        self.failCount = fail_count
+        self.recordCount = record_count
+        super().__init__(msg)
+
+
+class SourceFileError(Exception):
+    file_name: str
+
+    def __init__(self, msg, file_name):
+        self.file_name = file_name
+        super().__init__(msg)

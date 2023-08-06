87e00b13ba0782f6b9c56623ef6a54b178a27c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:14 +0000

    Add package d2iq-kaptain/0.0/d2iq-kaptain-0.0.tar.gz

---
+++ packages/d2iq-kaptain/d2iq-kaptain-0.0.tar.gz/d2iq-kaptain-0.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name = 'd2iq-kaptain',
+  packages = ['lib'],
+  version = '0.0',
+  license='NONE',
+  description = 'Stub package, do not use.',
+  author = 'D2iQ Kaptain',
+  author_email = 'tharper@mesosphere.com',
+  url = 'https://d2iq.com',
+  download_url = 'https://d2iq.com',
+  keywords = [],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',
+  ],
+)

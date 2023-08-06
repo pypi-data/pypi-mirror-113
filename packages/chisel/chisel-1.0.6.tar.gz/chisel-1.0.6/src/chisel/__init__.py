e6aa9b2c579fc85183706cc3d62ba750e22bf77b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:11 +0000

    Add package chisel/1.0.6/chisel-1.0.6.tar.gz

---
+++ packages/chisel/chisel-1.0.6.tar.gz/chisel-1.0.6/src/chisel/__init__.py
@@ -0,0 +1,27 @@
+# Licensed under the MIT License
+# https://github.com/craigahobbs/chisel/blob/main/LICENSE
+
+"""
+Chisel is a light-weight Python WSGI application framework with tools for building well-documented,
+well-tested, schema-validated JSON web APIs.
+"""
+
+__version__ = '1.0.6'
+
+from .action import \
+    Action, \
+    ActionError, \
+    action
+
+from .app import \
+    Application, \
+    Context
+
+from .doc import \
+    create_doc_requests
+
+from .request import \
+    RedirectRequest, \
+    Request, \
+    StaticRequest, \
+    request

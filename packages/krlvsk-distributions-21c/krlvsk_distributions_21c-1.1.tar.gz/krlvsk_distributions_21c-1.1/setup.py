a4347722b85f873010ac0d28ee1f5800264e4fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:54 +0000

    Add package krlvsk-distributions-21c/1.1/krlvsk_distributions_21c-1.1.tar.gz

---
+++ packages/krlvsk-distributions-21c/krlvsk_distributions_21c-1.1.tar.gz/krlvsk_distributions_21c-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='krlvsk_distributions_21c',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['krlvsk_distributions_21c'],
+      author='krlvsk',
+      zip_safe=False)

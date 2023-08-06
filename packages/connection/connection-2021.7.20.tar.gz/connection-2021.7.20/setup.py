f7e6688b23d26e1c50c4c226784c3a3bec0a93f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:41 +0000

    Add package connection/2021.7.20/connection-2021.7.20.tar.gz

---
+++ packages/connection/connection-2021.7.20.tar.gz/connection-2021.7.20/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='connection',
+    version='2021.7.20',
+    install_requires=[
+        'requests'
+    ],
+    packages=[
+        'connection'
+    ]
+)

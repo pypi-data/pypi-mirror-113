4e5c6b5e40afecb5a43771e3c3c214fb26244b83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:44 +0000

    Add package aditya-distribution/0.1/aditya_distribution-0.1.tar.gz

---
+++ packages/aditya-distribution/aditya_distribution-0.1.tar.gz/aditya_distribution-0.1/setup.py
@@ -0,0 +1,10 @@
+from os import name
+from setuptools import setup
+
+setup(name= 'aditya_distribution',
+    version = '0.1',
+    description='Gaussian Distributions',
+    packages=['aditya_distribution'],
+    author = 'Aditya Khadke',
+    author_email='Aditya.Khadke04@gmail.com',
+   zip_safe=False)

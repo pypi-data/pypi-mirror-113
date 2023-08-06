4c02358aa5f29e7af49288a3621fda6fa3f0e516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:49 +0000

    Add package bs-aweme-common/0.0.1/bs_aweme_common-0.0.1.tar.gz

---
+++ packages/bs-aweme-common/bs_aweme_common-0.0.1.tar.gz/bs_aweme_common-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bs_aweme_common',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='me',
+    author_email='me@example.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)

19b3921132c1d0ead79af755fa202078e78e8332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:43 +0000

    Add package weimcgrady91-nester/1.0.0/weimcgrady91-nester-1.0.0.tar.gz

---
+++ packages/weimcgrady91-nester/weimcgrady91-nester-1.0.0.tar.gz/weimcgrady91-nester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+       name = 'weimcgrady91-nester',
+       version = '1.0.0',
+       py_modules = ['nester'],
+       author ='hfpython',
+       author_email='331855608@qq.com',
+       url='http://www.headfirst.com',
+       description='A simple printer'
+     )

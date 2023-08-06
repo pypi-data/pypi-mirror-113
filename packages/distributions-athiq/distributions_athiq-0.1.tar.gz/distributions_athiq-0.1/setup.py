f576d8e836cb11d8d3974115e34ccec7786ea0d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:52 +0000

    Add package distributions-athiq/0.1/distributions_athiq-0.1.tar.gz

---
+++ packages/distributions-athiq/distributions_athiq-0.1.tar.gz/distributions_athiq-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_athiq',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_athiq'],
+      zip_safe=False)

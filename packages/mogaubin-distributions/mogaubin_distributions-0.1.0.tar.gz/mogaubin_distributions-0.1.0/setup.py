253890ddff770be664d29facaed0aa4f2bbe4ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:24 +0000

    Add package mogaubin-distributions/0.1.0/mogaubin_distributions-0.1.0.tar.gz

---
+++ packages/mogaubin-distributions/mogaubin_distributions-0.1.0.tar.gz/mogaubin_distributions-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from os import name
+from setuptools import setup, version
+import setuptools
+
+setup(
+    name='mogaubin_distributions',
+    version='0.1.0',
+    description='Gaussian and Binomial distributions',
+    package=['mogausbin_distributions'],
+    author='Martin Oywa',
+    author_email='test.email@email.com',
+    zip_safe=False
+)

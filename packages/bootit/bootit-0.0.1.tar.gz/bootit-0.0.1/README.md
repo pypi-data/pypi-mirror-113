029b1fd3323c345694a3c014dc965affc67046b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:16 +0000

    Add package bootit/0.0.1/bootit-0.0.1.tar.gz

---
+++ packages/bootit/bootit-0.0.1.tar.gz/bootit-0.0.1/README.md
@@ -0,0 +1,15 @@
+#PACKAGE NAME
+
+## Authors:
+
+.
+├── bin
+├── CHANGES.txt
+├── License.txt
+├── MANIFEST.in
+├── package_name
+│   ├── __init__.py
+│   └── test
+│       └── __init__.py
+├── README.md
+└── setup.py
---
+++ packages/bootit/bootit-0.0.1.tar.gz/bootit-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bootit",
+    version="0.0.1",
+    author="Vikram",
+    author_email="vikram_soni1@baxter.com",
+    description="test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    keywords='',
+    install_requires=[
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

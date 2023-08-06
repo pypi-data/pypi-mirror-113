09185326cdb7c8bf5053fa946244509c4969f395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:54 +0000

    Add package mabel/0.4.64/mabel-0.4.64.tar.gz

---
+++ packages/mabel/mabel-0.4.64.tar.gz/mabel-0.4.64/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages  # type:ignore
+
+with open("mabel/version.py", "r") as v:
+    vers = v.read()
+exec(vers)  # nosec
+
+with open("README.md", "r") as rm:
+    long_description = rm.read()
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="mabel",
+    version=__version__,
+    description="Python Data Libraries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    maintainer="Joocer",
+    packages=find_packages(include=["mabel", "mabel.*"]),
+    url="https://github.com/mabel-dev/mabel/",
+    install_requires=required,
+)

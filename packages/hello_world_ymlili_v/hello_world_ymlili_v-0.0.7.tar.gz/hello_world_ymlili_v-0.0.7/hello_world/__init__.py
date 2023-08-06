6135f7c2ef765845c60c7dd6141379e67bade6bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:30 +0000

    Add package hello_world_ymlili_v/0.0.7/hello_world_ymlili_v-0.0.7.tar.gz

---
+++ packages/hello_world_ymlili_v/hello_world_ymlili_v-0.0.7.tar.gz/hello_world_ymlili_v-0.0.7/hello_world/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/22 9:54 上午
+# @Author  : lili4cityu
+# @File    : __init__.py.py
+
+
+def hw():
+    print("Hello world")
+
+
+if __name__ == '__main__':
+    hw()

41ad646189da756b9fdd410a0b900d8eea70130b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:36 +0000

    Add package DeepXDE/0.13.0/DeepXDE-0.13.0-py3-none-any.whl

---
+++ packages/DeepXDE/DeepXDE-0.13.0-py3-none-any.whl/deepxde/utils/__init__.py
@@ -0,0 +1,18 @@
+"""Internal utilities."""
+
+import importlib
+import sys
+
+from .external import *
+from .internal import *
+from ..backend import backend_name
+
+
+def _load_backend(mod_name):
+    mod = importlib.import_module(".%s" % mod_name, __name__)
+    thismod = sys.modules[__name__]
+    for api, obj in mod.__dict__.items():
+        setattr(thismod, api, obj)
+
+
+_load_backend(backend_name.replace(".", "_"))

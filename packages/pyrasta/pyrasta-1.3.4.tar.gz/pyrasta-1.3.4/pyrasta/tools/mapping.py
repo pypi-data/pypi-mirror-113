7337bbebc6d716bd5602d164b7a43d16d7786a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:33 +0000

    Add package pyrasta/1.3.4/pyrasta-1.3.4.tar.gz

---
+++ packages/pyrasta/pyrasta-1.3.4.tar.gz/pyrasta-1.3.4/pyrasta/tools/mapping.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+""" Module summary description.
+
+More detailed description.
+"""
+
+GDAL_TO_NUMPY = {1: "int8",
+                 2: "uint16",
+                 3: "int16",
+                 4: "uint32",
+                 5: "int32",
+                 6: "float32",
+                 7: "float64",
+                 10: "complex64",
+                 11: "complex128"}
+
+NUMPY_TO_GDAL = {"int8": 1,
+                 "uint16": 2,
+                 "int16": 3,
+                 "uint32": 4,
+                 "int32": 5,
+                 "float32": 6,
+                 "float64": 7,
+                 "complex64": 10,
+                 "complex128": 11}

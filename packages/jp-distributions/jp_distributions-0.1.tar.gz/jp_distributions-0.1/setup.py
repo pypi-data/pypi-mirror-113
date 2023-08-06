c6aa0dc4d335205a530fa866adf5c724d9b46454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:23 +0000

    Add package jp-distributions/0.1/jp_distributions-0.1.tar.gz

---
+++ packages/jp-distributions/jp_distributions-0.1.tar.gz/jp_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='jp_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['jp_distributions'],
+      zip_safe=False,
+      install_requires=['matplotlib'])

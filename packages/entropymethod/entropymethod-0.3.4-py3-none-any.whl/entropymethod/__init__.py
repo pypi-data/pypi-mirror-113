852da5841de728eef8c0a9cb788d0d297ca46f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:00 +0000

    Add package entropymethod/0.3.4/entropymethod-0.3.4-py3-none-any.whl

---
+++ packages/entropymethod/entropymethod-0.3.4-py3-none-any.whl/entropymethod/__init__.py
@@ -0,0 +1,9 @@
+"""
+This package implements the Entropy Method for estimating the number of clusters in a data set.
+"""
+
+from .core import (
+    compute_prob_mat,
+    entropy_score,
+    find_k,
+)

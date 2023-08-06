d34859da3ff0703aa97a2d7581c7d085fc48345f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:47 +0000

    Add package python-seispy/1.1.26/python-seispy-1.1.26.tar.gz

---
+++ packages/python-seispy/python-seispy-1.1.26.tar.gz/python-seispy-1.1.26/seispy/__init__.py
@@ -0,0 +1,7 @@
+from seispy import decov
+from seispy.distaz import distaz
+from seispy import geo
+from seispy import bootstrap
+from seispy import mccc
+from seispy.para import para
+from seispy import signal

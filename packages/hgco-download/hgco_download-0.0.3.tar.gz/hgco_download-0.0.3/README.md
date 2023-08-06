e9fe9f64e0b3de8abcf4015c8253d89ac82fd26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:43 +0000

    Add package hgco-download/0.0.3/hgco_download-0.0.3.tar.gz

---
+++ packages/hgco-download/hgco_download-0.0.3.tar.gz/hgco_download-0.0.3/README.md
@@ -0,0 +1,13 @@
+# hgco_download
+hgco_download
+
+# install
+```bash
+pip install git+https://github.com/JunnYu/hgco_download
+or
+pip install hgco-download
+```
+# usage
+```bash
+hgco_download --url https://huggingface.co/bert-base-uncased --path ./
+```
---
+++ packages/hgco-download/hgco_download-0.0.3.tar.gz/hgco_download-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hgco_download",
+    version="0.0.3",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    entry_points={
+        "console_scripts": [
+            "hgco_download=hgco_download.download_cli:main",
+        ]
+    },
+    install_requires=["hf-lfs"],
+)
+
+"""
+python setup.py sdist bdist_wheel
+twine upload dist/*
+"""

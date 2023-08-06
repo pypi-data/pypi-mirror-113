52d8697fb6c28c732020c547d5fba6dead2dd45f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:33 +0000

    Add package yourtube/0.1.3/yourtube-0.1.3.tar.gz

---
+++ packages/yourtube/yourtube-0.1.3.tar.gz/yourtube-0.1.3/yourtube/__main__.py
@@ -0,0 +1,9 @@
+import shlex
+import subprocess
+import pathlib
+
+dir = pathlib.Path(__file__).parent.resolve()
+
+def run():
+    cmd = f"voila --theme=dark {dir}/YourTube.ipynb"
+    subprocess.run(shlex.split(cmd))

8054beda6d58f5038649f17525803c672d367e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:16 +0000

    Add package NewName-Dist/0.1/NewName_Dist-0.1.tar.gz

---
+++ packages/NewName-Dist/NewName_Dist-0.1.tar.gz/NewName_Dist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='NewName_Dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['NewName_Dist'],
+      zip_safe=False)

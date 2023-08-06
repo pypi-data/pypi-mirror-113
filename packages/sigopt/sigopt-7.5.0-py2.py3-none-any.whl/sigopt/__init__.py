d6d44896c0b921cabca8921a6f47bf74fbd101c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:27 +0000

    Add package sigopt/7.5.0/sigopt-7.5.0-py2.py3-none-any.whl

---
+++ packages/sigopt/sigopt-7.5.0-py2.py3-none-any.whl/sigopt/__init__.py
@@ -0,0 +1,23 @@
+from .interface import Connection
+from .magics import SigOptMagics as _Magics
+from .runs import (
+  RunFactory,
+  create_run,
+  get_parameter,
+  log_checkpoint,
+  log_dataset,
+  log_failure,
+  log_image,
+  log_metadata,
+  log_metric,
+  log_model,
+  set_parameters,
+)
+from .version import VERSION
+
+
+def load_ipython_extension(ipython):
+  ipython.register_magics(_Magics)
+
+def get_run_id():
+  return RunFactory.get_global_run_context().run_id

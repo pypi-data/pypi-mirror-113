9ef66afff2a05549406b7d623fad0c41793d667c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:10 +0000

    Add package perceptilabs/0.12.16/perceptilabs-0.12.16-cp38-cp38-win_amd64.whl

---
+++ packages/perceptilabs/perceptilabs-0.12.16-cp38-cp38-win_amd64.whl/perceptilabs/stats/__init__.py
@@ -0,0 +1,5 @@
+from perceptilabs.stats.sample import SampleStatsTracker, SampleStats
+from perceptilabs.stats.gradients import GradientStatsTracker, GradientStats
+from perceptilabs.stats.image import ImageOutputStatsTracker, ImageOutputStats
+from perceptilabs.stats.global_ import GlobalStatsTracker, GlobalStats
+

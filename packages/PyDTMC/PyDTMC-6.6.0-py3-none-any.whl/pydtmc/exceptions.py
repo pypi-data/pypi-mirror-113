3a1c680446a76668900131eaa3bb031f6078f2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:39 +0000

    Add package PyDTMC/6.6.0/PyDTMC-6.6.0-py3-none-any.whl

---
+++ packages/PyDTMC/PyDTMC-6.6.0-py3-none-any.whl/pydtmc/exceptions.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+__all__ = [
+    'ValidationError'
+]
+
+
+###########
+# CLASSES #
+###########
+
+class ValidationError(Exception):
+
+    """
+    Defines an exception thrown when inappropriate argument values are provided.
+    """

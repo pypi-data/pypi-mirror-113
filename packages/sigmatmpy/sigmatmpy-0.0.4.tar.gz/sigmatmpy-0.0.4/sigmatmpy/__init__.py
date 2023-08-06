cedb18b4499067338b29e1a969f27bf074124338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:00 +0000

    Add package sigmatmpy/0.0.4/sigmatmpy-0.0.4.tar.gz

---
+++ packages/sigmatmpy/sigmatmpy-0.0.4.tar.gz/sigmatmpy-0.0.4/sigmatmpy/__init__.py
@@ -0,0 +1,13 @@
+from .price import price
+from .trade import trade
+from .API import API
+
+
+__title__ = "SIGMATMPY"
+__version__ = "0.0.4"
+__author__ = "SIGMATM"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2021 BFJFunds"
+
+# Version synonym
+VERSION = __version__

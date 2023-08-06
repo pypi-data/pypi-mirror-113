f789fe5193c1accc28507bba3cc851285d05990d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:39 +0000

    Add package HypixelLib/2021.7.23.3/HypixelLib-2021.7.23.3-py3-none-any.whl

---
+++ packages/HypixelLib/HypixelLib-2021.7.23.3-py3-none-any.whl/HypixelLib/hypixel.py
@@ -0,0 +1,12 @@
+from .objects import *
+
+
+class Hypixel:
+
+	def __init__(self, api):
+		if api == "DEV":
+			self.api = "d44a57f7-ebe1-4b05-9370-9db9c94d9464"
+		else:
+			self.api = api
+
+		self.player = get_player(self.api)

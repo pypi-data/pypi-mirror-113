38d05f6ef134e386a8301f2734bcad46bc907ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:58 +0000

    Add package dler/0.3.1/dler-0.3.1.tar.gz

---
+++ packages/dler/dler-0.3.1.tar.gz/dler-0.3.1/dler/common/loggers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: wxnacy(wxnacy@gmail.com)
+"""
+
+"""
+import logging
+
+logging.basicConfig(
+    filename='/tmp/pydler.log',
+)
+
+from wpy.common.loggers import create_logger
+
+
+

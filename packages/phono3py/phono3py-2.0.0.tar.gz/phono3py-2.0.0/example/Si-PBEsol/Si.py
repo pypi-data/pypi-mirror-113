989f888990947377001e312ffef69a4e00715097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:01 +0000

    Add package phono3py/2.0.0/phono3py-2.0.0.tar.gz

---
+++ packages/phono3py/phono3py-2.0.0.tar.gz/phono3py-2.0.0/example/Si-PBEsol/Si.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import numpy as np
+from phonopy.interface.vasp import read_vasp
+from phono3py import Phono3py
+import phono3py
+
+
+def run_thermal_conductivity():
+    """Run RTA thermal conductivity calculation from input files."""
+    ph3 = phono3py.load("phono3py_disp.yaml", log_level=2)
+    ph3.mesh_numbers = [11, 11, 11]
+    ph3.init_phph_interaction()
+    ph3.run_thermal_conductivity(
+        temperatures=range(0, 1001, 10),
+        write_kappa=True)
+    # Conductivity_RTA object
+    cond_rta = ph3.thermal_conductivity
+
+
+def create_supercells_with_displacements():
+    """Create supercells with displacements."""
+    cell = read_vasp("POSCAR-unitcell")
+    ph3 = Phono3py(cell,
+                   np.diag([2, 2, 2]),
+                   primitive_matrix='F')
+    ph3.generate_displacements(distance=0.03)
+    scells_with_disps = ph3.supercells_with_displacements
+    print(ph3.displacements.shape)  # (supercells, atoms, xyz)
+
+
+if __name__ == '__main__':
+    create_supercells_with_displacements()
+    run_thermal_conductivity()

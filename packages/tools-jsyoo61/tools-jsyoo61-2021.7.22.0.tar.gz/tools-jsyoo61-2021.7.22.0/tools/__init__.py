bd09afd2306483551437e66526778e7582d4a53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:31 +0000

    Add package tools-jsyoo61/2021.7.22.0/tools-jsyoo61-2021.7.22.0.tar.gz

---
+++ packages/tools-jsyoo61/tools-jsyoo61-2021.7.22.0.tar.gz/tools-jsyoo61-2021.7.22.0/tools/__init__.py
@@ -0,0 +1,13 @@
+
+__version__ = "2021.07.22.0"
+# from . import tools, data, my_random
+from .tools import *
+from . import my_random, os
+
+'''
+For third-party packages, import manually by something like:
+import tools.pytorch
+import tools.pytorch as tpytorch
+import tools.pytorch as tpt
+
+'''

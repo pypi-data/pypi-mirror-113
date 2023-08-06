e22f4bf2de99c0787bf5f9fe46dec39fe816ddbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:48 +0000

    Add package quo/2021.3/quo-2021.3.tar.gz

---
+++ packages/quo/quo-2021.3.tar.gz/quo-2021.3/src/quo/i_o/__init__.py
@@ -0,0 +1,14 @@
+from .termui import (
+        echo,
+        pause, 
+        style,
+        unstyle,
+        confirm,
+        terminalsize,
+        launch, 
+        raw_terminal, 
+        interpose,
+        edit,
+        clear, 
+        prompt
+        )

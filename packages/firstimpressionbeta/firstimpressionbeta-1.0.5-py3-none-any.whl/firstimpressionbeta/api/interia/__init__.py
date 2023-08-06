66f3a253849c3ba9147c1a55e98bb3bc9f00c1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:14 +0000

    Add package firstimpressionbeta/1.0.5/firstimpressionbeta-1.0.5-py3-none-any.whl

---
+++ packages/firstimpressionbeta/firstimpressionbeta-1.0.5-py3-none-any.whl/firstimpressionbeta/api/interia/__init__.py
@@ -0,0 +1,141 @@
+from firstimpressionbeta.constants import APIS
+from firstimpressionbeta.constants import TEMP_FOLDER
+from firstimpressionbeta.constants import LOCAL_INTEGRATED_FOLDER
+from firstimpressionbeta.constants import PICTURE_FORMATS
+from firstimpressionbeta.constants import TAGS_TO_REMOVE
+from firstimpressionbeta.constants import REPLACE_DICT
+from firstimpressionbeta.file import update_directories_api
+from firstimpressionbeta.file import create_directories
+from firstimpressionbeta.file import check_too_old
+from firstimpressionbeta.file import write_root_to_xml_files
+from firstimpressionbeta.file import list_files_dir
+from firstimpressionbeta.file import download_media
+from firstimpressionbeta.file import xml_to_root
+from firstimpressionbeta.rss import get_feed
+from firstimpressionbeta.text import remove_tags_from_string
+from firstimpressionbeta.xml import get_attrib_from_element
+from firstimpressionbeta.scala import variables
+from firstimpressionbeta.scala import install_content
+import xml.etree.ElementTree as ET
+import os
+import glob
+
+
+##################################################################################################
+# CONSTANTS
+##################################################################################################
+
+NAME = APIS['news_polish']
+
+RSSFEED_URL = {
+    'sport': 'https://sport.interia.pl/feed',
+    'polska': 'https://wydarzenia.interia.pl/polska/feed',
+    'kultura': 'https://wydarzenia.interia.pl/kultura/feed'
+}
+
+TAGS = {
+    'item': 'channel/item',
+    'title': 'title',
+    'description': 'description'
+}
+
+LOCAL_PATH = os.path.join(LOCAL_INTEGRATED_FOLDER, NAME)
+
+MAX_ITEMS = 4
+MAX_FILE_AGE = 60 * 10
+
+##################################################################################################
+# MAIN FUNCTIONS API
+##################################################################################################
+
+def run_api(news_categories, minimal_items):
+
+    update_directories_api(NAME)
+
+    minimal_items = MAX_ITEMS if minimal_items > MAX_ITEMS else minimal_items
+
+    for news_category in news_categories:
+        if news_category == '' or news_category is None:
+            continue
+
+        category = news_category.lower()
+        url = RSSFEED_URL[category]
+        xml_temp_path = os.path.join(TEMP_FOLDER, NAME, '{}.xml'.format(category))
+
+        create_directories([os.path.join(LOCAL_INTEGRATED_FOLDER, NAME, category), os.path.join(TEMP_FOLDER, NAME, category)])
+
+        if check_too_old(xml_temp_path, MAX_FILE_AGE):
+            root = ET.Element("root")
+            feed = get_feed(url)
+
+            for news_item in feed.findall(TAGS['item']):
+                item = ET.SubElement(root, "item")
+                ET.SubElement(item, "title").text = get_title(news_item)
+                ET.SubElement(item, "description").text = get_description(news_item)
+
+                media_link = install_picture_content_wrap('square', os.path.join(NAME, category), TEMP_FOLDER, news_item, 'enclosure', 'url')
+
+                if not media_link:
+                    media_link = 'Content:\\placeholders\\img.png'
+
+                ET.SubElement(item, "picsqr").text = media_link
+
+                if len(root) == MAX_ITEMS:
+                    break
+            
+            if len(root) >= minimal_items:
+                write_root_to_xml_files(root, xml_temp_path, NAME)
+            else:
+                #TODO log
+                pass
+
+def check_api(news_categories):
+    svars = variables()
+
+    for category in news_categories:
+        file_path = glob.glob(os.path.join(LOCAL_INTEGRATED_FOLDER, NAME, '{}*.xml'.format(category.lower())))[0]
+        if check_too_old(file_path, MAX_FILE_AGE):
+            svars['skipscript'] = True
+            break
+        else:
+            svars['skipscript'] = False
+
+            svars['total_items'] = len(xml_to_root(file_path))
+
+    
+    svars['total_categories'] = len(news_categories)
+    
+
+##################################################################################################
+# MEDIA FUNCTIONS
+##################################################################################################
+
+def install_picture_content_wrap(picture_format, subdirectory, temp_folder, element, tag, attrib):
+    # Installs content to LocalIntegratedContent folder and returns mediapath
+    if not picture_format in PICTURE_FORMATS:
+        return None
+
+    media_link = get_attrib_from_element(element, tag, attrib).replace("_sqr256", "")
+
+    media_path = download_media(PICTURE_FORMATS[picture_format] + media_link, subdirectory, temp_folder)
+
+    install_content(media_path, subdirectory)
+
+    media_filename = media_path.split('\\').pop()
+
+    return os.path.join('Content:\\', subdirectory, media_filename)
+
+##################################################################################################
+# GET FUNCTIONS
+##################################################################################################
+
+def get_title(news_item):
+    return news_item.findtext(TAGS['title'], '')
+
+def get_description(news_item):
+    return remove_tags_from_string(TAGS_TO_REMOVE, news_item.findtext(TAGS['description'], ''))
+
+##################################################################################################
+# PARSE FUNCTIONS
+##################################################################################################
+

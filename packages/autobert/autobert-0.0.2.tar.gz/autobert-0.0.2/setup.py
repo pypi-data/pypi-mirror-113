e75a023c310eaa84f5686501e84014b4d03100f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:28 +0000

    Add package autobert/0.0.2/autobert-0.0.2.tar.gz

---
+++ packages/autobert/autobert-0.0.2.tar.gz/autobert-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="autobert",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    version="0.0.2",
+    license="MIT",
+    description="AutoBert_pytorch",
+    author="Jun Yu",
+    author_email="573009727@qq.com",
+    url="https://github.com/JunnYu/AutoBert_pytorch",
+    keywords=["autobert", "pytorch"],
+    install_requires=["transformers>=4.8.0"],
+)
+
+"""
+python setup.py sdist bdist_wheel
+twine upload dist/*
+"""

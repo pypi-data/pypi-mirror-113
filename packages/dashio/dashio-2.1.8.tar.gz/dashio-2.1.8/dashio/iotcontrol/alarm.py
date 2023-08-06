3e4243c78303221e14678eae70d315572e117a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:45 +0000

    Add package dashio/2.1.8/dashio-2.1.8.tar.gz

---
+++ packages/dashio/dashio-2.1.8.tar.gz/dashio-2.1.8/dashio/iotcontrol/alarm.py
@@ -0,0 +1,33 @@
+from .control import Control
+from .enums import SoundName
+
+
+class Alarm(Control):
+
+    def __init__(self, control_id, description="Alarm Descritption", sound_name=SoundName.DEFAULT):
+        super().__init__("ALM", control_id)
+        self.description = description
+        self.sound_name = sound_name
+
+    def send(self, header, body):
+        self.message_tx_event(self.control_id, header, body)
+
+    def get_state(self):
+        return ""
+
+    @property
+    def description(self) -> str:
+        return self._cfg["description"]
+
+    @description.setter
+    def description(self, val: str):
+        self._cfg["description"] = val
+
+    @property
+    def sound_name(self) -> SoundName:
+        return self._sound_name
+
+    @sound_name.setter
+    def sound_name(self, val: SoundName):
+        self._sound_name = val
+        self._cfg["soundName"] = val.value

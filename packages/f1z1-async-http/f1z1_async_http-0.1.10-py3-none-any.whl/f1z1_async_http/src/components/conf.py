412fb5ea84cb589301e14fb70a443f6824fde05d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:11 +0000

    Add package f1z1-async-http/0.1.10/f1z1_async_http-0.1.10-py3-none-any.whl

---
+++ packages/f1z1-async-http/f1z1_async_http-0.1.10-py3-none-any.whl/f1z1_async_http/src/components/conf.py
@@ -0,0 +1,162 @@
+# @Time     : 2021/5/30
+# @Project  : f1z1-g
+# @IDE      : PyCharm
+# @Author   : Angel
+# @Email    : 376355670@qq.com
+from collections import defaultdict
+from typing import Union
+
+from f1z1_common import AbstractConfReader
+
+DEFAULT_TIMEOUT = 3600
+DEFAULT_MAX_CONNECTION = 100
+DEFAULT_MAX_KEEPALIVE = 20
+
+
+class IConfig(object):
+
+    @property
+    def base_url(self) -> str:
+        """
+        base url config
+        :return:
+        """
+        raise NotImplementedError("NotImplemented .base_url -> str")
+
+    @property
+    def max_connection(self) -> int:
+        """
+        connect pool max connection
+        :return:
+        """
+        raise NotImplementedError("NotImplemented .max_connection -> int")
+
+    @property
+    def max_keepalive(self) -> int:
+        """
+        connect pool max keepalive
+        :return:
+        """
+        raise NotImplementedError("NotImplemented .max_keepalive -> int")
+
+    @property
+    def timeout(self) -> Union[int, float]:
+        """
+        timeout
+        :return:
+        """
+        raise NotImplementedError("NotImplemented .timeout -> int or float")
+
+
+class IConfReader(object):
+
+    def read_base_url(self) -> str:
+        raise NotImplementedError("")
+
+    def read_timeout(self) -> Union[int, float]:
+        raise NotImplementedError("")
+
+    def read_max_connection(self) -> int:
+        raise NotImplementedError("")
+
+    def read_max_keepalive(self) -> int:
+        raise NotImplementedError("")
+
+
+class IConfigGenerator(object):
+
+    def generate(self) -> IConfig:
+        raise NotImplementedError("")
+
+
+class IConfigManager(object):
+
+    def set(self, node: str, config: IConfig):
+        raise NotImplementedError("")
+
+    def get(self, node: str) -> IConfig:
+        raise NotImplementedError("")
+
+
+class Config(IConfig):
+
+    def __init__(self,
+                 base_url: str = None,
+                 timeout: Union[int, float] = None,
+                 max_connection: int = None,
+                 max_keepalive: int = None):
+        self._base_url = base_url if base_url else ""
+        self._timeout = timeout if timeout else DEFAULT_TIMEOUT
+        self._max_connection = max_connection if max_connection else DEFAULT_MAX_CONNECTION
+        self._max_keepalive = max_keepalive if max_keepalive else DEFAULT_MAX_KEEPALIVE
+
+    @property
+    def base_url(self) -> str:
+        return self._base_url
+
+    @property
+    def max_connection(self) -> int:
+        return abs(self._max_connection)
+
+    @property
+    def max_keepalive(self) -> int:
+        return abs(self._max_keepalive)
+
+    @property
+    def timeout(self) -> Union[int, float]:
+        return abs(self._timeout)
+
+
+class ConfigManager(IConfigManager):
+    __slots__ = [
+        "_configs"
+    ]
+
+    def __init__(self):
+        self._configs = defaultdict(Config)
+
+    def get(self, node: str) -> IConfig:
+        return self[node]
+
+    def set(self, node: str, config: IConfig):
+        self[node] = config
+
+    def __getitem__(self, item):
+        return self._configs[item]
+
+    def __setitem__(self, key, value: IConfig):
+        self._configs[key] = value
+
+
+class ConfigReader(IConfReader):
+
+    def __init__(self, reader: AbstractConfReader, node: str):
+        self._node = node
+        self._reader = reader
+
+    def read_base_url(self) -> str:
+        return self._reader.get_string(self._node, "BASE_URL")
+
+    def read_timeout(self) -> Union[int, float]:
+        return self._reader.get_float(self._node, "TIMEOUT")
+
+    def read_max_connection(self) -> int:
+        return self._reader.get_int(self._node, "MAX_CONNECTION")
+
+    def read_max_keepalive(self) -> int:
+        return self._reader.get_int(self._node, "MAX_KEEPALIVE")
+
+
+class ConfigGenerator(IConfigGenerator):
+
+    def __init__(self, reader: IConfReader):
+        self._reader = reader
+
+    def generate(self) -> IConfig:
+        config_reader = self._reader
+        return Config(
+            base_url=config_reader.read_base_url(),
+            timeout=config_reader.read_timeout(),
+            max_connection=config_reader.read_max_connection(),
+            max_keepalive=config_reader.read_max_keepalive()
+        )

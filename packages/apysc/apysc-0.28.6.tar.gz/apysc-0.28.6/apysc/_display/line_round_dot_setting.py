7d74ff7d7a183dc2f7688b5527e9daa1c159feba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:32 +0000

    Add package apysc/0.28.6/apysc-0.28.6.tar.gz

---
+++ packages/apysc/apysc-0.28.6.tar.gz/apysc-0.28.6/apysc/_display/line_round_dot_setting.py
@@ -0,0 +1,60 @@
+"""Round dot setting class implementation for line.
+"""
+
+from typing import Union
+
+import apysc as ap
+
+
+class LineRoundDotSetting(ap.Dictionary):
+
+    def __init__(
+            self, round_size: Union[int, ap.Int],
+            space_size: Union[int, ap.Int]) -> None:
+        """
+        Round dot setting class for line.
+
+        Parameters
+        ----------
+        round_size : int or Int
+            Dot round size.
+        space_size : int or Int
+            Blank space size between dots.
+        """
+        from apysc._converter.to_apysc_val_from_builtin import \
+            get_copied_int_from_builtin_val
+        from apysc._validation import number_validation
+        number_validation.validate_nums_are_int_and_gt_zero(
+            nums=[round_size, space_size])
+        round_size_: ap.Int = get_copied_int_from_builtin_val(
+            integer=round_size)
+        space_size_: ap.Int = get_copied_int_from_builtin_val(
+            integer=space_size)
+        super(LineRoundDotSetting, self).__init__({
+            'round_size': round_size_,
+            'space_size': space_size_,
+        })
+
+    @property
+    def round_size(self) -> ap.Int:
+        """
+        Get a round size setting.
+
+        Returns
+        -------
+        round_size : Int
+            Round size setting.
+        """
+        return self['round_size']
+
+    @property
+    def space_size(self) -> ap.Int:
+        """
+        Get a space size setting.
+
+        Returns
+        -------
+        space_size : Int
+            Blank space size between dots.
+        """
+        return self['space_size']

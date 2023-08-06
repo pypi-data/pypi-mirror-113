52cd50c9ed127b604dfa29b8a830f552bdd95847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:04 +0000

    Add package biglist/0.6.0/biglist-0.6.0-py3-none-any.whl

---
+++ packages/biglist/biglist-0.6.0-py3-none-any.whl/biglist/__init__.py
@@ -0,0 +1,12 @@
+from ._biglist import Biglist, ListView, FileView, FileIterStat
+from ._serializer import Serializer
+
+
+__version__ = '0.6.0'
+
+
+__all__ = [
+    'Biglist', 'ListView', 'FileView',
+    'Serializer',
+    'FileIterStat',
+]

7c240bc8e61eae30581ede419ba00deb4ad0cf4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:47 +0000

    Add package daktari/0.0.24/daktari-0.0.24.tar.gz

---
+++ packages/daktari/daktari-0.0.24.tar.gz/daktari-0.0.24/daktari/checks/files.py
@@ -0,0 +1,17 @@
+from daktari.file_utils import file_exists
+from os.path import expanduser
+from pathlib import Path
+
+from daktari.check import Check, CheckResult
+from daktari.os import OS
+
+
+class FileExists(Check):
+    name = "file.exists"
+
+    def __init__(self, file_path: str, suggestion: str):
+        self.file_path = file_path.replace("~", expanduser("~"))
+        self.suggestions = {OS.GENERIC: suggestion}
+
+    def check(self) -> CheckResult:
+        return self.verify(file_exists(self.file_path), f"{self.file_path} is <not/> present")

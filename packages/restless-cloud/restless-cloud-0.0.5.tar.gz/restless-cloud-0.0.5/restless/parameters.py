f64e33928c7a2127eeb2d49540b72463efaa9f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:38 +0000

    Add package restless-cloud/0.0.5/restless-cloud-0.0.5.tar.gz

---
+++ packages/restless-cloud/restless-cloud-0.0.5.tar.gz/restless-cloud-0.0.5/restless/parameters.py
@@ -0,0 +1,34 @@
+from restless.util import FormData
+from pydantic import BaseModel
+
+
+class StringParameter(str):
+    pass
+
+
+class PathParameter(StringParameter):
+    LOCATION = 'path'
+
+
+class QueryParameter(StringParameter):
+    LOCATION = 'query'
+
+
+class HeaderParameter(StringParameter):
+    LOCATION = 'header'
+
+
+class FormParameter(StringParameter):
+    LOCATION = 'formData'
+
+
+class FormFile(FormData.File):
+    LOCATION = 'formData'
+
+
+class BinaryParameter(bytes):
+    LOCATION = 'body'
+
+
+class BodyParameter(BaseModel):
+    pass

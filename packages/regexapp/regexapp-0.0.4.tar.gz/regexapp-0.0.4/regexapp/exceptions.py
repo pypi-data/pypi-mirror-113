e5993a31264d9df42a232cc08972d545b014cb7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:49 +0000

    Add package regexapp/0.0.4/regexapp-0.0.4.tar.gz

---
+++ packages/regexapp/regexapp-0.0.4.tar.gz/regexapp-0.0.4/regexapp/exceptions.py
@@ -0,0 +1,29 @@
+"""Module containing the exception class for regexapp."""
+
+
+class PatternError(Exception):
+    """Use to capture error during pattern conversion."""
+
+
+class EscapePatternError(PatternError):
+    """Use to capture error during performing do_soft_regex_escape"""
+
+
+class PatternReferenceError(PatternError):
+    """Use to capture error for PatternReference instance"""
+
+
+class TextPatternError(Exception):
+    """Use to capture error during pattern conversion."""
+
+
+class ElementPatternError(Exception):
+    """Use to capture error during pattern conversion."""
+
+
+class LinePatternError(PatternError):
+    """Use to capture error during pattern conversion."""
+
+
+class PatternBuilderError(PatternError):
+    """Use to capture error during pattern conversion."""

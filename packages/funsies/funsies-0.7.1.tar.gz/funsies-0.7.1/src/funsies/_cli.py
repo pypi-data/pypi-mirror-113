5fc902f53626fbf60b8e198f07d5c0e84cfac151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:57 +0000

    Add package funsies/0.7.1/funsies-0.7.1.tar.gz

---
+++ packages/funsies/funsies-0.7.1.tar.gz/funsies-0.7.1/src/funsies/_cli.py
@@ -0,0 +1,390 @@
+#!/usr/bin/env python
+"""Cli utilities."""
+from __future__ import annotations
+
+# std
+import json
+import sys
+import time
+from typing import Optional
+
+# external
+import click
+from redis import Redis
+from rq import Connection, Worker
+
+# funsies
+import funsies
+
+# module
+from . import __version__, types  # noqa
+from ._constants import hash_t
+from ._graph import get_status
+from ._logging import logger
+
+# required funsies libraries loaded in advance
+import hashlib, subprocess, loguru  # noqa isort:skip
+
+
+# This is the main funsies command
+@click.group()
+@click.version_option(__version__)
+@click.option(
+    "--url",
+    "-u",
+    type=str,
+    nargs=1,
+    default=funsies.config._get_funsies_url(),
+    show_default=True,
+    help="Redis connection URL.",
+)
+@click.pass_context
+def main(ctx: click.Context, url: str) -> None:
+    """Command-line tools for funsies.
+
+    The --url flag allows passing a custom url for the redis instance. This
+    has to come before any of the subcommands (worker, cat etc.).
+    Alternatively, the url can be set using the environment variables
+    FUNSIES_URL environment variable.
+    """
+    hn = funsies.config._extract_hostname(url)
+    logger.debug(f"connecting to {hn}")
+
+    def connect2db(try_fail: bool = False) -> Redis[bytes]:
+        db = Redis.from_url(url, decode_responses=False)
+        try:
+            db.ping()
+        except Exception as e:
+            if try_fail:
+                raise e
+            else:
+                logger.error("could not connect to server! exiting")
+                logger.error(str(e))
+                sys.exit(-1)
+        logger.debug("connection sucessful")
+        logger.info(f"connected to {hn}")
+        return db
+
+    ctx.obj = connect2db
+
+
+@main.command()
+@click.option(
+    "--burst",
+    "-b",
+    is_flag=True,
+    help="Run in burst mode (quit after all work is done)",
+)
+@click.option(
+    "--rq-log-level",
+    type=str,
+    default="WARNING",
+    help="Set logging for RQ",
+    show_default=True,
+)
+@click.argument("queues", nargs=-1)
+@click.pass_context
+def worker(ctx: click.Context, queues, burst, rq_log_level):  # noqa:ANN001,ANN201
+    """Starts an RQ worker for funsies."""
+    db: Redis[bytes] = ctx.obj()
+    with Connection(db):
+        queues = queues or ["default"]
+        if burst:
+            burst_mode = " in burst mode"
+        else:
+            burst_mode = ""
+        logger.success(f"working on queues={', '.join(queues)}{burst_mode}")
+        w = Worker(
+            queues,
+            log_job_description=False,
+            # TODO: make adjustable
+            serializer="rq.serializers.JSONSerializer",
+        )
+        w.work(burst=burst, logging_level=rq_log_level)
+
+
+@main.command()
+@click.pass_context
+def clean(ctx: click.Context) -> None:
+    """Clean job queues and DAGs."""
+    db = ctx.obj()
+    logger.info("shutting down workers")
+    funsies._context.shutdown_workers(db, True)
+    logger.info("cleaning up")
+    funsies._context.cleanup_funsies(db)
+    logger.success("done")
+
+
+@main.command()
+@click.option(
+    "--force",
+    "-f",
+    is_flag=True,
+    help="Shutdown workers without finishing jobs.",
+)
+@click.pass_context
+def shutdown(ctx: click.Context, force: bool) -> None:
+    """Tell workers to shutdown."""
+    db = ctx.obj()
+    funsies._context.shutdown_workers(db, force)
+    logger.success("done")
+
+
+@main.command()
+@click.argument("hashes", type=str, nargs=-1)
+@click.pass_context
+def cat(ctx: click.Context, hashes: tuple[str, ...]) -> None:
+    """Print artefacts to stdout."""
+    db = ctx.obj()
+
+    with funsies._context.Fun(db):
+        for hash in hashes:
+            logger.info(f"extracting {hash}")
+            things = funsies.get(hash)
+            if len(things) == 0:
+                logger.error("hash does not correspond to anything!")
+                raise SystemExit(2)
+
+            if len(things) > 1:
+                logger.error(f"hash resolves to {len(things)} things.")
+
+            art = things[0]
+            if isinstance(art, types.Artefact):
+                res = funsies.take(art, strict=False)
+                if isinstance(res, types.Error):
+                    logger.warning(f"error at {hash}: {res.kind}")
+                    if res.details is not None:
+                        sys.stderr.buffer.write((res.details + "\n").encode())
+                    logger.warning(f"error source: {res.source}")
+                elif isinstance(res, bytes):
+                    sys.stdout.buffer.write(res)
+                    logger.success(f"{hash} output to stdout")
+                elif isinstance(res, str):
+                    sys.stdout.buffer.write(res.encode())
+                    logger.success(f"{hash} output to stdout")
+                else:
+                    sys.stdout.buffer.write(json.dumps(res).encode())
+                    logger.success(f"{hash} output to stdout")
+
+            elif isinstance(art, types.Operation):
+                logger.error("not an artefact")
+                logger.info("did you mean...")
+                sys.stderr.write("      INPUTS:\n")
+                for key, val in art.inp.items():
+                    sys.stderr.write(f"      {key:<30} -> {val[:8]}\n")
+                sys.stderr.write("      OUTPUTS:\n")
+                for key, val in art.out.items():
+                    sys.stderr.write(f"      {key:<30} -> {val[:8]}\n")
+            else:
+                logger.error("not an artefact:")
+                logger.error(f"{art}")
+
+
+@main.command()
+@click.option(
+    "--output",
+    "-o",
+    type=click.Path(exists=False, writable=True),
+    help="Output location (defaults to first characters of hash).",
+)
+@click.argument(
+    "hash",
+    type=str,
+)
+@click.pass_context
+def debug(ctx: click.Context, hash: str, output: Optional[str]) -> None:
+    """Extract all data related to a given hash value."""
+    logger.info(f"extracting {hash}")
+    db = ctx.obj()
+    if output is None:
+        output = hash
+        output2 = ""
+    else:
+        output2 = output + "_"
+
+    with funsies._context.Fun(db):
+        things = funsies.get(hash)
+        if len(things) == 0:
+            logger.error(f"{hash} does not correspond to anything!")
+        elif len(things) == 1:
+            logger.info(f"got {type(things[0])}")
+            funsies.debug.anything(things[0], output)
+            logger.success(f"saved to {output}")
+
+        else:
+            logger.warning(f"got {len(things)} objects")
+            funsies.debug.anything(things[0], output)
+            logger.success(f"{type(things[0])} -> {output}")
+            for el in things[1:]:
+                funsies.debug.anything(el, output2 + el.hash)
+                logger.success(f"{type(el)} -> {output2 + el.hash}")
+
+
+@main.command()
+@click.argument(
+    "hashes",
+    type=str,
+    nargs=-1,
+)
+@click.pass_context
+def reset(ctx: click.Context, hashes: tuple[str, ...]) -> None:
+    """Reset operations and their dependents."""
+    db = ctx.obj()
+    with funsies._context.Fun(db):
+        for hash in hashes:
+            things = funsies.get(hash)
+            if len(things) == 0:
+                logger.warning(f"no object with hash {hash}")
+            if len(things) > 1:
+                logger.error(f"more than one object with hash starting with {hash}")
+                logger.info("which one do you mean of :")
+                for t in things:
+                    logger.info(f"\t{t.hash}")
+                logger.info("none were reset")
+            else:
+                if isinstance(things[0], types.Artefact) or isinstance(
+                    things[0], types.Operation
+                ):
+                    funsies.ui.reset(things[0])
+                    logger.success(f"{hash} was reset")
+                else:
+                    logger.error(
+                        f"object {hash} is neither an operation or an artefact"
+                    )
+                    logger.info(f"{hash} was not reset")
+
+
+@main.command()
+@click.argument(
+    "hashes",
+    type=str,
+    nargs=-1,
+)
+@click.option("-t", "--timeout", type=float, help="Timeout in seconds.")
+@click.pass_context
+def wait(  # noqa:C901
+    ctx: click.Context, hashes: tuple[str, ...], timeout: Optional[float]
+) -> None:
+    """Wait until redis database or certain hashes are ready."""
+    if timeout is not None:
+        tmax = time.time() + timeout
+
+    while True:
+        try:
+            db = ctx.obj(try_fail=True)
+            db.ping()
+            break
+        except Exception:
+            time.sleep(1.0)
+
+        if timeout is not None:
+            t1 = time.time()
+            if t1 > tmax:
+                logger.error("timeout exceeded")
+                raise SystemExit(2)
+
+    with funsies._context.Fun(db):
+        h = []
+        for hash in hashes:
+            things = funsies.get(hash)
+            if len(things) == 0:
+                logger.warning(f"no object with hash {hash}")
+            for t in things:
+                if isinstance(t, types.Artefact):
+                    h += [t.hash]
+                    logger.info(f"waiting on artefact at {hash}")
+                elif isinstance(t, types.Operation):
+                    h += [next(iter(t.out.values()))]
+                    logger.info(f"waiting on operation at {hash}")
+                else:
+                    logger.warning(f"ignoring {type(t)} at {t.hash}")
+
+        while len(h) > 0:
+            stat = get_status(db, h[0], resolve_links=True)
+            if stat > 0:
+                h.pop(0)
+                logger.success(f"{len(h)} things left to wait for.")
+            time.sleep(0.5)
+
+            if timeout is not None:
+                t1 = time.time()
+                if t1 > tmax:
+                    logger.error("timeout exceeded")
+                    raise SystemExit(2)
+
+
+@main.command()
+@click.argument(
+    "hashes",
+    type=str,
+    nargs=-1,
+)
+@click.option(
+    "-i",
+    "--inputs",
+    is_flag=True,
+)
+@click.pass_context
+def graph(ctx: click.Context, hashes: tuple[str, ...], inputs: bool) -> None:
+    """Print to stdout a DOT-formatted graph to visualize DAGs."""
+    # funsies
+    import funsies._graphviz
+
+    db = ctx.obj()
+    with funsies._context.Fun(db):
+        if len(hashes) == 0:
+            # If no hashes are passed, we graph all the DAGs on index
+            hashes = tuple(
+                [dag.decode() for dag in db.smembers(funsies._constants.DAG_INDEX)]
+            )
+
+        all_data: list[hash_t] = []
+        for hash in hashes:
+            things = funsies.get(hash.split("/")[-1])
+            if len(things) == 0:
+                logger.warning(f"no object with hash {hash}")
+            for t in things:
+                if isinstance(t, types.Operation) or isinstance(t, types.Artefact):
+                    all_data += [hash_t(t.hash)]
+
+        if len(all_data):
+            logger.info(f"writing graph for {len(all_data)} objects")
+            out = funsies._graphviz.format_dot(
+                *funsies._graphviz.export(db, all_data),
+                targets=all_data,
+                show_inputs=inputs,
+            )
+            sys.stdout.write(out)
+            logger.success("done")
+        else:
+            logger.error("No data points")
+            raise SystemExit(2)
+
+
+@main.command()
+@click.argument(
+    "hashes",
+    type=str,
+    nargs=-1,
+)
+@click.pass_context
+def execute(ctx: click.Context, hashes: tuple[str, ...]) -> None:
+    """Enqueue execution of hashes."""
+    db = ctx.obj()
+    with funsies._context.Fun(db):
+        exec_list = []
+        for hash in hashes:
+            things = funsies.get(hash)
+            if len(things) == 0:
+                logger.warning(f"no object with hash {hash}")
+            for t in things:
+                if isinstance(t, types.Operation) or isinstance(t, types.Artefact):
+                    exec_list += [t]
+                else:
+                    logger.warning(f"object with hash {hash} of type {type(t)} skipped")
+
+        funsies.execute(*exec_list)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/funsies/funsies-0.7.1.tar.gz/funsies-0.7.1/src/funsies/_context.py
@@ -0,0 +1,218 @@
+"""Contextual DB usage."""
+from __future__ import annotations
+
+# std
+from contextlib import contextmanager
+from dataclasses import replace
+import shutil
+import subprocess
+import tempfile
+import time
+from typing import Any, Iterator, Optional, Sequence
+
+# external
+from redis import Redis
+import rq
+from rq import command, Worker
+from rq.local import LocalStack
+
+# module
+from ._constants import _AnyPath, hash_t, join, OPERATIONS
+from ._logging import logger
+from .config import _extract_hostname, _get_funsies_url, Options
+
+# A thread local stack of connections (adapted from RQ)
+_connect_stack = LocalStack()
+_options_stack = LocalStack()
+
+
+def cleanup_funsies(connection: Redis[bytes]) -> None:
+    """Clean up Redis instance of DAGs, Queues and clear workers."""
+    queues = rq.Queue.all(connection=connection)
+    for queue in queues:
+        queue.delete(delete_jobs=True)
+
+    # Reset operation status
+    ops = join(OPERATIONS, hash_t("*"), "owner")
+    keys = connection.keys(ops)
+    if len(keys):
+        logger.info(f"clearing {len(keys)} unfinished ops")
+        for k in keys:
+            connection.delete(k)
+
+
+def shutdown_workers(db: Redis[bytes], force: bool) -> None:
+    """Shutdown funsies workers."""
+    workers = Worker.all(db)
+    logger.info(f"shutting down {len(workers)} workers")
+    for worker in workers:
+        command.send_shutdown_command(db, worker.name)  # Tells worker to shutdown
+        if force:
+            command.send_kill_horse_command(db, worker.name)
+
+
+# --------------------------------------------------------------------------------
+# Main DB context manager
+@contextmanager
+def Fun(
+    connection: Optional[Redis[bytes]] = None,
+    defaults: Optional[Options] = None,
+    cleanup: bool = False,
+) -> Iterator[Redis[bytes]]:
+    """Context manager for redis connections."""
+    if connection is None:
+        logger.warning("Opening new redis connection with default settings...")
+        url = _get_funsies_url()
+        hn = _extract_hostname(url)
+        connection = Redis.from_url(url, decode_responses=False)
+        logger.success(f"connected to {hn}")
+
+    if defaults is None:
+        defaults = Options()
+
+    if cleanup:
+        cleanup_funsies(connection)
+
+    _connect_stack.push(connection)
+    _options_stack.push(defaults)
+
+    # also push on rq
+    # TODO maybe just use the RQ version of this?
+    rq.connections.push_connection(connection)
+    try:
+        yield _connect_stack.top
+    finally:
+        popped = _connect_stack.pop()
+        assert popped == connection, (
+            "Unexpected Redis connection was popped off the stack. "
+            "Check your Redis connection setup."
+        )
+        rq.connections.pop_connection()
+        _ = _options_stack.pop()
+
+
+def get_db(db: Optional[Redis[bytes]] = None) -> Redis[bytes]:
+    """Get Redis instance."""
+    if db is None:
+        myjob = rq.get_current_job()
+        if _connect_stack.top is not None:
+            # try context instance
+            out: Redis[bytes] = _connect_stack.top
+            return out
+        elif myjob is not None:
+            out2: Redis[bytes] = myjob.connection
+            return out2
+        else:
+            raise RuntimeError("No redis instance available.")
+    else:
+        return db
+
+
+def get_options(opt: Optional[Options] = None) -> Options:
+    """Get Options instance."""
+    if opt is not None:
+        return opt
+    else:
+        if _options_stack.top is not None:
+            out: Options = _options_stack.top
+            return out
+        else:
+            raise RuntimeError("No Options instance available.")
+
+
+def options(**kwargs: Any) -> Options:
+    """Set operation and workflow options.
+
+    This function sets specific configuration options for an operation or a
+    workflow that do not change hash values or cause re-execution, but do
+    change runtime behaviour, such as job timeouts, queue selection, etc.
+    Available options and their names are described in the entry for
+    `config.Options`.
+
+    This function wraps the `config.Options` with layering of default values.
+    The value of each attribute of `config.Options` is set based on:
+
+    1. The values set by the `**kwargs` dictionary.
+
+    2. The values set in the `**kwargs` dictionary of the enclosing
+    `funsies.Fun()` context, if `default=options(**kwargs)` is passed to
+    `funsies.Fun()`.
+
+    3. The default values in `config.Options`.
+
+    This allows layering of fairly complex runtime behaviour.
+
+    """
+    if _options_stack.top is None:
+        return Options(**kwargs)
+    else:
+        defaults: Options = _options_stack.top
+        return replace(defaults, **kwargs)
+
+
+# ---------------------------------------------------------------------------------
+# Utility contexts
+@contextmanager
+def ManagedFun(
+    nworkers: int = 1,
+    worker_args: Optional[Sequence[str]] = None,
+    redis_args: Optional[Sequence[str]] = None,
+    defaults: Optional[Options] = None,
+    directory: Optional[_AnyPath] = None,
+) -> Iterator[Redis[bytes]]:
+    """Make a fully managed funsies db."""
+    if directory is None:
+        dir = tempfile.mkdtemp()
+    else:
+        dir = str(directory)
+
+    logger.debug(f"running redis-server in {dir}")
+
+    if worker_args is not None:
+        wargs = [el for el in worker_args]
+    else:
+        wargs = []
+
+    if redis_args is not None:
+        rargs = [el for el in redis_args]
+    else:
+        rargs = []
+
+    # Start redis
+    port = 16379
+    url = f"redis://localhost:{port}"
+    cmdline = ["redis-server"] + rargs + ["--port", f"{port}"]
+
+    redis_server = subprocess.Popen(
+        cmdline,
+        cwd=dir,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    # TODO:CHECK that server started successfully
+    time.sleep(0.1)
+    logger.debug(f"redis running at {url}")
+
+    # spawn workers
+    logger.debug(f"spawning {nworkers} funsies workers")
+    worker_pool = [
+        subprocess.Popen(["funsies", "--url", url, "worker"] + wargs, cwd=dir)
+        for i in range(nworkers)
+    ]
+
+    try:
+        logger.success(f"{nworkers} workers connected to {url}")
+        with Fun(Redis.from_url(url), defaults=defaults) as db:
+            yield db
+    finally:
+        logger.debug("terminating worker pool and server")
+        for w in worker_pool:
+            w.kill()
+            w.wait()
+        # stop db
+        db.shutdown()  # type:ignore
+        db.connection_pool.disconnect()
+        redis_server.wait()
+        if directory is None:
+            shutil.rmtree(dir)
+        logger.success("stopping managed fun")

4d54a8154985c95d1929e550c413093f6c4ccabd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:37 +0000

    Add package syntropy-sdk/0.2.0/syntropy_sdk-0.2.0.tar.gz

---
+++ packages/syntropy-sdk/syntropy_sdk-0.2.0.tar.gz/syntropy_sdk-0.2.0/syntropy_sdk/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+from syntropy_sdk.api.api_keys_api import ApiKeysApi
+
+# import apis into api package
+from syntropy_sdk.api.auth_api import AuthApi
+from syntropy_sdk.api.platform_api import PlatformApi
+
+# flake8: noqa

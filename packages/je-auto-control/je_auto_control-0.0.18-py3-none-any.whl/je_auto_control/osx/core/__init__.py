eea8847ca0ce45898990dd4b5f787b5d64e2236f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:51 +0000

    Add package je-auto-control/0.0.18/je_auto_control-0.0.18-py3-none-any.whl

---
+++ packages/je-auto-control/je_auto_control-0.0.18-py3-none-any.whl/je_auto_control/osx/core/__init__.py
@@ -0,0 +1,4 @@
+from je_auto_control.osx.core.osx_vk import osx_mouse_left
+from je_auto_control.osx.core.osx_vk import osx_mouse_middle
+from je_auto_control.osx.core.osx_vk import osx_mouse_right
+from je_auto_control.osx.core.osx_vk import osx_key_shift

b9a499088a86d7f0cb269a385edabd7dd39ccc57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:33 +0000

    Add package shioajicaller/0.1.2/shioajicaller-0.1.2.tar.gz

---
+++ packages/shioajicaller/shioajicaller-0.1.2.tar.gz/shioajicaller-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['shioajicaller', 'shioajicaller.cli', 'shioajicaller.codes']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dotenv>=0.18.0,<0.19.0', 'shioaji>=0.3.2.dev4,<0.4.0']
+
+setup_kwargs = {
+    'name': 'shioajicaller',
+    'version': '0.1.2',
+    'description': 'shioaj warp caller',
+    'long_description': None,
+    'author': 'Steve Lo',
+    'author_email': 'info@sd.idv.tw',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)

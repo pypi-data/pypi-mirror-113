cfab0bd4819e67967016d8fb8840fa14a245a7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:04 +0000

    Add package pgnsplit/0.1.0/pgnsplit-0.1.0.tar.gz

---
+++ packages/pgnsplit/pgnsplit-0.1.0.tar.gz/pgnsplit-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['pgnsplit']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['chess>=1.6.1,<2.0.0', 'click>=8.0.1,<9.0.0']
+
+entry_points = \
+{'console_scripts': ['pgnsplit = pgnsplit.main:main']}
+
+setup_kwargs = {
+    'name': 'pgnsplit',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'vsingh18567',
+    'author_email': '59497793+vsingh18567@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

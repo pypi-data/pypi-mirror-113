5cdf4a4a55873ae6ace7af54b5d92209522ceea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:26 +0000

    Add package yourtube/0.1.1/yourtube-0.1.1.tar.gz

---
+++ packages/yourtube/yourtube-0.1.1.tar.gz/yourtube-0.1.1/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['yourtube']
+
+package_data = \
+{'': ['*'], 'yourtube': ['failed/*']}
+
+install_requires = \
+['ipyevents>=2.0.1,<3.0.0',
+ 'ipywidgets>=7.6.3,<8.0.0',
+ 'matplotlib>=3.4.2,<4.0.0',
+ 'networkx>=2.6.1,<3.0.0',
+ 'numpy>=1.21.0,<2.0.0',
+ 'requests>=2.26.0,<3.0.0',
+ 'tqdm>=4.61.2,<5.0.0',
+ 'voila>=0.2.10,<0.3.0']
+
+entry_points = \
+{'console_scripts': ['yourtube = yourtube.__main__:run']}
+
+setup_kwargs = {
+    'name': 'yourtube',
+    'version': '0.1.1',
+    'description': 'Better youtube recommendations',
+    'long_description': None,
+    'author': 'Filip Sondej',
+    'author_email': 'filipsondej@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/yourtube/yourtube-0.1.1.tar.gz/yourtube-0.1.1/yourtube/__main__.py
@@ -0,0 +1,6 @@
+import shlex
+import subprocess
+
+def run():
+    cmd = "voila --theme=dark yourtube/YourTube.ipynb"
+    subprocess.run(shlex.split(cmd))

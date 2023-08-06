46fbf2281f0323da5a9759810def8ebaa1bba5a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:40 +0000

    Add package cloudwatch-alarms-to-teams/0.1.10/cloudwatch-alarms-to-teams-0.1.10.tar.gz

---
+++ packages/cloudwatch-alarms-to-teams/cloudwatch-alarms-to-teams-0.1.10.tar.gz/cloudwatch-alarms-to-teams-0.1.10/setup.py
@@ -0,0 +1,70 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cloudwatch-alarms-to-teams",
+    "version": "0.1.10",
+    "description": "cloudwatch-alarms-to-teams",
+    "license": "Apache-2.0",
+    "url": "https://github.com/1davidmichael/Cloudwatch-Alarms-to-Chat-Platforms",
+    "long_description_content_type": "text/markdown",
+    "author": "David Michael<1.david.michael@gmail.com>",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/1davidmichael/Cloudwatch-Alarms-to-Chat-Platforms"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cloudwatch_alarms_to_teams",
+        "cloudwatch_alarms_to_teams._jsii"
+    ],
+    "package_data": {
+        "cloudwatch_alarms_to_teams._jsii": [
+            "cloudwatch-alarms-to-teams@0.1.10.jsii.tgz"
+        ],
+        "cloudwatch_alarms_to_teams": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "aws-cdk.aws-cloudwatch-actions>=1.114.0, <2.0.0",
+        "aws-cdk.aws-cloudwatch>=1.114.0, <2.0.0",
+        "aws-cdk.aws-lambda-event-sources>=1.114.0, <2.0.0",
+        "aws-cdk.aws-lambda-nodejs>=1.114.0, <2.0.0",
+        "aws-cdk.aws-lambda>=1.114.0, <2.0.0",
+        "aws-cdk.aws-sns>=1.114.0, <2.0.0",
+        "aws-cdk.core>=1.114.0, <2.0.0",
+        "constructs>=3.2.27, <4.0.0",
+        "jsii>=1.30.0, <2.0.0",
+        "publication>=0.0.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Typing :: Typed",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)

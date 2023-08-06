829db90eb802fe046b1e15147f47d7840ad940ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:00 +0000

    Add package pyneva/0.2.0/pyneva-0.2.0.tar.gz

---
+++ packages/pyneva/pyneva-0.2.0.tar.gz/pyneva-0.2.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyneva",
+    version="0.2.0",
+    description="Custom library for electricity meters Neva MT 3xx",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Nikita Nemirovsky",
+    author_email="vaze.legend@gmail.com",
+    url="https://github.com/vazelegend/pyneva",
+    license="MIT",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    install_requires=["pyserial"],
+    python_requires=">=3.7",
+)

086e3d0508c6594fd00a09e8e0871738fe4f225a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:23 +0000

    Add package recipe-tagger/0.2.1/recipe-tagger-0.2.1.tar.gz

---
+++ packages/recipe-tagger/recipe-tagger-0.2.1.tar.gz/recipe-tagger-0.2.1/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='recipe-tagger',
+    packages=find_packages(include=['recipe_tagger']),
+    version='0.2.1',
+    description='A library for tagging and classify recipes',
+    author='Andrea Turconi',
+    license='MIT',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/TurconiAndrea/recipe-tagger',
+    download_url='https://github.com/TurconiAndrea/recipe-tagger/archive/refs/tags/0.2.1.tar.gz',
+    keywords=['food', 'recipe', 'tag', 'tagging', 'ingredient'],
+    install_requires=['wikipedia', 'PyDictionary', 'textblob', 'pyfood', 'unidecode'],
+    test_suite='tests',
+    include_package_data=True,
+    package_data={'': ['data/*.npy']},
+)

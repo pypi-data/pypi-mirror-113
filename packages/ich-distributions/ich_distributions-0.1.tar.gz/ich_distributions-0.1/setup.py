74a1dad35ae58a260830f655d33a7cc044a3d8f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:14 +0000

    Add package ich-distributions/0.1/ich_distributions-0.1.tar.gz

---
+++ packages/ich-distributions/ich_distributions-0.1.tar.gz/ich_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ich_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ich_distributions'],
+      author = 'lchh',
+      zip_safe=False
+)

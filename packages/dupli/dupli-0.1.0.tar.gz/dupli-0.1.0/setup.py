4a8d040dbea03de50c8b4ae7c81c1640476d486a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:24 +0000

    Add package dupli/0.1.0/dupli-0.1.0.tar.gz

---
+++ packages/dupli/dupli-0.1.0.tar.gz/dupli-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='dupli',
+    version='0.1.0',    
+    description='A example library',
+    url='https://github.com/abhishek5878/LibExample/',
+    author='Abhishek Vyas',
+    author_email='abhishekvyas02032001@gmail.com',    
+    license='MIT Lincense',
+    install_requires=['pandas','numpy'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)    

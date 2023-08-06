acceceb4d2afeddabd3da0d9490d76034e03355e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:35 +0000

    Add package pppoetry-demo/0.1.0/pppoetry-demo-0.1.0.tar.gz

---
+++ packages/pppoetry-demo/pppoetry-demo-0.1.0.tar.gz/pppoetry-demo-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pppoetry_demo']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pendulum>=2.1.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'pppoetry-demo',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'neilbaldwin',
+    'author_email': 'neil.baldwin@mac.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)

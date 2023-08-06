d7ffa7d478aa77c1935d5e6fb33bcc777f4fb28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:34 +0000

    Add package st-material-table/0.0.2/st-material-table-0.0.2.tar.gz

---
+++ packages/st-material-table/st-material-table-0.0.2.tar.gz/st-material-table-0.0.2/st_material_table/frontend/build/precache-manifest.83657ff081a7834a4955d6110711f052.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03f26378f0d34e59e077031d8374e21",
+    "url": "./index.html"
+  },
+  {
+    "revision": "43fd811a8cb7084f9957",
+    "url": "./static/js/2.d0448f1b.chunk.js"
+  },
+  {
+    "revision": "61af748d7030040b3d9e7f68f29fbc01",
+    "url": "./static/js/2.d0448f1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d2db60e5bfe5db2f4c7",
+    "url": "./static/js/main.39c2d08d.chunk.js"
+  },
+  {
+    "revision": "f3458c042c75ce411c2d",
+    "url": "./static/js/runtime-main.928d0424.js"
+  }
+]);

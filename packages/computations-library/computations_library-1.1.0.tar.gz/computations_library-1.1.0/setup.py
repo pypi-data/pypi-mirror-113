e3a8c3fd5f9991a3a53d1fcf04be37fcc4c3acf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:53 +0000

    Add package computations-library/1.1.0/computations_library-1.1.0.tar.gz

---
+++ packages/computations-library/computations_library-1.1.0.tar.gz/computations_library-1.1.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from setuptools import find_packages, setup
+
+import numpy
+from Cython.Build import cythonize
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+fileSet = set()
+fileSet.add("./computations_library/__init__.pyx")
+
+setup(name="computations_library",
+    zip_safe=False,
+    version="1.1.0",
+    packages=find_packages(),
+    author="Mohit Sharma",
+    description="A classification library used to compute ravi strategy for now",
+    long_description=long_description,
+    install_requires=['numpy>=1.18.1','Cython>=0.29.22'],
+    ext_modules = cythonize(fileSet),include_dirs=[numpy.get_include()])

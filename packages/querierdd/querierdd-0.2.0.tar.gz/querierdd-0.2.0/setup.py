32c10de2a9b236e118150cbacfa16c134a160d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:23 +0000

    Add package querierdd/0.2.0/querierdd-0.2.0.tar.gz

---
+++ packages/querierdd/querierdd-0.2.0.tar.gz/querierdd-0.2.0/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['querierdd']
+install_requires = \
+['click>=8.0.1,<9.0.0', 'psutil>=5.8.0,<6.0.0']
+
+entry_points = \
+{'console_scripts': ['querier = querierdd:cli']}
+
+setup_kwargs = {
+    'name': 'querierdd',
+    'version': '0.2.0',
+    'description': 'Query the system and present details in an asthetically pleasing way.',
+    'long_description': None,
+    'author': 'egee-irl',
+    'author_email': 'brian@egee.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)

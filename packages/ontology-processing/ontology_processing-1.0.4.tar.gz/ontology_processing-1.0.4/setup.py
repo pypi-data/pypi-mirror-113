801b89f47398f08b2bf6e9d581084467ab98cf2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:38 +0000

    Add package ontology-processing/1.0.4/ontology_processing-1.0.4.tar.gz

---
+++ packages/ontology-processing/ontology_processing-1.0.4.tar.gz/ontology_processing-1.0.4/setup.py
@@ -0,0 +1,53 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ontology_processing",
+    version="1.0.4",
+    description="Climate Mind ontology processing code.",
+    author="ClimateMind",
+    author_email="hello@climatemind.org",
+    url="https://github.com/ClimateMind/climatemind-ontology-processing",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        'Brotli',
+        'click',
+        'cycler',
+        'dash',
+        'dash-core-components',
+        'dash-html-components',
+        'dash-renderer',
+        'dash-table',
+        'decorator',
+        'Flask',
+        'Flask-Compress',
+        'future',
+        'itsdangerous',
+        'Jinja2',
+        'kiwisolver',
+        'MarkupSafe',
+        'matplotlib',
+        'networkx',
+        'numpy',
+        'Owlready2',
+        'pandas',
+        'Pillow',
+        'plotly',
+        'pyparsing',
+        'python-dateutil',
+        'pytz',
+        'retrying',
+        'scipy',
+        'six',
+        'validators',
+        'Werkzeug',
+    ]
+)

e653a2eb0f9371032ca2321c2bbc1dec4dc6a3a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:21 +0000

    Add package DeleteFilesRapidly/0.0.6/DeleteFilesRapidly-0.0.6.tar.gz

---
+++ packages/DeleteFilesRapidly/DeleteFilesRapidly-0.0.6.tar.gz/DeleteFilesRapidly-0.0.6/README.md
@@ -0,0 +1,1 @@
+# Just a quick tool to use threading to delete files faster on a server"# DeleteFilesRapidly" 
---
+++ packages/DeleteFilesRapidly/DeleteFilesRapidly-0.0.6.tar.gz/DeleteFilesRapidly-0.0.6/setup.py
@@ -0,0 +1,29 @@
+__author__ = 'Brian M Anderson'
+# Created on 07/20/2021
+
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='DeleteFilesRapidly',
+    author='Brian Mark Anderson',
+    author_email='b5anderson@health.ucsd.edu',
+    version='0.0.6',
+    description='Tool for quickly deleting files on a remote server via threading',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={'DeleteFilesRapidly': 'DeleteFilesRapidly'},
+    packages=['DeleteFilesRapidly'],
+    include_package_data=True,
+    url='https://github.com/brianmanderson/DeleteFilesRapidly',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+    ],
+    install_requires=required,
+)

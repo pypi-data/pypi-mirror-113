1e4248a8877935e66432aadf52abc9e45704c3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:23 +0000

    Add package jupyter-releaser/0.2.1/jupyter_releaser-0.2.1.tar.gz

---
+++ packages/jupyter-releaser/jupyter_releaser-0.2.1.tar.gz/jupyter_releaser-0.2.1/jupyter_releaser/actions/draft_release.py
@@ -0,0 +1,47 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+import os
+import shutil
+from pathlib import Path
+
+from jupyter_releaser.util import CHECKOUT_NAME
+from jupyter_releaser.util import log
+from jupyter_releaser.util import run
+
+check_release = os.environ.get("RH_IS_CHECK_RELEASE", "").lower() == "true"
+
+if check_release:
+    log("Handling Check Release action")
+
+    # Extract the changelog
+    changelog_location = os.environ.get("RH_CHANGELOG", "CHANGELOG.md")
+    changelog_location = Path(CHECKOUT_NAME) / changelog_location
+    changelog_text = changelog_location.read_text(encoding="utf-8")
+
+    # Remove the checkout
+    shutil.rmtree(CHECKOUT_NAME)
+
+    # Re-install the parent dir if it was overshadowed
+    if os.environ.get("RH_REPO_NAME") == "jupyter_releaser":
+        run("pip install -e .")
+
+run("jupyter-releaser prep-git")
+run("jupyter-releaser bump-version")
+
+if check_release:
+    # Override the changelog
+    log("Patching the changelog")
+    log(changelog_text)
+    Path(changelog_location).write_text(changelog_text)
+
+run("jupyter-releaser check-changelog")
+# Make sure npm comes before python in case it produces
+# files for the python package
+run("jupyter-releaser build-npm")
+run("jupyter-releaser check-npm")
+run("jupyter-releaser build-python")
+run("jupyter-releaser check-python")
+run("jupyter-releaser check-manifest")
+run("jupyter-releaser check-links")
+run("jupyter-releaser tag-release")
+run("jupyter-releaser draft-release")

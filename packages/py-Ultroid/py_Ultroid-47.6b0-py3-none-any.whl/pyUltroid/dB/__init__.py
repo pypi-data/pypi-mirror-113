87d51a0be5e5581c30bf7ae878925e1053fffcf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:11 +0000

    Add package py-Ultroid/47.6b0/py_Ultroid-47.6b0-py3-none-any.whl

---
+++ packages/py-Ultroid/py_Ultroid-47.6b0-py3-none-any.whl/pyUltroid/dB/__init__.py
@@ -0,0 +1,72 @@
+import os
+from decouple import config
+
+DEVLIST = [
+    "719195224",
+    "1318486004",
+    "1322549723",
+    "611816596",
+    "1003250439",
+    "1152902819",
+    "716243352",
+    "1444249738",
+    "559661211",
+    "630654925",
+    "1585809209",
+    "1572994416",
+    "1303895686",
+    "803243487",
+]
+
+stickers = [
+    "CAADAQADeAIAAm_BZBQh8owdViocCAI",
+    "CAADAQADegIAAm_BZBQ6j8GpKtnrSgI",
+    "CAADAQADfAIAAm_BZBQpqC84n9JNXgI",
+    "CAADAQADfgIAAm_BZBSxLmTyuHvlzgI",
+    "CAADAQADgAIAAm_BZBQ3TZaueMkS-gI",
+    "CAADAQADggIAAm_BZBTPcbJMorVVsQI",
+    "CAADAQADhAIAAm_BZBR3lnMZRdsYxAI",
+    "CAADAQADhgIAAm_BZBQGQRx4iaM4pQI",
+    "CAADAQADiAIAAm_BZBRRF-cjJi_QywI",
+    "CAADAQADigIAAm_BZBQQJwfzkqLM0wI",
+    "CAADAQADjAIAAm_BZBQSl5GSAT0viwI",
+    "CAADAQADjgIAAm_BZBQ2xU688gfHhQI",
+    "CAADAQADkAIAAm_BZBRGuPNgVvkoHQI",
+    "CAADAQADpgIAAm_BZBQAAZr0SJ5EKtQC",
+    "CAADAQADkgIAAm_BZBTvuxuayqvjhgI",
+    "CAADAQADlAIAAm_BZBSMZdWN2Yew1AI",
+    "CAADAQADlQIAAm_BZBRXyadiwWGNkwI",
+    "CAADAQADmAIAAm_BZBQDoB15A1jS1AI",
+    "CAADAQADmgIAAm_BZBTnOLQ8_d72vgI",
+    "CAADAQADmwIAAm_BZBTve1kgdG0Y5gI",
+    "CAADAQADnAIAAm_BZBQUMyFiylJSqQI",
+    "CAADAQADnQIAAm_BZBSMAe2V4pwhNgI",
+    "CAADAQADngIAAm_BZBQ06D92QL_vywI",
+    "CAADAQADnwIAAm_BZBRw7UAbr6vtEgI",
+    "CAADAQADoAIAAm_BZBRkv9DnGPXh_wI",
+    "CAADAQADoQIAAm_BZBQwI2NgQdyKlwI",
+    "CAADAQADogIAAm_BZBRPHJF3XChVLgI",
+    "CAADAQADowIAAm_BZBThpas7rZD6DAI",
+    "CAADAQADpAIAAm_BZBQcC2DpZcCw1wI",
+    "CAADAQADpQIAAm_BZBQKruTcEU4ntwI",
+]
+
+def get_redis_vars():
+    uri = config("REDIS_URI", default=None)
+    passw = config("REDIS_PASSWORD", default=None)
+    if url and passw:
+        var = ""
+        for i in os.environ:
+            if i.startswith("QOVERY_REDIS_") and i.endswith("_PORT"):
+                var = i
+        if var:
+            hash = var.split("QOVERY_REDIS_")[1].split("_")[0]
+            try:
+                endpoint = os.environ[f"QOVERY_REDIS_{hash}_HOST"]
+                port = os.environ[f"QOVERY_REDIS_{hash}_PORT"]
+                passw = os.environ[f"QOVERY_REDIS_{hash}_PASSWORD"]
+            except KeyError:
+                LOGS.info("Redis Vars are missing. Quitting.")
+                exit(1)
+            uri = endpoint + ":" + port
+    return uri, passw

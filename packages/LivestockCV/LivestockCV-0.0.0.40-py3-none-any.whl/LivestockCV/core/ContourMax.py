18c12a7d0f8fac56a31582b682a3a5c9a1c5387e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:06 +0000

    Add package LivestockCV/0.0.0.40/LivestockCV-0.0.0.40-py3-none-any.whl

---
+++ packages/LivestockCV/LivestockCV-0.0.0.40-py3-none-any.whl/LivestockCV/core/ContourMax.py
@@ -0,0 +1,12 @@
+# ContourMax
+
+
+# my basic libraries 
+import cv2
+from LivestockCV.core import show_image
+
+def ContourMax(img):
+	(contours, hierarchy) = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
+	biggest_contour = max(contours, key= cv2.contourArea)
+	cv2.drawContours(img, biggest_contour, contourIdx=-1, color=(0, 0, 250), thickness=2) 
+	show_image(img)

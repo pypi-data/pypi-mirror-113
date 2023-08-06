4dcdc70fa217c70c6ffd5332e10b9e7c316c3867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:26 +0000

    Add package checkov/2.0.284/checkov-2.0.284.tar.gz

---
+++ packages/checkov/checkov-2.0.284.tar.gz/checkov-2.0.284/checkov/cloudformation/checks/resource/base_registry.py
@@ -0,0 +1,9 @@
+from checkov.common.checks.base_check_registry import BaseCheckRegistry
+
+
+class Registry(BaseCheckRegistry):
+
+    def extract_entity_details(self, entity):
+        resource_name, resource = next(iter(entity.items()))
+        resource_type = resource['Type']
+        return resource_type, resource_name, resource

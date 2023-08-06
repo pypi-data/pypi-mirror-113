c747fd9849cde98b465b76f6bfd9e02f32471b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:41 +0000

    Add package funing/0.2.27/funing-0.2.27.tar.gz

---
+++ packages/funing/funing-0.2.27.tar.gz/funing-0.2.27/funing.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+git_push(){
+    echo 'Push to git@gitee.com:larryw3i/Funing.git'
+    git push git@gitee.com:larryw3i/Funing.git
+
+    echo 'Push to git@github.com:larryw3i/Funing.git'
+    git push git@github.com:larryw3i/Funing.git
+}
+
+update_gitignore(){
+    git rm -r --cached .
+    git add .
+    read -p "commit now?(y/N)" commit_now
+    if [ "$commit_now" = 'y' ] || [ "$commit_now" = 'Y' ]; then
+        git commit -m 'update .gitignore'
+    fi
+    echo "gitignore updated!"
+}
+
+
+$1

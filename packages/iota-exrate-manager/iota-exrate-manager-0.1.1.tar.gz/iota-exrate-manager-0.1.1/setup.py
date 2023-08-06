febff7bd494ab57c3bafdcd96b017292539d9027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:55 +0000

    Add package iota-exrate-manager/0.1.1/iota-exrate-manager-0.1.1.tar.gz

---
+++ packages/iota-exrate-manager/iota-exrate-manager-0.1.1.tar.gz/iota-exrate-manager-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='iota-exrate-manager',
+    version='0.1.1',    
+    description='Python package that keeps track of iota prices via various APIs and converts prices',
+    url='https://github.com/F-Node-Karlsruhe/iota-exrate-manager',
+    author='F-Node-Karlsruhe',
+    author_email='contact@f-node.de',
+    license='MIT License',
+    packages=['iota_exrate_manager'],
+    install_requires=[
+                    'certifi>=2021.5.30',
+                    'charset-normalizer>=2.0.3',
+                    'idna>=3.2',
+                    'requests>=2.26.0',
+                    'urllib3>=1.26.6 ',                 
+                      ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Programming Language :: Python :: 3',
+    ],
+)

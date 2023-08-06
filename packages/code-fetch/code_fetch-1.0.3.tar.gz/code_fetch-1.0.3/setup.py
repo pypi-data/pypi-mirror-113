a11ffd6aee3574b489ef2501cf7a6a977102803f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:22 +0000

    Add package code-fetch/1.0.3/code_fetch-1.0.3.tar.gz

---
+++ packages/code-fetch/code_fetch-1.0.3.tar.gz/code_fetch-1.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="code_fetch",
+    version="1.0.3",
+    author="Mordy Waldo",
+    author_email="imky171@gmail.com",
+    description="A package to fetch single files from Github repos instead of cloning the whole thing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mordy-python/code-fetch",
+    project_urls={
+        "Bug Tracker": "https://github.com/mordy-python/code-fetch/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)

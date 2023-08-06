733cb5332586f01ebb9346685f165d8b82447182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:11 +0000

    Add package adaptnlp/0.2.4/adaptnlp-0.2.4-py3-none-any.whl

---
+++ packages/adaptnlp/adaptnlp-0.2.4-py3-none-any.whl/adaptnlp/__init__.py
@@ -0,0 +1,52 @@
+import pkg_resources
+from pathlib import Path
+
+# Easy Modules
+from .embeddings import (
+    EasyWordEmbeddings,
+    EasyStackedEmbeddings,
+    EasyDocumentEmbeddings,
+)
+from .token_classification import EasyTokenTagger
+from .sequence_classification import (
+    EasySequenceClassifier,
+    TransformersSequenceClassifier,
+    FlairSequenceClassifier,
+)
+from .question_answering import EasyQuestionAnswering, TransformersQuestionAnswering
+from .summarization import EasySummarizer, TransformersSummarizer
+from .translation import EasyTranslator, TransformersTranslator
+from .text_generation import EasyTextGenerator, TransformersTextGenerator
+from .language_model import LMFineTuner
+
+# Training and Fine-tuning Modules
+# TODO: Deprecating in 0.3.0+
+from .training import SequenceClassifierTrainer
+from .transformers.finetuning import LMFineTunerManual
+
+# global variable like flair's: cache_root
+cache_root = Path.home()/".adaptnlp"
+
+__version__ = "0.2.3"
+
+__all__ = [
+    "__version__",
+    "EasyWordEmbeddings",
+    "EasyStackedEmbeddings",
+    "EasyDocumentEmbeddings",
+    "EasyTokenTagger",
+    "EasySequenceClassifier",
+    "FlairSequenceClassifier",
+    "TransformersSequenceClassifier",
+    "EasyQuestionAnswering",
+    "TransformersQuestionAnswering",
+    "EasySummarizer",
+    "TransformersSummarizer",
+    "EasyTranslator",
+    "TransformersTranslator",
+    "EasyTextGenerator",
+    "TransformersTextGenerator",
+    "SequenceClassifierTrainer",
+    "LMFineTuner",
+    "LMFineTunerManual",
+]

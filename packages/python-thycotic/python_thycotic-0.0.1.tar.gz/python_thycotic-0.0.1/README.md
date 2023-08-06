ba75eae2af7fe5d2acb3b3b7b3b7038585d5ce6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:45 +0000

    Add package python-thycotic/0.0.1/python_thycotic-0.0.1.tar.gz

---
+++ packages/python-thycotic/python_thycotic-0.0.1.tar.gz/python_thycotic-0.0.1/README.md
@@ -0,0 +1,4 @@
+# Python Thycotic
+This is a Python wrapper for [Thycotic Secret Server API](https://updates.thycotic.net/secretserver/restapiguide/10.9/TokenAuth/)
+
+My plan is to first write the methods for the GET requests for all the endpoints, or whatever endpoints I have access to. I won't necessarily add all the request parameters for each request but they are easy to add later.
---
+++ packages/python-thycotic/python_thycotic-0.0.1.tar.gz/python_thycotic-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="python_thycotic",
+    version="0.0.1",
+    description="Python wrapper for Thycotic Secret Server API",
+    url="https://github.com/ksalman/python_thycotic",
+    author="Kashif Salman",
+    author_email="1268871+ksalman@users.noreply.github.com",
+    license="Apache Software License",
+    packages=["thycotic"],
+    zip_safe=False,
+)

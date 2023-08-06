476ea509c5f1acf7336a4db4cc50ce77e905498c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:41 +0000

    Add package medical-shape/0.0.1/medical_shape-0.0.1-py3-none-any.whl

---
+++ packages/medical-shape/medical_shape-0.0.1-py3-none-any.whl/medical_shape/__about__.py
@@ -0,0 +1,19 @@
+__version__ = '0.0.1'
+__author__ = 'Justus Schock'
+__author_email__ = 'justus.schock@posteo.de'
+__license__ = ''
+__copyright__ = f'Copyright (c) 2020-2021, {__author__}.'
+# TODO: Change website
+__homepage__ = 'https://github.com/justusschock/medical_shape'
+__docs__ = "Shape Fitting for medical image data"
+__docs_url__ = ""
+__long_doc__ = """ """
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__copyright__",
+    "__docs__",
+    "__homepage__",
+    "__license__",
+    "__version__",
+]

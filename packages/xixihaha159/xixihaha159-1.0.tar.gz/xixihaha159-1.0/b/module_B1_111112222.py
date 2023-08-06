f04e8b3d553894188403b66f617843d7f3b95168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:32 +0000

    Add package xixihaha159/1.0/xixihaha159-1.0.tar.gz

---
+++ packages/xixihaha159/xixihaha159-1.0.tar.gz/xixihaha159-1.0/b/module_B1_111112222.py
@@ -0,0 +1,8 @@
+# import a.aa.module_A4
+# from a.aa import module_A4
+# from a.aa.module_A4 import fun_AA
+# # fun_AA()
+
+# import a
+# print(a.math.pi)
+
---
+++ packages/xixihaha159/xixihaha159-1.0.tar.gz/xixihaha159-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+name='xixihaha159',# 对外我们模块的名字
+version='1.0', # 版本号
+description='这是第一个对外发布的模块，测试哦', #描述
+author='1122',# 作者
+author_email='1jjjj@qq.com',
+py_modules=['b.module_B1_111112222','b.module_B2_111112222'] # 要发布的模块
+)

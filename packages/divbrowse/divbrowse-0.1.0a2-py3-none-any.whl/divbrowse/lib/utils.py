45dcdac6aa2a35d381973e7531f17d0f4028d42f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:53 +0000

    Add package divbrowse/0.1.0a2/divbrowse-0.1.0a2-py3-none-any.whl

---
+++ packages/divbrowse/divbrowse-0.1.0a2-py3-none-any.whl/divbrowse/lib/utils.py
@@ -0,0 +1,39 @@
+import gunicorn.app.base
+
+RED = '\033[31m'
+RESET = '\033[0m'
+
+def print_error(msg):
+    print(f"{RED}ERROR: {msg}{RESET}")
+    pass
+
+class ApiError(Exception):
+    status_code = 400
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['status'] = 'error'
+        rv['message'] = self.message
+        return rv
+
+
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application

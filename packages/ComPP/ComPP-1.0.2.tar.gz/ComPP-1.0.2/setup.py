c81b3c3a60e971548abab4763d1c51e49ec6b049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:26 +0000

    Add package ComPP/1.0.2/ComPP-1.0.2.tar.gz

---
+++ packages/ComPP/ComPP-1.0.2.tar.gz/ComPP-1.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='ComPP',
+    version='1.0.2',
+    author="Zeecka",
+    packages=["ComPP"],
+    scripts=["bin/ComPP"],
+    install_requires=["argparse", "colorama"],
+    license='MIT License',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/sec-it/ComPP',
+    project_urls = {
+        "Source": "https://github.com/sec-it/ComPP",
+        "Tracker": "https://github.com/sec-it/ComPP/issues"
+    },
+    package_data={
+        "ComPP": ["*"]
+    },
+    python_requires=">=3.7",
+)

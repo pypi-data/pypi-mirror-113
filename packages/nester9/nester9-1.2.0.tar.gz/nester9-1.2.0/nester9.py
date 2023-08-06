845f3390f05479c5ed1d437e57881e5094432fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:36 +0000

    Add package nester9/1.2.0/nester9-1.2.0.tar.gz

---
+++ packages/nester9/nester9-1.2.0.tar.gz/nester9-1.2.0/nester9.py
@@ -0,0 +1,13 @@
+"""
+A sample which provide print_lol().
+"""
+
+
+def print_lol(the_list, level=0):
+    """print list"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            print('\t'*level, end='')
+            print(each_item)

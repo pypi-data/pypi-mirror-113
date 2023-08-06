d8df8b550de2263246298f9e38a2a5cbf9ceec2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:45 +0000

    Add package py-automapper/0.1.1/py_automapper-0.1.1-py3-none-any.whl

---
+++ packages/py-automapper/py_automapper-0.1.1-py3-none-any.whl/automapper/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa: F401
+from .mapper import Mapper
+
+from .exceptions import DuplicatedRegistrationError, MappingError, CircularReferenceError
+
+from .mapper_initializer import create_mapper
+
+# Global mapper
+mapper = create_mapper()

4c286985825878169d7ea4051347b179a9def651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:44 +0000

    Add package pybuildme/0.1.0/pybuildme-0.1.0.tar.gz

---
+++ packages/pybuildme/pybuildme-0.1.0.tar.gz/pybuildme-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'pybuildme',
+    python_requires='>=3.7',
+    packages = find_packages(),
+    install_requires=[],
+    version = '0.1.0',
+    description = 'PyBuild is a build environment for Python that starts from the virtual environment upwards, must be installed on base system prior to running.',
+    # long_description = long_description,
+    author = 'Kyle Darling',
+    author_email = 'kdarling95@yahoo.com',
+    url = 'https://github.com/IAmAbszol/PyBuild',
+    download_url = '',
+    keywords = ['build', 'python', 'automation', 'git'],
+    classifiers = [],
+    license = "MIT",
+    include_package_data=True
+)
